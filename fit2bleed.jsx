6c7f2b7020ade4a4260e708f99fd8a235a3635f7
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-03-04 14:28:17 +0200

    Add files via upload

--- fit2bleed.jsx
+++ fit2bleed.jsx
@@ -1,5 +1,5 @@
 /*
-    Fit to bleedbox v1.0.1
+    Fit to bleedbox v1.1
     © March 2020, Paul Chiorean
     This script resizes the selection to page bleedbox.
 */
@@ -9,12 +9,17 @@ var doc = app.activeDocument;
 // Function to calculate bleedbox coordinates
 function pageBleedSize(page) {
     var pageSize = doc.pages[page].bounds;
-    var pageBleed = doc.documentPreferences.properties.documentBleedTopOffset;
-    if (pageBleed != 0) {
-        var b_y1 = pageSize[0] - pageBleed;
-        var b_x1 = pageSize[1] - pageBleed;
-        var b_y2 = pageSize[2] + pageBleed;
-        var b_x2 = pageSize[3] + pageBleed;
+    var pageBleed = {
+        top: doc.documentPreferences.properties.documentBleedTopOffset,
+        left: doc.documentPreferences.properties.documentBleedInsideOrLeftOffset,
+        bottom: doc.documentPreferences.properties.documentBleedBottomOffset,
+        right: doc.documentPreferences.properties.documentBleedOutsideOrRightOffset
+    }
+    if (pageBleed.top + pageBleed.left + pageBleed.bottom + pageBleed.right != 0) {
+        var b_y1 = pageSize[0] - pageBleed.top;
+        var b_x1 = pageSize[1] - pageBleed.left;
+        var b_y2 = pageSize[2] + pageBleed.bottom;
+        var b_x2 = pageSize[3] + pageBleed.right;
         return [b_y1, b_x1, b_y2, b_x2]
     } else {
         return pageSize
