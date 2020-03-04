f35e843d46dec6fa1505e38a9cf1e5fd8f4be16f
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-03-04 19:03:20 +0200

    WIP

--- fit2margins.jsx
+++ fit2margins.jsx
@@ -1,5 +1,5 @@
 /*
-    Fit to margins v1.3
+    Fit to margins v1.4 WIP
     © March 2020, Paul Chiorean
     This script resizes the selection to page margins.
 */
@@ -9,28 +9,38 @@ var doc = app.activeDocument;
 // Function to calculate safe area coordinates from page margin size
 // ***TODO*** Check filenames for safe area size
 function pageSafeArea(page) {
-    var pageSize = doc.pages[page].bounds; // check for spreads
-    if (doc.pages[page].side != PageSideOptions.LEFT_HAND) {
-        var pageMargins = {
-            top: doc.pages[page].marginPreferences.top,
-            bottom: doc.pages[page].marginPreferences.bottom,
-            left: doc.pages[page].marginPreferences.left,
-            right: doc.pages[page].marginPreferences.right
-        }
-    } else {
-        // Reverse left and right if left-hand page
-        var pageMargins = {
-            top: doc.pages[page].marginPreferences.top,
-            bottom: doc.pages[page].marginPreferences.bottom,
-            left: doc.pages[page].marginPreferences.right,
-            right: doc.pages[page].marginPreferences.left
-        }
+    var curPage = doc.pages[page];
+    var pageSize = curPage.bounds; // check for spreads
+
+    // Set ruler origin to spread
+    var ro = doc.viewPreferences.rulerOrigin;
+    doc.viewPreferences.rulerOrigin = RulerOrigin.SPREAD_ORIGIN;
+
+    var pageMargins = {
+        top: curPage.marginPreferences.top,
+        bottom: curPage.marginPreferences.bottom,
+        left: curPage.marginPreferences.left,
+        right: curPage.marginPreferences.right
+    }
+    // Reverse left and right if left-hand page
+    if (curPage.side == PageSideOptions.LEFT_HAND) {
+        pageMargins.left = curPage.marginPreferences.right;
+        pageMargins.right = curPage.marginPreferences.left
     }
     if (pageMargins.top + pageMargins.left + pageMargins.bottom + pageMargins.right != 0) {
         var m_y1 = pageMargins.top;
         var m_x1 = pageMargins.left;
         var m_y2 = pageSize[2] - pageMargins.bottom;
         var m_x2 = pageSize[3] - pageMargins.right;
+        if (curPage.side == PageSideOptions.RIGHT_HAND) {
+            // var m_x1 = pageMargins.left;
+            // var m_x2 = pageSize[3] - pageMargins.right;
+        } else {}
+
+        //alert("pageSize = [" + pageSize[0] + ", " + pageSize[1] + ", " + pageSize[2] + ", " + pageSize[3] + "]\n" + "pageMargins = [" + pageMargins.top + ", " + pageMargins.left + ", " + pageMargins.bottom + ", " + pageMargins.right + "]\n" + "pageSafeArea = [" + [m_y1, m_x1, m_y2, m_x2] + "]");
+
+        // Restore ruler origin
+        doc.viewPreferences.rulerOrigin = ro;
         return [m_y1, m_x1, m_y2, m_x2]
     } else {
         return pageSize
@@ -41,9 +51,7 @@ function pageSafeArea(page) {
 // ***TODO*** If selection = multiple objects, resize it proportionally as an unit
 if (doc.selection.length != 0) {
     var docPage = doc.selection[0].parentPage.documentOffset;
-    if (pageSafeArea(docPage) != false) {
-        doc.selection[0].geometricBounds = pageSafeArea(docPage)
-    }
+    doc.selection[0].geometricBounds = pageSafeArea(docPage)
 } else {
     // alert("Please select an object and try again.")
 }
