252e1caa6c0c0cd1a265fcafc00b82db8a8df32b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-03-05 13:38:28 +0200

    updated to consider spreads instead of pages

--- fit2margins.jsx
+++ fit2margins.jsx
@@ -1,62 +1,70 @@
 /*
-    Fit to margins v1.4 WIP
+    Fit to margins v1.4
     © March 2020, Paul Chiorean
     This script resizes the selection to page margins.
 */
 
 var doc = app.activeDocument;
-var spread = doc.selection[0].parentPage.parent;
 
 // If something is selected, resize it to safe area, if defined
-// ***TODO*** If selection = multiple objects, resize it proportionally as an unit
-if (doc.selection.length != 0) {
-    doc.selection[0].geometricBounds = pageSafeArea(spread.index)
+if (doc.selection.length != 0 && doc.selection[0].parentPage != null) {
+    var spread = doc.selection[0].parentPage.parent; // parent spread
+    for (i = 0; i < doc.selection.length; i++) {
+        doc.selection[i].geometricBounds = pageSafeArea(spread.index)
+    }
 } else {
-    // ***TODO*** Check if on pasteboard
-    // alert("Please select an object and try again.")
+    // alert("Please select an object not on pasteboard and try again.")
 }
 
-// Function to calculate safe area coordinates from page/spread margin size
+// Function to calculate safe area coordinates from page(s) margin size
 // ***TODO*** Check filenames for safe area size
 function pageSafeArea(s) {
-    // Set ruler origin to spread
+    // Save setting and set ruler origin to spread
     var ro = doc.viewPreferences.rulerOrigin;
     doc.viewPreferences.rulerOrigin = RulerOrigin.SPREAD_ORIGIN;
 
-    var spreadPages = doc.selection[0].parentPage.parent.pages;
-    var curPage = doc.spreads[s].pages[0];
-    // alert("spread: " + s + "\rpage in spread: " + curPage.index + "/" + (spreadPages.length - 1));
-    if (spreadPages.length > 1) {
-        // Spread is multiple pages
-
-    } else {
+    var spreadPages = doc.spreads[s].pages; // spread pages
+    var firstPage = spreadPages.firstItem(); // first page of spread
+    if (spreadPages.length == 1) {
         // Spread is single page
-        var pageSize = curPage.bounds;
+        var pageSize = firstPage.bounds;
         var pageMargins = {
-            top: curPage.marginPreferences.top,
-            bottom: curPage.marginPreferences.bottom,
-            left: curPage.marginPreferences.left,
-            right: curPage.marginPreferences.right
+            top: firstPage.marginPreferences.top,
+            left: firstPage.marginPreferences.left,
+            bottom: firstPage.marginPreferences.bottom,
+            right: firstPage.marginPreferences.right
         }
-        // Reverse left and right if left-hand page
-        if (curPage.side == PageSideOptions.LEFT_HAND) {
-            pageMargins.left = curPage.marginPreferences.right;
-            pageMargins.right = curPage.marginPreferences.left
+        // Reverse left and right margins if left-hand page
+        if (firstPage.side == PageSideOptions.LEFT_HAND) {
+            pageMargins.left = firstPage.marginPreferences.right;
+            pageMargins.right = firstPage.marginPreferences.left
         }
-        if (pageMargins.top + pageMargins.left + pageMargins.bottom + pageMargins.right != 0) {
-            var m_y1 = pageMargins.top;
-            var m_x1 = pageMargins.left;
-            var m_y2 = pageSize[2] - pageMargins.bottom;
-            var m_x2 = pageSize[3] - pageMargins.right;
-            if (curPage.side == PageSideOptions.RIGHT_HAND) {
+    } else {
+        // Spread is multiple pages
+        var lastPage = spreadPages.lastItem(); // last page of spread
+        var pageSize = [firstPage.bounds[0], firstPage.bounds[1], lastPage.bounds[2], lastPage.bounds[3]]
+        var pageMargins = {
+            top: firstPage.marginPreferences.top,
+            left: firstPage.marginPreferences.left,
+            bottom: lastPage.marginPreferences.bottom,
+            right: lastPage.marginPreferences.right
+        }
+        // Reverse left and right margins if left-hand page
+        if (firstPage.side == PageSideOptions.LEFT_HAND) {
+            pageMargins.left = firstPage.marginPreferences.right;
+        }
+    }
+    if (pageMargins.top + pageMargins.left + pageMargins.bottom + pageMargins.right != 0) {
+        var m_y1 = pageMargins.top;
+        var m_x1 = pageMargins.left;
+        var m_y2 = pageSize[2] - pageMargins.bottom;
+        var m_x2 = pageSize[3] - pageMargins.right;
+        // alert("pageSize = [" + pageSize[0] + ", " + pageSize[1] + ", " + pageSize[2] + ", " + pageSize[3] + "]\r" + "pageMargins.left = " + pageMargins.left + ", pageMargins.right = " + pageMargins.right + "\r" + "pageSafeArea = [" + [m_y1, m_x1, m_y2, m_x2] + "]");
 
-            }
-            // alert("pageSize = [" + pageSize[0] + ", " + pageSize[1] + ", " + pageSize[2] + ", " + pageSize[3] + "]\n" + "pageMargins = [" + pageMargins.top + ", " + pageMargins.left + ", " + pageMargins.bottom + ", " + pageMargins.right + "]\n" + "pageSafeArea = [" + [m_y1, m_x1, m_y2, m_x2] + "]");
-            // Restore ruler origin
-            doc.viewPreferences.rulerOrigin = ro;
-            return [m_y1, m_x1, m_y2, m_x2]
-        } else {
-            return pageSize
-        }
+        // Restore ruler origin
+        doc.viewPreferences.rulerOrigin = ro;
+        return [m_y1, m_x1, m_y2, m_x2]
+    } else {
+        return pageSize
     }
 }
