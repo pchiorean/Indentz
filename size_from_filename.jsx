4fabd64e97d35efecea0a787357d08d46a202c18
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-03-22 21:44:53 +0200

    Update size_from_filename.jsx

--- size_from_filename.jsx
+++ size_from_filename.jsx
@@ -15,14 +15,12 @@ doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
 
 var docName = doc.name.substr(0, doc.name.lastIndexOf(".")); // name w/o extension
 var sizeArray = docName.match(/_\d{2,}([\.,]\d{1,2})?x\d{2,}([\.,]\d{1,2})?/ig); // match '_000x000' pairs
-alert(sizeArray);
 
 if (sizeArray != null) { // at least one pair of dimensions
     for (i = 0; i < sizeArray.length; i++) { // clean up
         sizeArray[i] = sizeArray[i].replace(/_/g, "");
         sizeArray[i] = sizeArray[i].replace(/,/g, ".");
     }
-alert(sizeArray);
     switch (sizeArray.length) {
         case 1: // one pair; page size
             var p = 0;
