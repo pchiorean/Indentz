21f0fad7d020c5dec26847df71cb2ede6f2ee390
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-03-22 17:42:09 +0200

    Add files via upload

--- size_from_filename.jsx
+++ size_from_filename.jsx
@@ -1,37 +1,60 @@
 /*
-    Size from filename v1.0
+    Size from filename v1.1
     © March 2020, Paul Chiorean
-    This script sets every page size and margins based on the filename. 
+    This script sets every page size and margins based on the filename.
     It looks for pairs of values like 000x000 (page size) or 000x000_000x000 (page size_safe area).
 */
 
 var doc = app.activeDocument;
+app.scriptPreferences.measurementUnit = MeasurementUnits.MILLIMETERS;
 doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-app.scriptPreferences.measurementUnit = MeasurementUnits.MILLIMETERS;
+doc.adjustLayoutPreferences.enableAdjustLayout = false;
+doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
+// app.generalPreferences.objectsMoveWithPage = false;
 
 var docName = doc.name.substr(0, doc.name.lastIndexOf(".")); // name w/o extension
-var sizeArray = docName.match(/_\d{2,}x\d{2,}/ig); // match '_000x000'
+var sizeArray = docName.match(/_\d{2,}x\d{2,}/ig); // match '_000x000' pairs
 
-if (sizeArray != null) { // At least one pair of dimensions
-    // Clean up underscores
-    for (i = 0; i < sizeArray.length; i++) {
-        sizeArray[i] = sizeArray[i].replace(/_/g, ""); // delete '_'
+if (sizeArray != null) { // at least one pair of dimensions
+    for (i = 0; i < sizeArray.length; i++) { // clean up underscores
+        sizeArray[i] = sizeArray[i].replace(/_/g, "");
     }
+    switch (sizeArray.length) {
+        case 1: // one pair; page size
+            var p = 0;
+            break;
+        case 2: // 2 pairs; page size is larger
+            var dimA = sizeArray[0].split("x");
+            var dimB = sizeArray[1].split("x");
+            if ((dimA[0] >= dimB[0]) && (dimA[1] >= dimB[1])) {
+                var p = 0; var m = 1;
+            } else {
+                var p = 1; var m = 0;
+            }
+    }
+    resizePages(p, m);
 
-    // 1st pair is page size
-    var pageSize = sizeArray[0].split("x");
-    var pageSizeMM = {
+    // Check for bleed
+    var bleedArray = /_\d{2,}x\d{2,}_(\d{1,3})\s?[cm]m/i.exec(docName); // match '_000x000_(00)mm'
+    if (bleedArray != null) {
+        doc.documentPreferences.documentBleedUniformSize = true;
+        doc.documentPreferences.documentBleedTopOffset = bleedArray[1];
+    }
+}
+
+
+function resizePages(p, m) {
+    var pageSize = sizeArray[p].split("x");
+    var pageSizeMM = { // page size in millimeters
         width: Number(pageSize[0]),
         height: Number(pageSize[1])
-    };
-    var pageSizePT = {
+    }
+    var pageSizePT = { // page size in points
         width: pageSizeMM.width / 0.352777777777778,
         height: pageSizeMM.height / 0.352777777777778
-    };
-    // Resize page(s)
-    doc.documentPreferences.pageWidth = pageSizeMM.width;
-    doc.documentPreferences.pageHeight = pageSizeMM.height;
+    }
+    // Resize pages
     for (var i = 0; i < doc.pages.length; i++) {
         doc.pages[i].layoutRule = LayoutRuleOptions.OFF;
         doc.pages[i].resize(CoordinateSpaces.INNER_COORDINATES,
@@ -39,17 +62,18 @@ if (sizeArray != null) { // At least one pair of dimensions
             ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH,
             [pageSizePT.width, pageSizePT.height]);
     }
+    // Also set document size
+    doc.documentPreferences.pageWidth = pageSizeMM.width;
+    doc.documentPreferences.pageHeight = pageSizeMM.height;
 
-    // If 2 pairs, 2nd is safe area (or is it?)
-    if (sizeArray.length == 2) {
-        var pageMargins = sizeArray[1].split("x");
+    if (m != null) { // margins
+        var pageMargins = sizeArray[m].split("x");
         var pageMarginsMM = {
-            top: Math.abs((pageSizeMM.height - pageMargins[1]) / 2),
-            left: Math.abs((pageSizeMM.width - pageMargins[0]) / 2),
-            bottom: Math.abs((pageSizeMM.height - pageMargins[1]) / 2),
-            right: Math.abs((pageSizeMM.width - pageMargins[0]) / 2)
+            top: (pageSizeMM.height - pageMargins[1]) / 2,
+            left: (pageSizeMM.width - pageMargins[0]) / 2,
+            bottom: (pageSizeMM.height - pageMargins[1]) / 2,
+            right: (pageSizeMM.width - pageMargins[0]) / 2
         }
-        // Set margins
         for (var i = 0; i < doc.pages.length; i++) {
             doc.pages[i].marginPreferences.properties = pageMarginsMM;
         }
