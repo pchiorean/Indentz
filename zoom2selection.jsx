80c775f884c1ef2f7424ca65b986bc9087be3f83
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-08 08:08:15 +0300

    zoom to page as backup (issue #21)

--- zoom2selection.jsx
+++ zoom2selection.jsx
@@ -1,11 +1,12 @@
 /*
-    Zoom to selection v1.2.3
+    Zoom to selection v1.3.0
     © April 2020, Paul Chiorean
-    This script zooms to the selected objects.
+    This script zooms to the selected objects or, if nothing is selected, to the current page.
 */
 
 var doc = app.activeDocument;
 var window = app.activeWindow;
+var selPage = window.activePage;
 var selObj = doc.selection;
 
 if (selObj.length != 0) {
@@ -15,23 +16,23 @@ if (selObj.length != 0) {
     var selObj_x2 = selObj[0].visibleBounds[3];
     for (i = 1; i < selObj.length; i++) {
         selObj_y1 = Math.min(selObj[i].visibleBounds[0], selObj_y1);
-        selObj_y1 = Math.min(selObj[i].visibleBounds[1], selObj_x1);
-        selObj_y1 = Math.max(selObj[i].visibleBounds[2], selObj_y2);
-        selObj_y1 = Math.max(selObj[i].visibleBounds[3], selObj_x2);
+        selObj_x1 = Math.min(selObj[i].visibleBounds[1], selObj_x1);
+        selObj_y2 = Math.max(selObj[i].visibleBounds[2], selObj_y2);
+        selObj_x2 = Math.max(selObj[i].visibleBounds[3], selObj_x2);
     }
-    var W_sel = selObj_x2 - selObj_x1;
-    var H_sel = selObj_y2 - selObj_y1;
-
-    var W_win = window.bounds[3] - window.bounds[1];
-    var H_win = (window.bounds[2] - window.bounds[0]) * 1.33;
-
-    var zoom = H_win / H_sel;
-    if (W_sel * zoom > W_win) { zoom = W_win / W_sel }
-    zoom = zoom * 13.3;
-
-    window.zoom(ZoomOptions.fitPage);
-    if (zoom > 4000) { zoom = 4000 };
-    window.zoomPercentage = zoom;
+    var W_obj = selObj_x2 - selObj_x1;
+    var H_obj = selObj_y2 - selObj_y1;
 } else {
-    // alert("Please select an object not on pasteboard and try again.")
+    var W_obj = selPage.bounds[3] - selPage.bounds[1];
+    var H_obj = selPage.bounds[2] - selPage.bounds[0];
 }
+var W_win = window.bounds[3] - window.bounds[1];
+var H_win = (window.bounds[2] - window.bounds[0]) * 1.33;
+
+var zoom = H_win / H_obj;
+if (W_obj * zoom > W_win) { zoom = W_win / W_obj }
+zoom = zoom * 13.3;
+
+window.zoom(ZoomOptions.FIT_PAGE);
+if (zoom > 4000) { zoom = 4000 };
+window.zoomPercentage = zoom;
