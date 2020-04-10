9a0ab8bf8e74d65f7f663c32d7718e3e402c6da0
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-10 21:56:56 +0300

    avoid error 30481 'Data is out of range'

--- zoom2selection.jsx
+++ zoom2selection.jsx
@@ -1,5 +1,5 @@
 /*
-    Zoom to selection v1.4.1
+    Zoom to selection v1.4.2
     © April 2020, Paul Chiorean
     This script zooms to the selected objects or, if nothing is selected, to the current page.
 */
@@ -8,7 +8,7 @@ var doc = app.activeDocument;
 var window = app.activeWindow;
 var selPage = window.activePage;
 var selObj = doc.selection;
-app.scriptPreferences.measurementUnit = MeasurementUnits.MILLIMETERS;
+app.scriptPreferences.measurementUnit = MeasurementUnits.PIXELS;
 
 // Get target dimensions
 if (selObj.length != 0) {
@@ -38,13 +38,22 @@ if (selObj.length != 0) {
     var W_obj = selPage.bounds[3] - selPage.bounds[1];
     var H_obj = selPage.bounds[2] - selPage.bounds[0];
 }
-W_obj = Math.max(W_obj, 13); H_obj = Math.max(H_obj, 13); // OoB err?!
 
 // Get window dimensions
 var W_win = window.bounds[3] - window.bounds[1];
 var H_win = (window.bounds[2] - window.bounds[0]) * 1.33;
 
-var zoom = (W_obj * H_win / H_obj > W_win) ? W_win / W_obj : H_win / H_obj;
-zoom = Math.min(zoom * 10 * 1.5, 4000);// 4000% is max zoom
-window.zoom(ZoomOptions.FIT_PAGE); try { window.zoomPercentage = zoom } catch (e) {};
-try { app.select(sel) } catch (e) {}; // Restore initial selection
+// Compute zoom percentage
+var zoom = Math.min(W_win / W_obj, H_win / H_obj);
+zoom = Number(zoom * 10 * 4).toFixed(1); // Adjust to taste
+zoom = Math.max(5, zoom); zoom = Math.min(zoom, 4000); // Fit in 5-4000% range
+
+// Zoom
+window.zoom(ZoomOptions.FIT_PAGE);
+try { window.zoomPercentage = zoom } catch (e) {
+    // Avoid error 30481 'Data is out of range'
+    app.menuActions.item("$ID/Fit Selection in Window").invoke();
+}
+
+// Restore initial selection
+try { app.select(sel) } catch (e) {};
