d4de9c17ce4c00248f91efcbaeff783eb54e3104
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-12 13:37:46 +0300

    transformation units error

--- page_size_from_margins.jsx
+++ page_size_from_margins.jsx
@@ -1,10 +1,13 @@
 /*
-    Page size from page margins v1.0.0
+    Page size from page margins v1.0.1
     © April 2020, Paul Chiorean
     This script sets the page size to the page margins.
 */
 
 var doc = app.activeDocument;
+app.scriptPreferences.measurementUnit = MeasurementUnits.MILLIMETERS;
+doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
+doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.adjustLayoutPreferences.enableAdjustLayout = false;
 doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
 // app.generalPreferences.objectsMoveWithPage = false;
@@ -18,12 +21,12 @@ for (var i = 0; i < doc.pages.length; i++) {
             pageMargins.left = page.marginPreferences.right;
             pageMargins.right = page.marginPreferences.left;
         }
-        var m_y1 = (page.bounds[0] + pageMargins.top) / 0.352777777777778;
-        var m_x1 = (page.bounds[1] + pageMargins.left) / 0.352777777777778;
-        var m_y2 = (page.bounds[2] - pageMargins.bottom) / 0.352777777777778;
-        var m_x2 = (page.bounds[3] - pageMargins.right) / 0.352777777777778;
-        var pageTL = [m_x1, m_y1];
-        var pageBR = [m_x2, m_y2];
+        var m_y1 = (page.bounds[0] + pageMargins.top);
+        var m_x1 = (page.bounds[1] + pageMargins.left);
+        var m_y2 = (page.bounds[2] - pageMargins.bottom);
+        var m_x2 = (page.bounds[3] - pageMargins.right);
+        var pageTL = [m_x1 / 0.352777777777778, m_y1 / 0.352777777777778];
+        var pageBR = [m_x2 / 0.352777777777778, m_y2 / 0.352777777777778];
         page.layoutRule = LayoutRuleOptions.OFF;
         page.reframe(CoordinateSpaces.INNER_COORDINATES, [pageTL, pageBR]);
         // Set margins to zero
@@ -34,8 +37,8 @@ for (var i = 0; i < doc.pages.length; i++) {
 if (doc.pages.length == 1) {
     // Also set document size
     var pageSize = {
-        width: (m_x2 - m_x1) / 0.352777777777778,
-        height: (m_y2 - m_y1) / 0.352777777777778
+        width: (m_x2 - m_x1),
+        height: (m_y2 - m_y1)
     };
     doc.documentPreferences.pageWidth = pageSize.width;
     doc.documentPreferences.pageHeight = pageSize.height;
