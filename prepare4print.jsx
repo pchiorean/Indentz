ef4c9a09412690cc8152d46f693d7ae6b57320ad
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-12 17:07:45 +0300

    prettify

--- prepare4print.jsx
+++ prepare4print.jsx
@@ -1,38 +1,32 @@
 ﻿/*
-    Prepare for print v1.3.0
+    Prepare for print v1.3.1
     © April 2020, Paul Chiorean
-    This script hides 'safe area' and moves 'dielines' to separate page(s).
+    This script hides 'safe area' layer and moves dielines to separate spread(s).
 */
 
 var doc = app.activeDocument;
 var safeLayer = doc.layers.item("safe area" | "Visible" | "Vis. area" | "vizibil");
 var dieLayer = doc.layers.item("dielines" | "diecut" | "Die Cut" | "cut lines" | "Stanze");
 
-doc.layers.everyItem().locked = false; // unlock all layers
-try { safeLayer.visible = false } catch (e) {}; // hide 'safe area' layer
-try { dieLayer.visible = true } catch (e) {}; // show 'dielines' layer
-
-// Function to check if page has dielines
-function dieLayerItems(i) {
-    for (var j = 0; j < doc.spreads[i].pageItems.length; j++) {
-        if (doc.spreads[i].pageItems.item(j).itemLayer.name == dieLayer.name) { return true };
-    }
-}
+doc.layers.everyItem().locked = false; // Unlock all layers
+try { safeLayer.visible = false } catch (e) {}; // Hide 'safe area' layer
+try { dieLayer.visible = true } catch (e) {}; // Show 'dielines' layer
 
 if (dieLayer.isValid) {
     for (var i = 0; i < doc.spreads.length; i++) {
         if (dieLayerItems(i)) {
-            // Page has dielines; duplicate it
+            // Spread has dielines; duplicate it
             doc.spreads[i].duplicate(LocationOptions.AFTER, doc.spreads[i]);
-            // Delete dielines from this spread
+            // Pass 1: delete dielines from this spread
             for (var j = 0; j < doc.spreads[i].pageItems.length; j++) {
                 var pageItem = doc.spreads[i].pageItems.item(j);
+                // Check for locked items
                 if (pageItem.itemLayer.name == dieLayer.name) {
                     if (pageItem.locked) { pageItem.locked = false };
                     pageItem.remove(); j--;
                 }
             }
-            // Delete non-dielines from next page
+            // Pass 1: delete non-dielines from next spread
             for (var j = 0; j < doc.spreads[i + 1].pageItems.length; j++) {
                 var pageItem = doc.spreads[i + 1].pageItems.item(j);
                 if (pageItem.itemLayer.name !== dieLayer.name) {
@@ -42,7 +36,16 @@ if (dieLayer.isValid) {
             }
             // If empty, delete spread
             if (doc.spreads[i].pageItems.length == 0) { doc.spreads[i].remove() };
-            i++ // skip next spread
+            i++ // Skip next spread
         }
+    }
+}
+// END
+
+
+// Function to check if spread has dielines
+function dieLayerItems(i) {
+    for (var j = 0; j < doc.spreads[i].pageItems.length; j++) {
+        if (doc.spreads[i].pageItems.item(j).itemLayer.name == dieLayer.name) { return true };
     }
 }
