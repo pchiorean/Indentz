d4ba5c9f50bfe63bd46f7ced7bde0d97c10ba0cc
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-13 18:17:23 +0300

    remove 'sanitize' section

--- zoom2selection.jsx
+++ zoom2selection.jsx
@@ -1,5 +1,5 @@
 /*
-    Zoom to selection v1.4.5
+    Zoom to selection v1.4.6
     © April 2020, Paul Chiorean
     This script zooms to the selected objects or, if nothing is selected, to the current page.
 */
@@ -9,41 +9,27 @@ var window = app.activeWindow;
 var selPage = window.activePage;
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 
-// Sanitize selection
-var sel = doc.selection;
-var selObj = [];
-if (sel.length != 0) {
-    for (var i = 0; i < sel.length; i++) {
-        switch (sel[i].constructor.name) {
-            case "Rectangle":
-            case "Oval":
-            case "Polygon":
-            case "TextFrame":
-            case "Group":
-            case "Button":
-            case "GraphicLine":
-                selObj.push(sel[i]);
-                break;
-        }
-    }
-}
+var sel = doc.selection; // Save selection
+var selObj = sel;
+var selObj_y1, selObj_x1, selObj_y2, selObj_x2;
+var W_win, H_win, W_obj, H_obj, zoom;
 
 // Get target dimensions
-if (selObj.length != 0) {
+if (selObj.length > 0 && selObj[0].constructor.name != "Guide") {
         // Something is selected, get dimensions
         if (selObj[0].hasOwnProperty("parentTextFrames")) {
             // We are inside a text frame, get frame bounds
-            var selObj_y1 = selObj[0].parentTextFrames[0].visibleBounds[0];
-            var selObj_x1 = selObj[0].parentTextFrames[0].visibleBounds[1];
-            var selObj_y2 = selObj[0].parentTextFrames[0].visibleBounds[2];
-            var selObj_x2 = selObj[0].parentTextFrames[0].visibleBounds[3];
+            selObj_y1 = selObj[0].parentTextFrames[0].visibleBounds[0];
+            selObj_x1 = selObj[0].parentTextFrames[0].visibleBounds[1];
+            selObj_y2 = selObj[0].parentTextFrames[0].visibleBounds[2];
+            selObj_x2 = selObj[0].parentTextFrames[0].visibleBounds[3];
             app.select(selObj[0].parentTextFrames[0]); // Select text frame
         } else {
             // Iterate selection, get extremities
-            var selObj_y1 = selObj[0].visibleBounds[0];
-            var selObj_x1 = selObj[0].visibleBounds[1];
-            var selObj_y2 = selObj[0].visibleBounds[2];
-            var selObj_x2 = selObj[0].visibleBounds[3];
+            selObj_y1 = selObj[0].visibleBounds[0];
+            selObj_x1 = selObj[0].visibleBounds[1];
+            selObj_y2 = selObj[0].visibleBounds[2];
+            selObj_x2 = selObj[0].visibleBounds[3];
             for (var i = 1; i < selObj.length; i++) {
                 selObj_y1 = Math.min(selObj[i].visibleBounds[0], selObj_y1);
                 selObj_x1 = Math.min(selObj[i].visibleBounds[1], selObj_x1);
@@ -51,21 +37,21 @@ if (selObj.length != 0) {
                 selObj_x2 = Math.max(selObj[i].visibleBounds[3], selObj_x2);
             }
         }
-        var W_obj = selObj_x2 - selObj_x1;
-        var H_obj = selObj_y2 - selObj_y1;
+        W_obj = selObj_x2 - selObj_x1;
+        H_obj = selObj_y2 - selObj_y1;
     } else {
     // Nothing is selected, we'll zoom to page
-    var W_obj = selPage.bounds[3] - selPage.bounds[1];
-    var H_obj = selPage.bounds[2] - selPage.bounds[0];
+    W_obj = selPage.bounds[3] - selPage.bounds[1];
+    H_obj = selPage.bounds[2] - selPage.bounds[0];
 }
 
 // Get window dimensions
-var W_win = window.bounds[3] - window.bounds[1];
-var H_win = (window.bounds[2] - window.bounds[0]) * 1.33;
+W_win = window.bounds[3] - window.bounds[1];
+H_win = (window.bounds[2] - window.bounds[0]) * 1.33;
 
 // Compute zoom percentage
-var zoom = Math.min(W_win / W_obj, H_win / H_obj);
-zoom = Number(zoom * 10 * 4).toFixed(1); // Adjust to taste
+zoom = Math.min(W_win / W_obj, H_win / H_obj);
+zoom = Number(zoom * 10 * 4.4).toFixed(1); // Adjust to taste
 zoom = Math.max(5, zoom); zoom = Math.min(zoom, 4000); // Fit in 5-4000% range
 
 // Zoom to target
