pchiorean/Indentz

commit c56ba6de3bcacc5fd96b0fca0e3bdac02310b034

protect 'Safe area' color

delete_gremlins.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-    Delete gremlins v1.3.8
+    Delete gremlins v1.3.9
     © April 2020, Paul Chiorean
     This script does some househeeping.
 */
@@ -37,7 +37,7 @@ function normalizeCMYK( /*Document*/ doc, swa, a, r, o, t, k, i) {
         if (o.model != CM_PROCESS) continue;
         if (o.space != CS_CMYK) continue;
         t = swa.itemByName(o.name);
-        if (!t.isValid) continue;
+        if (!t.isValid || t != "Safe area") continue;
         for (i = (k = o.colorValue).length; i--; k[i] = Math.round(k[i]));
         k = __("C=%1 M=%2 Y=%3 K=%4", k[0], k[1], k[2], k[3]);
         (r[k] || (r[k] = [])).push({ id: t.id, name: t.name });

commit fbeba2e8f9f51fdfe3056eed64b32a54de973bc9

correct regression from c56ba6d

delete_gremlins.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-    Delete gremlins v1.4.0
+    Delete gremlins v1.4.1
     © April 2020, Paul Chiorean
     This script does some househeeping.
 */
@@ -37,7 +37,8 @@ function normalizeCMYK( /*Document*/ doc, swa, a, r, o, t, k, i) {
         if (o.model != CM_PROCESS) continue;
         if (o.space != CS_CMYK) continue;
         t = swa.itemByName(o.name);
-        if (!t.isValid || t != "Safe area") continue;
+        if (!t.isValid) continue;
+        if (t.name == "Safe area") continue;
         for (i = (k = o.colorValue).length; i--; k[i] = Math.round(k[i]));
         k = __("C=%1 M=%2 Y=%3 K=%4", k[0], k[1], k[2], k[3]);
         (r[k] || (r[k] = [])).push({ id: t.id, name: t.name });