b510d7b4bae737610e8516757b3c94a42a098dd4
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-17 16:47:03 +0300

    fix issue #31, clean-up & comment regexps

--- page_size_from_filename.jsx
+++ page_size_from_filename.jsx
@@ -1,5 +1,5 @@
 /*
-    Page size from filename v1.3.1
+    Page size from filename v1.3.2
     © April 2020, Paul Chiorean
     This script sets every page size and margins based on the filename.
     It looks for pairs of values like 000x000 (page size) or 000x000_000x000 (page size_safe area).
@@ -11,23 +11,31 @@ doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.adjustLayoutPreferences.enableAdjustLayout = false;
 doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
-// app.generalPreferences.objectsMoveWithPage = false;
+app.generalPreferences.objectsMoveWithPage = false;
 
-var docName = doc.name.substr(0, doc.name.lastIndexOf(".")); // name w/o extension
-var sizeArray = docName.match(/[_-]\d{2,}([\.,]\d{1,2})?\s?([cm]m)?\s?x\s?\d{2,}([\.,]\d{1,2})?\s?([cm]m)?/ig); // match '_000x000' pairs with optional decimals, whitespace & mm/cm
+var docName = doc.name.substr(0, doc.name.lastIndexOf(".")); // get name w/o extension
+// Get '_00[.0] [mm] x 00[.0] [mm]' pairs with optional decimals, whitespace & mm/cm
+var sizeArray = docName.match(/[_-]\d{2,}([\.,]\d{1,2})?\s?([cm]m)?\s?x\s?\d{2,}([\.,]\d{1,2})?\s?([cm]m)?(?!x)/ig);
+    // 1. [_-] -- '_' or '-' separator
+    // 2. \d{2,}([\.,]\d{1,2})? -- 2 digits or more followed by optional 1 or 2 decimals
+    // 3. \s?([cm]m)? -- optional space followed by optional 'cm' or 'mm'
+    // 4. \s?x\s? -- 'x' separator between optional spaces
+    // 5. identical to 2.
+    // 6. identical to 3.
+    // 7. (?!x) -- negative lookhead 'x' separator (to avoid _000x00x00)
 
 if (sizeArray != null) { // at least one pair of dimensions
     for (i = 0; i < sizeArray.length; i++) {
-        sizeArray[i] = sizeArray[i].replace(/[_-]/g, "");  // clean up underscores
-        sizeArray[i] = sizeArray[i].replace(/\s/g, "");    // clean up whitespace
+        sizeArray[i] = sizeArray[i].replace(/[_-]/g, ""); // clean up underscores
+        sizeArray[i] = sizeArray[i].replace(/\s/g, ""); // clean up whitespace
         sizeArray[i] = sizeArray[i].replace(/[cm]m/g, ""); // clean up mm/cm
-        sizeArray[i] = sizeArray[i].replace(/,/g, ".");    // replace commas
+        sizeArray[i] = sizeArray[i].replace(/,/g, "."); // replace commas
     }
     switch (sizeArray.length) {
-        case 1: // one pair; page size
+        case 1: // one pair: page size
             var p = 0;
             break;
-        case 2: // 2 pairs; page size & safe area; page size is larger
+        case 2: // 2 pairs: page size & safe area; page size is larger
             var dimA = sizeArray[0].split("x");
             var dimB = sizeArray[1].split("x");
             if ((dimA[0] >= dimB[0]) && (dimA[1] >= dimB[1])) {
@@ -40,7 +48,12 @@ if (sizeArray != null) { // at least one pair of dimensions
 }
 
 // Check for bleed
-var bleedArray = /[_+](\d{1,3})\s?(?:[cm]m)(?:.*)$/i.exec(docName); // match '_(00)[mm]' at end
+// Get '_00 [mm]' after '0 [mm]'
+var bleedArray = /\d\s?(?:[cm]m)?[_+](\d{1,2})\s?(?:[cm]m)/i.exec(docName);
+    // \d\s?(?:[cm]m)? -- 1 digit followed by optional space and optional 'cm' or 'mm'
+    // [_+] -- '_' or '+' separator
+    // (\d{1,2}) -- 1 or 2 digits (capturing group #1)
+    // \s?(?:[cm]m) -- optional space followed by mandatory 'cm' or 'mm'
 if (bleedArray != null) {
     doc.documentPreferences.documentBleedUniformSize = true;
     doc.documentPreferences.documentBleedTopOffset = bleedArray[1];
