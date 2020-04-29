0f7347c59d1be31e1182e2864238975fa4b5b105
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-04-29 15:08:58 +0300

    take into account asymmetric margins (#49)

--- page_size_from_margins.jsx
+++ page_size_from_margins.jsx
@@ -1,5 +1,5 @@
 /*
-	Page size from page margins v1.1.1
+	Page size from page margins v1.2.0
 	© April 2020, Paul Chiorean
 	This script sets the page size to the page margins.
 */
@@ -8,10 +8,6 @@ var doc = app.activeDocument;
 app.generalPreferences.objectsMoveWithPage = false;
 doc.adjustLayoutPreferences.enableAdjustLayout = false;
 doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
-
-// Save setting and set ruler origin to spread
-var ro = doc.viewPreferences.rulerOrigin;
-doc.viewPreferences.rulerOrigin = RulerOrigin.PAGE_ORIGIN;
 
 for (var i = 0; i < doc.pages.length; i++) {
 	var page = doc.pages[i];
@@ -27,15 +23,20 @@ for (var i = 0; i < doc.pages.length; i++) {
 		}
 		var m_y1 = page.bounds[0] + pageMargins.top;
 		var m_y2 = page.bounds[2] - pageMargins.bottom;
-		var pageSize = { width: (m_x2 - m_x1), height: (m_y2 - m_y1) };
+		// Make temp rectangle
+		var mg = page.rectangles.add({
+			geometricBounds: [m_y1, m_x1, m_y2, m_x2],
+			contentType: ContentType.UNASSIGNED, fillColor: "None", strokeColor: "None"
+		});
 		// Set margins to zero
 		page.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Page margins
 		doc.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Document margins
 		// Resize page
 		page.layoutRule = LayoutRuleOptions.OFF;
-		page.resize(CoordinateSpaces.INNER_COORDINATES,
-			AnchorPoint.CENTER_ANCHOR, ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH,
-			[pageSize.width / 0.352777777777778, pageSize.height / 0.352777777777778]);
+		var mg_TL = mg.resolve(AnchorPoint.TOP_LEFT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
+		var mg_BR = mg.resolve(AnchorPoint.BOTTOM_RIGHT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
+		page.reframe(CoordinateSpaces.SPREAD_COORDINATES, [mg_TL, mg_BR]);
+		mg.remove(); // Remove temp rectangle
 	}
 }
 // Also set document size
@@ -57,5 +58,3 @@ if (doc.pages.length == 1) {
 		doc.documentPreferences.pageHeight = pageSize.height;
 	}
 }
-// Restore ruler origin setting
-doc.viewPreferences.rulerOrigin = ro;
