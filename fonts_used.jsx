494686e2b1e3b14965fcace3d3c6aa6844679dc9
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-03 00:45:21 +0300

    FontStatus update

--- fonts_used.jsx
+++ fonts_used.jsx
@@ -1,16 +1,40 @@
 /*
-	Used fonts 1.0.0
+	Used fonts 1.1.0
 	© May 2020, Paul Chiorean
 	This script makes a list of the fonts used in the current document.
 */
 
-var usedFonts = app.activeDocument.fonts;
-var fontName1 = [], fontName2 = [];
+var usedFonts = app.activeDocument.fonts.everyItem().getElements();
+var fontName1 = [], fontName2 = [], fontName3 = [], fontName4 = [], fontName5 = [];
+
 for (var i = 0; i < usedFonts.length; i++) {
-	if (usedFonts[i].status == FontStatus.INSTALLED) {
-		fontName1.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
-	} else {
-		fontName2.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+	switch (String(usedFonts[i].status)) {
+		case "INSTALLED":
+			fontName1.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+			break;
+		case "SUBSTITUTED":
+			fontName2.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+			break;
+		case "FAUXED":
+			fontName3.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+			break;
+		case "NOT_AVAILABLE":
+			fontName4.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+			break;
+		case "UNKNOWN":
+			fontName5.push(usedFonts[i].fontFamily + " " + usedFonts[i].fontStyleName);
+			break;
 	}
 }
-alert("Fonts used:\r" + fontName1.join("\n") + "\r\rMissing:\r" + fontName2.join("\n"));
+
+var fontList = "INSTALLED:\r" + fontName1.join("\n") +
+	"\r\rSUBSTITUTED:\r" + fontName2.join("\n") +
+	"\r\rFAUXED:\r" + fontName3.join("\n") +
+	"\r\rNOT_AVAILABLE:\r" + fontName4.join("\n") +
+	"\r\rUNKNOWN:\r" + fontName5.join("\n");
+
+var w = new Window('dialog { text: "Document fonts", alignChildren: "right" }');
+w.ed = w.add('edittext', [0, 0, 400, 600], fontList, { multiline: true });
+w.buttons = w.add('group');
+w.close = w.buttons.add('button { text: "Close", properties: { name: "cancel" } }');
+w.show();
