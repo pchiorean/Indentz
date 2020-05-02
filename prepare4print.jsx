28be28e6ac680ec7df18724b76c05088b867ac08
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-03 00:42:37 +0300

    var. names; alt. 'dielines'

--- prepare4print.jsx
+++ prepare4print.jsx
@@ -1,13 +1,13 @@
 ﻿/*
-	Prepare for print v1.4.1
+	Prepare for print v1.4.2
 	© May 2020, Paul Chiorean
 	This script hides 'safe area' layer and moves dielines to separate spread(s).
 */
 
 var doc = app.activeDocument;
 
-var safeLayerName = ["safe area", "visible", "vizibil", "vis. area"];
-var dieLayerName = ["dielines", "diecut", "die cut", "cut lines", "stanze"];
+var safeLayerName = ["safe area", "visible", "Visible", "vizibil", "Vizibil", "vis. area", "Vis. area"];
+var dieLayerName = ["dielines", "diecut", "die cut", "Die Cut", "cut lines", "Stanze"];
 var safeLayer = findLayer(safeLayerName);
 var dieLayer = findLayer(dieLayerName);
 
@@ -16,49 +16,46 @@ try { safeLayer.visible = false } catch (_) {}; // Hide 'safe area' layer
 try { dieLayer.visible = true } catch (_) {}; // Show 'dielines' layer
 
 if (dieLayer != null) {
+	var selSp, pageItem;
 	for (var i = 0; i < doc.spreads.length; i++) {
-		if (dieLayerItems(i)) {
-			var pageItem;
-			// Spread has dielines; duplicate it
-			doc.spreads[i].duplicate(LocationOptions.AFTER, doc.spreads[i]);
-			// Pass 1: delete dielines from this spread
-			for (var j = 0; j < doc.spreads[i].pageItems.length; j++) {
-				pageItem = doc.spreads[i].pageItems.item(j);
-				// Check for locked items
-				if (pageItem.itemLayer.name == dieLayer.name) {
-					if (pageItem.locked) pageItem.locked = false;
-					pageItem.remove(); j--;
-				}
+		selSp = doc.spreads[i];
+		if (!dieLayerItems(selSp)) continue;
+		// Spread has dielines; duplicate it
+		selSp.duplicate(LocationOptions.AFTER, selSp);
+		// Pass 1: delete dielines from this spread
+		for (var j = 0; j < selSp.pageItems.length; j++) {
+			pageItem = selSp.pageItems.item(j);
+			// Check for locked items
+			if (pageItem.itemLayer.name == dieLayer.name) {
+				if (pageItem.locked) pageItem.locked = false;
+				pageItem.remove(); j--;
 			}
-			// Pass 2: delete non-dielines from next spread
-			for (var j = 0; j < doc.spreads[i + 1].pageItems.length; j++) {
-				pageItem = doc.spreads[i + 1].pageItems.item(j);
-				if (pageItem.itemLayer.name !== dieLayer.name) {
-					if (pageItem.locked) pageItem.locked = false;
-					pageItem.remove(); j--;
-				}
-			}
-			// If empty, delete spread
-			if (doc.spreads[i].pageItems.length == 0) doc.spreads[i].remove();
-			i++; // Skip next spread
 		}
+		// Pass 2: delete non-dielines from next spread
+		for (var j = 0; j < doc.spreads[i + 1].pageItems.length; j++) {
+			pageItem = doc.spreads[i + 1].pageItems.item(j);
+			if (pageItem.itemLayer.name !== dieLayer.name) {
+				if (pageItem.locked) pageItem.locked = false;
+				pageItem.remove(); j--;
+			}
+		}
+		if (selSp.pageItems.length == 0) selSp.remove(); // If empty, delete spread
+		i++; // Skip next spread
 	}
 }
-// END
 
-
-// Function to check if spread has dielines
-function dieLayerItems(spread) {
-	for (var i = 0; i < doc.spreads[spread].pageItems.length; i++) {
-		if (doc.spreads[spread].pageItems.item(i).itemLayer.name == dieLayer.name) return true;
-	}
-}
 
 // Function to find first layer from a list of names
 function findLayer(names) {
-	var layer;
 	for (var i = 0; i < names.length; i++) {
-		layer = doc.layers.item(names[i]);
-		if (layer.isValid) { return layer } else continue;
+		var layer = doc.layers.item(names[i]);
+		if (layer.isValid) return layer;
+	}
+}
+
+// Function to check if spread has dielines
+function dieLayerItems(spread) {
+	for (var i = 0; i < spread.pageItems.length; i++) {
+		if (spread.pageItems.item(i).itemLayer.name == dieLayer.name) return true;
 	}
 }
