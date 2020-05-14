bf7ccac42f40df6b43ab80c7560dcf72e059885d
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-14 10:05:50 +0300

    first commit (#37)

---
+++ fit2text.jsx
@@ -0,0 +1,19 @@
+/*
+	Fit frame to text v1.0.0
+	© May 2020, Paul Chiorean
+	This script auto-sizes the text frame to the content.
+*/
+
+var doc = app.activeDocument;
+var sel = doc.selection;
+
+if (sel.length > 0 && sel[0].constructor.name == "TextFrame") {
+	sel[0].fit(FitOptions.FRAME_TO_CONTENT);
+	sel[0].textFramePreferences.properties = {
+		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
+		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
+		useNoLineBreaksForAutoSizing: true
+	}
+	sel[0].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
+	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_LEFT_POINT;
+} else alert("Select a text frame and try again.");
