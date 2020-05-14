291793eef22410a763b2b77d7b2424ea1edf24b2
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-14 10:05:28 +0300

    omit step 3

--- delete_gremlins.jsx
+++ delete_gremlins.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-	Delete gremlins v1.4.6
+	Delete gremlins v1.4.7
 	© May 2020, Paul Chiorean
 	This script does some househeeping.
 */
@@ -29,31 +29,30 @@ for (var i = doc.unusedSwatches.length - 1; i >= 0; i--) {
 }
 
 // Step 3. Normalize similar CMYK swatches
-const __ = $.global.localize;
-const CM_PROCESS = +ColorModel.PROCESS;
-const CS_CMYK = +ColorSpace.CMYK;
-var swa, a, r, o, t, k, i;
-swa = doc.swatches;
-a = doc.colors.everyItem().properties;
-r = {};
-// Gather CMYK swatches => {CMYK_Key => {id, name}[]}
-while (o = a.shift()) {
-	if (o.model != CM_PROCESS) continue;
-	if (o.space != CS_CMYK) continue;
-	t = swa.itemByName(o.name);
-	if (!t.isValid) continue;
-	if (t.name == "Safe area") continue;
-	for (i = (k = o.colorValue).length; i--; k[i] = Math.round(k[i]));
-	k = __("C=%1 M=%2 Y=%3 K=%4", k[0], k[1], k[2], k[3]);
-	(r[k] || (r[k] = [])).push({ id: t.id, name: t.name });
-}
-for (k in r) {
-	if (!r.hasOwnProperty(k)) continue;
-	t = swa.itemByID((o = (a = r[k])[0]).id);
-	for (i = a.length; --i; swa.itemByID(a[i].id).remove(t));
-	if (k == o.name) continue; // No need to rename
-	try { t.name = k } catch (_) {} // Prevent read-only errors
-}
+// const __ = $.global.localize;
+// const CM_PROCESS = +ColorModel.PROCESS;
+// const CS_CMYK = +ColorSpace.CMYK;
+// var swa, a, r, o, t, k, i;
+// swa = doc.swatches;
+// a = doc.colors.everyItem().properties;
+// r = {};
+// while (o = a.shift()) { // Gather CMYK swatches => {CMYK_Key => {id, name}[]}
+// 	if (o.model != CM_PROCESS) continue;
+// 	if (o.space != CS_CMYK) continue;
+// 	t = swa.itemByName(o.name);
+// 	if (!t.isValid) continue;
+// 	if (t.name == "Safe area") continue;
+// 	for (i = (k = o.colorValue).length; i--; k[i] = Math.round(k[i]));
+// 	k = __("C=%1 M=%2 Y=%3 K=%4", k[0], k[1], k[2], k[3]);
+// 	(r[k] || (r[k] = [])).push({ id: t.id, name: t.name });
+// }
+// for (k in r) {
+// 	if (!r.hasOwnProperty(k)) continue;
+// 	t = swa.itemByID((o = (a = r[k])[0]).id);
+// 	for (i = a.length; --i; swa.itemByID(a[i].id).remove(t));
+// 	if (k == o.name) continue; // No need to rename
+// 	try { t.name = k } catch (_) {} // Prevent read-only errors
+// }
 
 // Step 4. Show 'guides' layer
 try { doc.layers.item("guides").visible = true } catch (_) {
