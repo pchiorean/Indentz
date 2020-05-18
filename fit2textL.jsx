1fdaa68f80a470ba219638cccedaa9a0a3b45766
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-18 14:48:42 +0300

    add left/center/right

--- fit2textL.jsx
+++ fit2textL.jsx
@@ -1,5 +1,5 @@
 /*
-	Fit frame to text, center v1.0.0
+	Fit frame to text, left v1.0.0
 	© May 2020, Paul Chiorean
 	This script auto-sizes the text frame to the content.
 */
@@ -10,10 +10,10 @@ var sel = doc.selection;
 if (sel.length > 0 && sel[0].constructor.name == "TextFrame") {
 	sel[0].fit(FitOptions.FRAME_TO_CONTENT);
 	sel[0].textFramePreferences.properties = {
-		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_CENTER_POINT,
+		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
 		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
 		useNoLineBreaksForAutoSizing: true
 	}
 	sel[0].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
-	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.CENTER_POINT;
+	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_LEFT_POINT;
 } else alert("Select a text frame and try again.");
--- fit2textR.jsx
+++ fit2textR.jsx
@@ -1,5 +1,5 @@
 /*
-	Fit frame to text, center v1.0.0
+	Fit frame to text, right v1.0.0
 	© May 2020, Paul Chiorean
 	This script auto-sizes the text frame to the content.
 */
@@ -10,10 +10,10 @@ var sel = doc.selection;
 if (sel.length > 0 && sel[0].constructor.name == "TextFrame") {
 	sel[0].fit(FitOptions.FRAME_TO_CONTENT);
 	sel[0].textFramePreferences.properties = {
-		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_CENTER_POINT,
+		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_RIGHT_POINT,
 		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
 		useNoLineBreaksForAutoSizing: true
 	}
 	sel[0].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
-	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.CENTER_POINT;
+	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_RIGHT_POINT;
 } else alert("Select a text frame and try again.");
