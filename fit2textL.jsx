429b5ee61e48aca8839b099f5d96ba08e0933db3
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-18 15:44:53 +0300

    fit all selected frames

--- fit2textL.jsx
+++ fit2textL.jsx
@@ -1,19 +1,21 @@
 /*
-	Fit frame to text, left v1.0.0
+	Fit frame to text, left v1.1.0
 	© May 2020, Paul Chiorean
 	This script auto-sizes the text frame to the content.
 */
 
 var doc = app.activeDocument;
-var sel = doc.selection;
+var selObj = doc.selection;
 
-if (sel.length > 0 && sel[0].constructor.name == "TextFrame") {
-	sel[0].fit(FitOptions.FRAME_TO_CONTENT);
-	sel[0].textFramePreferences.properties = {
-		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
-		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
-		useNoLineBreaksForAutoSizing: true
+if (selObj.length > 0 && selObj[0].constructor.name == "TextFrame") {
+	for (i = 0; i < selObj.length; i++) {
+		selObj[i].fit(FitOptions.FRAME_TO_CONTENT);
+		selObj[i].textFramePreferences.properties = {
+			autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
+			firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
+			useNoLineBreaksForAutoSizing: true
+		}
+		selObj[i].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
+		selObj[i].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_LEFT_POINT;
 	}
-	sel[0].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
-	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_LEFT_POINT;
 } else alert("Select a text frame and try again.");
--- fit2textR.jsx
+++ fit2textR.jsx
@@ -1,19 +1,18 @@
 /*
-	Fit frame to text, right v1.0.0
+	Fit frame to text, right v1.1.0
 	© May 2020, Paul Chiorean
 	This script auto-sizes the text frame to the content.
 */
 
-var doc = app.activeDocument;
-var sel = doc.selection;
-
-if (sel.length > 0 && sel[0].constructor.name == "TextFrame") {
-	sel[0].fit(FitOptions.FRAME_TO_CONTENT);
-	sel[0].textFramePreferences.properties = {
-		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_RIGHT_POINT,
-		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
-		useNoLineBreaksForAutoSizing: true
+if (selObj.length > 0 && selObj[0].constructor.name == "TextFrame") {
+	for (i = 0; i < selObj.length; i++) {
+		selObj[i].fit(FitOptions.FRAME_TO_CONTENT);
+		selObj[i].textFramePreferences.properties = {
+			autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_RIGHT_POINT,
+			firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
+			useNoLineBreaksForAutoSizing: true
+		}
+		selObj[i].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
+		selObj[i].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_RIGHT_POINT;
 	}
-	sel[0].textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
-	sel[0].textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_RIGHT_POINT;
 } else alert("Select a text frame and try again.");
