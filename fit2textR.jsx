fdc3df9e7644d03fcee174778b7333f505765346
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-19 19:57:02 +0300

    fix missing vars

--- fit2textR.jsx
+++ fit2textR.jsx
@@ -4,6 +4,9 @@
 	This script auto-sizes the text frame to the content.
 */
 
+var doc = app.activeDocument;
+var selObj = doc.selection;
+
 if (selObj.length > 0 && selObj[0].constructor.name == "TextFrame") {
 	for (i = 0; i < selObj.length; i++) {
 		selObj[i].fit(FitOptions.FRAME_TO_CONTENT);
