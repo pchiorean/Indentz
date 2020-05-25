620dd896a21555375b2be85d0c2b74979a43a63b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-25 23:01:59 +0300

    var. rename

--- page_ratios.jsx
+++ page_ratios.jsx
@@ -1,12 +1,12 @@
 /*
-	Page ratios v1.1.0
+	Page ratios v1.1.1
 	© May 2020, Paul Chiorean
 */
 
 var doc = app.activeDocument;
 var idLayerName = "id", idLayer = doc.layers.item(idLayerName);
 var ratioLayerName = "ratio", ratioLayer = doc.layers.item(ratioLayerName);
-var i, j, selPg, sizeMg, ratio, infoTF;
+var i, j, selPg, sizeMg, ratio, infoFrame;
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 
 if (!ratioLayer.isValid) doc.layers.add({ name: ratioLayerName });
@@ -18,10 +18,10 @@ for (i = 0; i < doc.pages.length; i++) {
 	selPg = doc.pages.item(i); sizeMg = bounds(selPg);
 	ratio = ((sizeMg[3] - sizeMg[1]) / (sizeMg[2] - sizeMg[0])).toFixed(3);
 	for (j = 0; j < selPg.pageItems.length; j++) if (selPg.pageItems.item(j).label == "ratio") selPg.pageItems.item(j).remove();
-	infoTF = selPg.textFrames.add({ itemLayer: ratioLayer.name, contents: ratio, label: "ratio", fillColor: "Black" });
-	infoTF.paragraphs.everyItem().properties = { appliedFont: app.fonts.item("Verdana"), fontStyle: "Bold", pointSize: 32, fillColor: "Paper" };
-	infoTF.fit(FitOptions.FRAME_TO_CONTENT);
-	infoTF.textFramePreferences.properties = {
+	infoFrame = selPg.textFrames.add({ itemLayer: ratioLayer.name, contents: ratio, label: "ratio", fillColor: "Black" });
+	infoFrame.paragraphs.everyItem().properties = { appliedFont: app.fonts.item("Verdana"), fontStyle: "Bold", pointSize: 32, fillColor: "Paper" };
+	infoFrame.fit(FitOptions.FRAME_TO_CONTENT);
+	infoFrame.textFramePreferences.properties = {
 		verticalJustification: VerticalJustification.CENTER_ALIGN,
 		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
 		autoSizingReferencePoint: AutoSizingReferenceEnum.TOP_CENTER_POINT,
@@ -29,8 +29,8 @@ for (i = 0; i < doc.pages.length; i++) {
 		useNoLineBreaksForAutoSizing: true,
 		insetSpacing: 4.25196850393701
 	}
-	doc.align(infoTF, AlignOptions.LEFT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
-	doc.align(infoTF, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+	doc.align(infoFrame, AlignOptions.LEFT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+	doc.align(infoFrame, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
 }
 ratioLayer.locked = true;
 
