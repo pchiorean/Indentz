029f04052c3d1d5e0e62c0aaa7102c6f3b35388b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-05-28 21:27:11 +0300

    first issue

--- misc/remove_labels.jsx
+++ misc/remove_labels.jsx
@@ -1,4 +1,17 @@
 var doc = app.activeDocument;
 var item, items = doc.allPageItems;
 
-while (item = items.shift()) item.label = "";
+while (item = items.shift()) {
+	switch (item.label) {
+		case "alignL":
+		case "alignR":
+		case "alignT":
+		case "alignB":
+		case "alignCh":
+		case "alignCv":
+		case "alignC":
+		case "bleed":
+		case "expand":
+			item.label = "";
+	}
+}
