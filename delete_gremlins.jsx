2f2cf49b8a44d8114b878963aaa4b65644432330
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-08 22:27:38 +0300

    turn off 'AutoUpdateURLStatus'

--- delete_gremlins.jsx
+++ delete_gremlins.jsx
@@ -1,12 +1,13 @@
 ﻿/*
-	Delete gremlins v1.5.0
+	Delete gremlins v1.6.0
 	© June 2020, Paul Chiorean
 	This script does some househeeping.
 */
 
+if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
 
-// Step 1. Default settings
+// Step 0. Initialisation
 doc.zeroPoint = [0, 0];
 doc.guidePreferences.guidesShown = true;
 doc.guidePreferences.guidesLocked = false;
@@ -22,30 +23,33 @@ doc.pageItemDefaults.fillColor = "None";
 doc.pageItemDefaults.strokeColor = "None";
 doc.selection = [];
 
-// Step 2. Delete unused swatches
+// Step 1. Delete unused swatches
 var i, swa; for (i = (swa = doc.unusedSwatches).length; i--; (swa[i].name != "") && swa[i].remove());
 
-// Step 3. Normalize similar CMYK swatches
-// try {
-// 	app.doScript(File(app.activeScript.path + "/normalize_swatches.jsx"), 
-// 	ScriptLanguage.javascript, null, UndoModes.FAST_ENTIRE_SCRIPT, "Normalize similar CMYK swatches")
-// } catch (_) {};
+// Step 2. Turn off 'AutoUpdateURLStatus' from 'Hyperlinks' panel
+var set_AUU = app.menuActions.itemByName("$ID/AutoUpdateURLStatus");
+var hyperLinksPanel = app.panels.itemByName("$ID/Hyperlinks");
+var flag_HLP = hyperLinksPanel.visible;
+if (!flag_HLP) hyperLinksPanel.visible = true;
+if (set_AUU.checked) set_AUU.invoke();
+hyperLinksPanel.visible = flag_HLP;
 
-// Step 4. Show 'guides' layer
+// Step 3. Show 'guides' layer
 try { doc.layers.item("guides").visible = true } catch (_) {
 	try { doc.layers.item("Guides").visible = true } catch (_) {};
 };
 
-// Step 5. Delete guides
+// Step 4. Delete guides
 try {
 	var i, guide;
-	for (i = (guide = doc.guides.everyItem().getElements()).length; i--; (guide[i].label != "HW") && guide[i].remove());
+	for (i = (guide = doc.guides.everyItem().getElements()).length; i--; 
+	(guide[i].label != "HW") && guide[i].remove());
 } catch (_) {};
 
-// Step 6. Delete unused layers
+// Step 5. Delete unused layers
 try { app.menuActions.item("$ID/Delete Unused Layers").invoke() } catch (_) {};
 
-// Step 7. Delete empty spreads
+// Step 6. Delete empty spreads
 for (var i = 0; i < doc.spreads.length; i++) {
 	if (doc.spreads[i].pageItems.length == 0 && doc.spreads.length > 1) doc.spreads[i].remove();
 }
