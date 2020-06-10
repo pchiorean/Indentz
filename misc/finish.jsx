b1333d131544f7900e78724fa918ad9e36d22188
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-10 10:06:53 +0300

    update for '6150383_LS_BC_FU_SEPT'

--- misc/finish.jsx
+++ misc/finish.jsx
@@ -1,44 +1,23 @@
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
-// var page = app.activeWindow.activePage;
-// var bleed = bleedBounds(page);
-// var item, items = doc.allPageItems;
+var page = app.activeWindow.activePage;
 
-// while (item = items.shift()) {
-// 	item.redefineScaling();
-// 	switch (item.label) {
-// 		case "bleed":
-// 		case "bg":
-// 			item.geometricBounds = bleed; break;
-// 		case "expand":
-// 		case "aw":
-// 			item.fit(FitOptions.FRAME_TO_CONTENT);
-// 			item.geometricBounds = [
-// 				Math.max(item.visibleBounds[0], bleed[0]),
-// 				Math.max(item.visibleBounds[1], bleed[1]),
-// 				Math.min(item.visibleBounds[2], bleed[2]),
-// 				Math.min(item.visibleBounds[3], bleed[3])
-// 			];
-// 			break;
-// 	}
-// }
+// ExpandItems();
 
-// Links
 TextRegColor();
-Relink("LS_RING_RED_770_CHF 3D.ai", "Price Tag/LS_RING_RED_790_CHF 3D.ai"); // 7.70 CHF => 7.90 CHF
-Relink("LS_RING_AMBER_770_CHF 3D.ai", "Price Tag/LS_RING_AMBER_790_CHF 3D.ai"); // 7.70 CHF => 7.90 CHF
-Relink("6150241_LS BC_FU_MAR_APR _2020_Original Image_982x737mm_v2_CMYK.tif", 
+
+Relink("LS_RING_RED_770_CHF 3D.ai", "Price Tag/LS_RING_RED_790_CHF 3D.ai");
+Relink("LS_RING_AMBER_770_CHF 3D.ai", "Price Tag/LS_RING_AMBER_790_CHF 3D.ai");
+Relink("6150241_LS BC_FU_MAR_APR _2020_Original Image_982x737mm_v2_CMYK.tif",
 	"6150241_LS BC_FU_MAR_APR_2020_Original Image_982x737mm_v2_CMYK.tif");
-Relink("6150241_LS BC_FU_MAR_APR _2020_Sky&Amber Image_982x737mm_v4_CMYK.tif", 
+Relink("6150241_LS BC_FU_MAR_APR _2020_Sky&Amber Image_982x737mm_v4_CMYK.tif",
 	"6150241_LS BC_FU_MAR_APR_2020_Sky&Amber Image_982x737mm_v4_CMYK.tif");
 
-// Swatches
-var i, swa; for (i = (swa = doc.unusedSwatches).length; i--; 
+var i, swa; for (i = (swa = doc.unusedSwatches).length; i--;
 	(swa[i].name != "" && swa[i].name != "C=60 M=40 Y=40 K=100") && swa[i].remove());
 try { doc.swatches.itemByName("SPOT LIGHT BLUE").remove() } catch (_) {};
 // try { doc.swatches.itemByName("Safe area").colorValue = [100, 0, 0, 0] } catch (_) {};
 
-// Layers
 try { doc.layers.itemByName("visible area").name = "safe area" } catch (_) {};
 try { doc.layers.itemByName("die cut").name = "dielines" } catch (_) {};
 try { doc.layers.itemByName("diecut").name = "dielines" } catch (_) {};
@@ -48,6 +27,29 @@ try { doc.layers.itemByName("safe area").visible = true } catch (_) {};
 try { doc.layers.itemByName("HW").properties = { visible: true, locked: true } } catch (_) {};
 try { doc.layers.itemByName("bg").properties = { visible: true, locked: true } } catch (_) {};
 
+
+// function ExpandItems() {
+// 	var bleed = bleedBounds(page);
+// 	var item, items = doc.allPageItems;
+// 	while (item = items.shift()) {
+// 		item.redefineScaling();
+// 		switch (item.label) {
+// 			case "bleed":
+// 			case "bg":
+// 				item.geometricBounds = bleed; break;
+// 			case "expand":
+// 			case "aw":
+// 				item.fit(FitOptions.FRAME_TO_CONTENT);
+// 				item.geometricBounds = [
+// 					Math.max(item.visibleBounds[0], bleed[0]),
+// 					Math.max(item.visibleBounds[1], bleed[1]),
+// 					Math.min(item.visibleBounds[2], bleed[2]),
+// 					Math.min(item.visibleBounds[3], bleed[3])
+// 				];
+// 				break;
+// 		}
+// 	}
+// }
 
 // function BleedBounds(page) {
 // 	var bleed = {
@@ -67,15 +69,7 @@ try { doc.layers.itemByName("bg").properties = { visible: true, locked: true } }
 // 	return [m_y1, m_x1, m_y2, m_x2];
 // }
 
-function Relink(oldLink, newLink) {
-	for (var i = 0; i < doc.links.length; i++) {
-		var link = doc.links[i];
-		if (link.name == oldLink) link.relink(File(doc.filePath + "/Links/" + newLink));
-		// if (link.name == oldLink) link.relink(File(File(link.filePath).path + "/" + newLink));
-	}
-}
-
-function TextRegColor(){ // Registration text => 100/100/100/100
+function TextRegColor(){
 	try { doc.colors.add({
 		name: "Reg. black",
 		model: ColorModel.PROCESS,
@@ -91,4 +85,12 @@ function TextRegColor(){ // Registration text => 100/100/100/100
 	app.changeTextPreferences.fillColor = "Reg. black";
 	doc.changeText();
 	app.findTextPreferences = app.changeTextPreferences = NothingEnum.nothing;
+}
+
+function Relink(oldLink, newLink) {
+	for (var i = 0; i < doc.links.length; i++) {
+		var link = doc.links[i];
+		if (link.name == oldLink) link.relink(File(doc.filePath + "/Links/" + newLink));
+		// if (link.name == oldLink) link.relink(File(File(link.filePath).path + "/" + newLink));
+	}
 }
