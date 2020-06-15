05fe1e452f1b7f385d73ed4b1dc2eae4336e17cd
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-15 22:30:02 +0300

    update step 0

--- delete_gremlins.jsx
+++ delete_gremlins.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-	Delete gremlins v1.6.1
+	Delete gremlins v1.7.0
 	© June 2020, Paul Chiorean
 	This script does some househeeping.
 */
@@ -8,17 +8,38 @@ if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
 
 // Step 0. Initialisation
+app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
+app.scriptPreferences.enableRedraw = false;
 doc.zeroPoint = [0, 0];
+try { doc.cmykProfile = "ISO Coated v2 (ECI)" } catch (_) { doc.cmykProfile = "Coated FOGRA39 (ISO 12647-2:2004)" };
+doc.rgbProfile = "sRGB IEC61966-2.1";
 doc.guidePreferences.guidesShown = true;
 doc.guidePreferences.guidesLocked = false;
 doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
 doc.viewPreferences.showFrameEdges = true;
 doc.viewPreferences.showRulers = true;
+doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
+doc.pasteboardPreferences.previewBackgroundColor = UIColors.LIGHT_GRAY;
+doc.viewPreferences.cursorKeyIncrement = "0.2mm";
+doc.textPreferences.leadingKeyIncrement = "0.5pt";
+doc.textPreferences.kerningKeyIncrement = 5;
+doc.textPreferences.baselineShiftKeyIncrement = "0.1pt";
+doc.textPreferences.typographersQuotes = true;
+doc.textPreferences.useParagraphLeading = true;
+doc.documentPreferences.intent = DocumentIntentOptions.PRINT_INTENT;
+doc.transparencyPreferences.blendingSpace = BlendingSpace.CMYK;
+app.transformPreferences.adjustStrokeWeightWhenScaling = true;
+app.transformPreferences.adjustEffectsWhenScaling = true;
+app.generalPreferences.includePreview = true;
+app.generalPreferences.preventSelectingLockedItems = true;
 app.generalPreferences.ungroupRemembersLayers = true;
 app.clipboardPreferences.pasteRemembersLayers = true;
+app.displayPerformancePreferences.persistLocalSettings = true;
 app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
+app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_TO_PAGE;
 app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_OFF;
+app.preflightOptions.preflightOff = true;
 doc.pageItemDefaults.fillColor = "None";
 doc.pageItemDefaults.strokeColor = "None";
 doc.selection = [];
