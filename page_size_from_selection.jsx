f585a5e657eec93af6521ee1efc98ed8b0e793e7
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-16 23:12:19 +0300

    fix #48

--- page_size_from_selection.jsx
+++ page_size_from_selection.jsx
@@ -1,5 +1,5 @@
 /*
-	Page size from selection v1.4.8
+	Page size from selection v1.5.0
 	© June 2020, Paul Chiorean
 	This script sets the page size to the selection bounds.
 */
@@ -12,54 +12,68 @@ doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
 
 var sel = doc.selection; // Save selection
 if (sel.length > 0 && sel[0].constructor.name != "Guide") {
-	var selObj = sel;
 	// Get selection's parent page
+	var selObj = sel;
 	var s = selObj[0].parent;
 	while (s.constructor.name != "Spread") s = s.parent;
-	var selPg = s.pages[0];
-	for (i = 0; i < selObj.length; i++) {
-		if (selObj[i].parentPage != null) { selPg = selObj[i].parentPage; break };
+	var page = s.pages[0];
+	for (var i = 0; i < selObj.length; i++) {
+		if (selObj[i].parentPage != null) { page = selObj[i].parentPage; break };
 	}
 	// If multiple selection, temporarily group it
 	var flagUngroup = false;
 	if (selObj.length > 1) {
 		var selObjArray = [], selObjLockedArray = [];
-		for (i = 0; i < selObj.length; i++) {
+		for (var i = 0; i < selObj.length; i++) {
 			// If locked, unlock and save index
 			if (selObj[i].locked) { selObj[i].locked = false; selObjLockedArray.push(i) };
 			selObjArray.push(selObj[i]);
 		}
-		selObj = selPg.groups.add(selObjArray); flagUngroup = true;
+		selObj = page.groups.add(selObjArray); flagUngroup = true;
 	} else {
 		selObj = selObj[0];
 	}
+	// Get selection bounds
+	var objBounds = selObj.visibleBounds;
+	for (var i = 1; i < selObj.getElements().length; i++) {
+		// Top-left corner
+		objBounds[0] = Math.min(selObj[i].visibleBounds[0], objBounds[0]);
+		objBounds[1] = Math.min(selObj[i].visibleBounds[1], objBounds[1]);
+		// Bottom-right corner
+		objBounds[2] = Math.max(selObj[i].visibleBounds[2], objBounds[2]);
+		objBounds[3] = Math.max(selObj[i].visibleBounds[3], objBounds[3]);
+	}
+	// Make temp rectangle
+	var mg = page.rectangles.add({
+		geometricBounds: objBounds,
+		contentType: ContentType.UNASSIGNED,
+		fillColor: "None", strokeColor: "None"
+	});
 	// Set margins to zero
-	selPg.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Page margins
-	doc.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Document margins
+	page.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Page mgs
+	doc.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Doc mgs
 	// Resize page
-	selPg.layoutRule = LayoutRuleOptions.OFF; // Don't scale page items
-	var selObjTL = selObj.resolve(AnchorPoint.TOP_LEFT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
-	var selObjBR = selObj.resolve(AnchorPoint.BOTTOM_RIGHT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
-	var sizePg = { width: (selObjBR[0] - selObjTL[0]), height: (selObjBR[1] - selObjTL[1]) };
-	selPg.reframe(CoordinateSpaces.SPREAD_COORDINATES, [selObjTL, selObjBR]);
+	page.layoutRule = LayoutRuleOptions.OFF; // Don't scale page items
+	var mg_TL = mg.resolve(AnchorPoint.TOP_LEFT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
+	var mg_BR = mg.resolve(AnchorPoint.BOTTOM_RIGHT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
+	page.reframe(CoordinateSpaces.SPREAD_COORDINATES, [mg_TL, mg_BR]);
+	mg.remove(); // Remove temp rectangle
 	// Ungroup and restore locked state
 	if (flagUngroup) {
 		selObj.ungroup();
-		for (i = 0; i < selObjLockedArray.length; i++) sel[selObjLockedArray[i]].locked = true;
+		for (var i = 0; i < selObjLockedArray.length; i++) sel[selObjLockedArray[i]].locked = true;
 	}
 	// Also set document size
+	var sizePg = { width: objBounds[3] - objBounds[1], height: objBounds[2] - objBounds[0] };
 	if (doc.pages.length == 1) {
 		try {
 			doc.documentPreferences.pageWidth = sizePg.width;
 			doc.documentPreferences.pageHeight = sizePg.height;
 		} catch (_) {
 			// Set master pages margins to zero
-			var masterPg;
-			for (s = 0; s < doc.masterSpreads.length; s++) {
-				masterPg = doc.masterSpreads[s].pages;
-				for (i = 0; i < masterPg.length; i++) {
-					masterPg[i].marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 };
-				}
+			for (var s = 0; s < doc.masterSpreads.length; s++) {
+				var masterPg = doc.masterSpreads[s].pages;
+				for (var i = 0; i < masterPg.length; i++) masterPg[i].marginPreferences.properties = [0, 0, 0, 0];
 			}
 			doc.documentPreferences.pageWidth = sizePg.width;
 			doc.documentPreferences.pageHeight = sizePg.height;
