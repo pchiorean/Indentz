9f44f4fa6e65d9990fd78965da4daecd18fdc0d5
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-18 19:46:23 +0300

    cosmetic changes

--- page_margins_from_selection.jsx
+++ page_margins_from_selection.jsx
@@ -1,51 +1,50 @@
 /*
-	Page margins from selection v1.0.0
+	Page margins from selection v1.0.1
 	© June 2020, Paul Chiorean
 	This script sets the page margins to the selection bounds.
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
-app.generalPreferences.objectsMoveWithPage = false;
-doc.adjustLayoutPreferences.enableAdjustLayout = false;
-doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
 
 var sel = doc.selection; // Save selection
-if (sel.length == 0 || (sel[0].constructor.name == "Guide")) { alert("Select an object and try again."); exit() };
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
+}
 // Get selection's parent page
 var selObj = sel, page;
-for (var i = 0; i < selObj.length; i++) if (selObj[i].parentPage != null) { page = selObj[i].parentPage; break };
+for (var i = 0; i < selObj.length; i++) {
+	if (selObj[i].parentPage != null) { page = selObj[i].parentPage; break };
+}
 if (page == null) { alert("Select an object on page and try again."); exit() };
 // Get selection dimensions
-var flagUngroup = false;
+var ungroup = false;
 if (selObj.length > 1) { // If multiple selection, temporarily group it
-	var selObjArray = [], selObjLockedArray = [];
+	var objArr = [], lockedArr = [];
 	for (var i = 0; i < selObj.length; i++) { // If locked, unlock and save index
-		if (selObj[i].locked) { selObj[i].locked = false; selObjLockedArray.push(i) };
-		selObjArray.push(selObj[i]);
+		if (selObj[i].locked) { selObj[i].locked = false; lockedArr.push(i) };
+		objArr.push(selObj[i]);
 	}
-	selObj = page.groups.add(selObjArray); flagUngroup = true;
+	selObj = page.groups.add(objArr); ungroup = true;
 } else selObj = selObj[0];
-var objBounds = selObj.visibleBounds;
+var size = selObj.visibleBounds;
 for (var i = 1; i < selObj.getElements().length; i++) { // Iterate selection, get extremities
-	// Top-left corner
-	objBounds[0] = Math.min(selObj[i].visibleBounds[0], objBounds[0]);
-	objBounds[1] = Math.min(selObj[i].visibleBounds[1], objBounds[1]);
-	// Bottom-right corner
-	objBounds[2] = Math.max(selObj[i].visibleBounds[2], objBounds[2]);
-	objBounds[3] = Math.max(selObj[i].visibleBounds[3], objBounds[3]);
+	size[0] = Math.min(selObj[i].visibleBounds[0], size[0]);
+	size[1] = Math.min(selObj[i].visibleBounds[1], size[1]);
+	size[2] = Math.max(selObj[i].visibleBounds[2], size[2]);
+	size[3] = Math.max(selObj[i].visibleBounds[3], size[3]);
 }
-if (flagUngroup) { // Ungroup and restore locked state
+if (ungroup) { // Ungroup and restore locked state
 	selObj.ungroup();
-	for (var i = 0; i < selObjLockedArray.length; i++) sel[selObjLockedArray[i]].locked = true;
+	for (var i = 0; i < lockedArr.length; i++) sel[lockedArr[i]].locked = true;
 }
 // Set page margins
 page.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 };
-var mgPg = {
-	top: (objBounds[0] - page.bounds[0]),
-	left: (objBounds[1] - page.bounds[1]),
-	bottom: (page.bounds[2] - objBounds[2]),
-	right: (page.bounds[3] - objBounds[3]),
-}
-page.marginPreferences.properties = mgPg;
-try { app.select(sel) } catch (_) {}; // Restore initial selection
+page.marginPreferences.properties = {
+	top: size[0] - page.bounds[0],
+	left: size[1] - page.bounds[1],
+	bottom: page.bounds[2] - size[2],
+	right: page.bounds[3] - size[3],
+};
+// Restore initial selection
+try { app.select(sel) } catch (_) {};
--- page_size_from_selection.jsx
+++ page_size_from_selection.jsx
@@ -1,5 +1,5 @@
 /*
-	Page size from selection v1.5.2
+	Page size from selection v1.5.3
 	© June 2020, Paul Chiorean
 	This script sets the page size to the selection bounds.
 */
@@ -11,37 +11,38 @@ doc.adjustLayoutPreferences.enableAdjustLayout = false;
 doc.adjustLayoutPreferences.enableAutoAdjustMargins = false;
 
 var sel = doc.selection; // Save selection
-if (sel.length == 0 || (sel[0].constructor.name == "Guide")) { alert("Select an object and try again."); exit() };
-
-// Get selection's parent page
-var selObj = sel;
-var s = selObj[0].parent; while (s.constructor.name != "Spread") s = s.parent; var page = s.pages[0];
-for (var i = 0; i < selObj.length; i++) if (selObj[i].parentPage != null) { page = selObj[i].parentPage; break };
-// Get selection dimensions
-var flagUngroup = false;
-if (selObj.length > 1) { // If multiple selection, temporarily group it
-	var selObjArray = [], selObjLockedArray = [];
-	for (var i = 0; i < selObj.length; i++) { // If locked, unlock and save index
-		if (selObj[i].locked) { selObj[i].locked = false; selObjLockedArray.push(i) };
-		selObjArray.push(selObj[i]);
-	}
-	selObj = page.groups.add(selObjArray); flagUngroup = true;
-} else selObj = selObj[0];
-var objBounds = selObj.visibleBounds;
-for (var i = 1; i < selObj.getElements().length; i++) { // Iterate selection, get extremities
-	// Top-left corner
-	objBounds[0] = Math.min(selObj[i].visibleBounds[0], objBounds[0]);
-	objBounds[1] = Math.min(selObj[i].visibleBounds[1], objBounds[1]);
-	// Bottom-right corner
-	objBounds[2] = Math.max(selObj[i].visibleBounds[2], objBounds[2]);
-	objBounds[3] = Math.max(selObj[i].visibleBounds[3], objBounds[3]);
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
 }
-if (flagUngroup) { // Ungroup and restore locked state
-	try { selObj.ungroup(); flagUngroup = false } catch (_) {};
-	for (var i = 0; i < selObjLockedArray.length; i++) sel[selObjLockedArray[i]].locked = true;
+// Get selection's parent page
+var selObj = sel; var s = selObj[0].parent;
+while (s.constructor.name != "Spread") s = s.parent; var page = s.pages[0];
+for (var i = 0; i < selObj.length; i++) {
+	if (selObj[i].parentPage != null) { page = selObj[i].parentPage; break };
+}
+// Get selection dimensions
+var ungroup = false;
+if (selObj.length > 1) { // If multiple selection, temporarily group it
+	var objArr = [], lockedArr = [];
+	for (var i = 0; i < selObj.length; i++) { // If locked, unlock and save index
+		if (selObj[i].locked) { selObj[i].locked = false; lockedArr.push(i) };
+		objArr.push(selObj[i]);
+	}
+	selObj = page.groups.add(objArr); ungroup = true;
+} else selObj = selObj[0];
+var size = selObj.visibleBounds;
+for (var i = 1; i < selObj.getElements().length; i++) { // Iterate selection, get extremities
+	size[0] = Math.min(selObj[i].visibleBounds[0], size[0]);
+	size[1] = Math.min(selObj[i].visibleBounds[1], size[1]);
+	size[2] = Math.max(selObj[i].visibleBounds[2], size[2]);
+	size[3] = Math.max(selObj[i].visibleBounds[3], size[3]);
+}
+if (ungroup) { // Ungroup and restore locked state
+	try { selObj.ungroup(); ungroup = false } catch (_) {};
+	for (var i = 0; i < lockedArr.length; i++) sel[lockedArr[i]].locked = true;
 }
 var mg = page.rectangles.add({ // Make temp rectangle and resolve TL-BR
-	geometricBounds: objBounds, contentType: ContentType.UNASSIGNED, fillColor: "None", strokeColor: "None"
+	geometricBounds: size, contentType: ContentType.UNASSIGNED, fillColor: "None", strokeColor: "None"
 });
 var mg_TL = mg.resolve(AnchorPoint.TOP_LEFT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
 var mg_BR = mg.resolve(AnchorPoint.BOTTOM_RIGHT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0];
@@ -51,9 +52,9 @@ page.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; //
 doc.marginPreferences.properties = { top: 0, left: 0, bottom: 0, right: 0 }; // Set doc mgs to zero
 page.layoutRule = LayoutRuleOptions.OFF; // Don't scale page items
 page.reframe(CoordinateSpaces.SPREAD_COORDINATES, [mg_TL, mg_BR]);
-if (flagUngroup) try { selObj.ungroup() } catch (_) {};
+if (ungroup) try { selObj.ungroup() } catch (_) {};
 // Also set document size
-var sizePg = { width: objBounds[3] - objBounds[1], height: objBounds[2] - objBounds[0] };
+var sizePg = { width: size[3] - size[1], height: size[2] - size[0] };
 if (doc.pages.length == 1) {
 	try {
 		doc.documentPreferences.pageWidth = sizePg.width;
