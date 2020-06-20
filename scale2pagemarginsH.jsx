ae17df791def114669246ae5b4a03648eed8f04b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-06-20 18:08:37 +0300

    get rid of main 'if' loops

--- scale2pagemarginsH.jsx
+++ scale2pagemarginsH.jsx
@@ -1,56 +1,53 @@
 /*
-	Scale to page margins (top/bottom) v1.4.9
+	Scale to page margins (top/bottom) v1.4.10
 	© June 2020, Paul Chiorean
 	This script scales the selected objects to the page top/bottom margins.
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
-var sel = doc.selection; // Save selection
 
-if (sel.length > 0 && sel[0].constructor.name != "Guide") {
-	// Get selection's parent page
-	var selPg;
-	for (i = 0; i < sel.length; i++) {
-		if (sel[i].parentPage != null) { selPg = doc.pages[sel[i].parentPage.documentOffset]; break };
-	}
-	if (selPg != null) {
-		// Remember layers for grouping/ungrouping
-		var set_uRL = app.generalPreferences.ungroupRemembersLayers;
-		var set_pRL = app.clipboardPreferences.pasteRemembersLayers;
-		app.generalPreferences.ungroupRemembersLayers = true;
-		app.clipboardPreferences.pasteRemembersLayers = true;
-		// Get selection dimensions
-		var selObj = sel;
-		var flagUngroup = false;
-		if (selObj.length > 1) { // If multiple selection, temporarily group it
-			var selObjArray = [];
-			for (i = 0; i < selObj.length; i++) {
-				if (!selObj[i].locked) selObjArray.push(selObj[i]);
-			}
-			selObj = selPg.groups.add(selObjArray); flagUngroup = true;
-		} else selObj = selObj[0];
-		var W_pg = selPg.bounds[3] - selPg.bounds[1];
-		var H_pg = selPg.bounds[2] - selPg.bounds[0];
-		var W_mg = W_pg - (selPg.marginPreferences.left + selPg.marginPreferences.right);
-		var H_mg = H_pg - (selPg.marginPreferences.top + selPg.marginPreferences.bottom);
-		var W_obj = selObj.visibleBounds[3] - selObj.visibleBounds[1];
-		var H_obj = selObj.visibleBounds[2] - selObj.visibleBounds[0];
-		// Compute scale factor
-		var objSF = H_mg / H_obj;
-		var matrix = app.transformationMatrices.add({
-			horizontalScaleFactor: objSF,
-			verticalScaleFactor: objSF
-		});
-		// Scale selection
-		selObj.transform(CoordinateSpaces.PASTEBOARD_COORDINATES, AnchorPoint.CENTER_ANCHOR, matrix);
-		doc.align(selObj, DistributeOptions.HORIZONTAL_CENTERS, AlignDistributeBounds.MARGIN_BOUNDS);
-		doc.align(selObj, DistributeOptions.VERTICAL_CENTERS, AlignDistributeBounds.MARGIN_BOUNDS);
-		// Ungroup and restore selection
-		if (flagUngroup) selObj.ungroup();
-		app.select(sel);
-		// Restore layer grouping settings
-		app.generalPreferences.ungroupRemembersLayers = set_uRL;
-		app.clipboardPreferences.pasteRemembersLayers = set_pRL;
-	} else alert("Please select an object not on pasteboard and try again.");
-} else alert("Please select an object and try again.");
+var sel = doc.selection;
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
+}
+// Get selection's parent page
+var page;
+for (var i = 0; i < sel.length; i++) {
+	if (sel[i].parentPage != null) { page = doc.pages[sel[i].parentPage.documentOffset]; break };
+}
+if (page == null) { alert("Select an object on page and try again."); exit() };
+// Remember layers for grouping/ungrouping
+var set_URL = app.generalPreferences.ungroupRemembersLayers;
+var set_PRL = app.clipboardPreferences.pasteRemembersLayers;
+app.generalPreferences.ungroupRemembersLayers = true;
+app.clipboardPreferences.pasteRemembersLayers = true;
+// Get selection dimensions
+var selObj = sel;
+var ungroup = false;
+if (selObj.length > 1) { // If multiple selection, temporarily group it
+	var selObjArray = [];
+	for (var i = 0; i < selObj.length; i++) if (!selObj[i].locked) selObjArray.push(selObj[i]);
+	selObj = page.groups.add(selObjArray); ungroup = true;
+} else selObj = selObj[0];
+var pgW = page.bounds[3] - page.bounds[1];
+var pgH = page.bounds[2] - page.bounds[0];
+var mgW = pgW - (page.marginPreferences.left + page.marginPreferences.right);
+var mgH = pgH - (page.marginPreferences.top + page.marginPreferences.bottom);
+var objW = selObj.visibleBounds[3] - selObj.visibleBounds[1];
+var objH = selObj.visibleBounds[2] - selObj.visibleBounds[0];
+// Compute scale factor
+var objSF = mgH / objH;
+var matrix = app.transformationMatrices.add({
+	horizontalScaleFactor: objSF, verticalScaleFactor: objSF
+});
+// Scale selection
+selObj.transform(CoordinateSpaces.PASTEBOARD_COORDINATES, AnchorPoint.CENTER_ANCHOR, matrix);
+doc.align(selObj, DistributeOptions.HORIZONTAL_CENTERS, AlignDistributeBounds.MARGIN_BOUNDS);
+doc.align(selObj, DistributeOptions.VERTICAL_CENTERS, AlignDistributeBounds.MARGIN_BOUNDS);
+// Ungroup and restore selection
+if (ungroup) selObj.ungroup();
+app.select(sel);
+// Restore layer grouping settings
+app.generalPreferences.ungroupRemembersLayers = set_URL;
+app.clipboardPreferences.pasteRemembersLayers = set_PRL;
