ad8172aec89fe0333feb557ad05f365e25dc3934
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-07-04 14:56:33 +0300

    add info; add default alert height

--- misc/view_properties.jsx
+++ misc/view_properties.jsx
@@ -1,47 +1,44 @@
 /*
-Modified from https://github.com/grefel/indesignjs/blob/version2/Allgemeine_Skripte/
-EigenschaftenAnzeigen.jsx by Gregor Fellenz (grefel).
-The script shows all properties and methods of a selected object.
+	Show properties 1.2.0
+	July 2020, Paul Chiorean
+	The script shows all properties and methods of a selected object.
+
+	Modified from https://github.com/grefel/indesignjs/blob/version2/Allgemeine_Skripte/
+	EigenschaftenAnzeigen.jsx by Gregor Fellenz (grefel).
 */
 
-if (app.documents.length == 0) exit();
+if (app.documents.length > 0 && app.selection.length > 0) {
+	obj = app.selection[0];
+} else if (app.documents.length > 0) {
+	alert("Nothing is selected.\nThe properties and methods of the document are displayed.")
+	obj = app.documents[0];
+} else {
+	alert("Nothing is selected.\nThe properties and methods of the application are displayed.")
+	obj = app;
+}
+ShowProps(obj);
 
-var uIL = app.scriptPreferences.userInteractionLevel;
-app.scriptPreferences.userInteractionLevel = UserInteractionLevels.INTERACT_WITH_ALL;
 
-showProps();
-
-app.scriptPreferences.userInteractionLevel = uIL;
-
-
-function showProps() {
-	if (app.documents.length > 0 && app.selection.length > 0) {
-		object = app.selection[0];
-	} else if (app.documents.length > 0) {
-		alert("Nothing is selected.\nThe properties and methods of the document are displayed.")
-		object = app.documents[0];
-	} else {
-		alert("Nothing is selected.\nThe properties and methods of the application are displayed.")
-		object = app;
-	}
-
+function ShowProps(obj) {
 	var resultArray = [];
-	for (var i = 0; i < object.reflect.properties.length; i++) {
-		var property = object.reflect.properties[i];
+	for (var i = 0; i < obj.reflect.properties.length; i++) {
+		var property = obj.reflect.properties[i];
 		if (property.toString() === "__proto__" || property.toString() === "reflect" || property.toString() === "properties") continue;
-		try { result = object[property] } catch (e) { result = "N/A" };
-		if (result != null && result.constructor.name === "Array") resultArray.push(object.constructor.name + "." + property + " = [" + result + "]");
-		else resultArray.push(object.constructor.name + "." + property + " = " + result);
+		try { result = obj[property] } catch (e) { result = "N/A" };
+		if (result != null && result.constructor.name === "Array") resultArray.push(obj.constructor.name + "." + property + " = [" + result + "]");
+		else resultArray.push(obj.constructor.name + "." + property + " = " + result);
 	}
-	alert_scroll("Properties", resultArray.sort());
+	AlertScroll("Properties", resultArray.sort());
 }
 
-// Scrollable alert function by Peter Kahrel http://forums.adobe.com/message/2869250#2869250
-function alert_scroll(title, input) {
+// Scrollable alert function by Peter Kahrel
+// http://forums.adobe.com/message/2869250#2869250
+function AlertScroll(title, input) {
 	if (input instanceof Array) input = input.join("\r");
 	var w = new Window("dialog", title);
 	var list = w.add("edittext", undefined, input, { multiline: true, scrolling: true });
-	list.maximumSize.height = app.activeWindow.bounds[2] - 200;
+	// list.maximumSize.height = app.activeWindow.bounds[2] - 200;
+	list.maximumSize.height = app.documents.length > 0 ? app.activeWindow.bounds[2] - 200 : 900;
 	list.minimumSize.width = 650;
 	w.add("button", undefined, "Close", { name: "ok" });
 	w.show();
