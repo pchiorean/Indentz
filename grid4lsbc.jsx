18176570d0a95985d032e7de771616b29a5e412b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-07-05 12:19:16 +0300

    rounds margins; add XL

--- grid4lsbc.jsx
+++ grid4lsbc.jsx
@@ -1,67 +1,70 @@
 /*
-	LS BC grid 1.0.0
+	LS BC grid 1.1.0
 	© July 2020, Paul Chiorean
 	This script sets the page margins to the LS BC grid system.
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
+app.scriptPreferences.measurementUnit = MeasurementUnits.MILLIMETERS;
 
 doc.activeLayer = doc.layers.item(0);
 try { doc.layers.add({ name: "guides", layerColor: UIColors.MAGENTA }) } catch (_) {};
+try { doc.layers.itemByName("guides").visible = true } catch (_) {};
+try { doc.layers.itemByName("guides").locked = false } catch (_) {};
 
 for (var i = 0; i < doc.pages.length; i++) {
 	var szPg = {
 		width: doc.pages[i].bounds[3] - doc.pages[i].bounds[1],
 		height: doc.pages[i].bounds[2] - doc.pages[i].bounds[0]
 	}
-	switch (szPg.width / szPg.height <= 1) {
+	switch (szPg.width / szPg.height <= 1) { // Page orientation
 		case true: // Portrait
-			var logo = szPg.width * 0.12; // Logo = 12% of format height except HW
-			// var oring = logo * 1.16 // Outer ring = 116% of logo
-			var mg = logo * 0.35 // Margin = 35% of logo
-			var b = 4; // Bottom margin = 4 x mg
+			// Logo = 12% of format height except HW
+			// Outer ring = 116% of logo
+			// Margin = 35% of logo
+			// Bottom margin = 4 x margin
+			var mg = Math.ceil(szPg.width * 0.12 * 0.35);
+			var logo = mg * 2.857142857142857;
 			// Set margins
 			doc.pages[i].marginPreferences.properties = {
-				top: mg, left: mg, bottom: mg * b + szPg.height / 10, right: mg,
-				columnCount: 1, columnGutter: 0
-			}
-			// Vertical middle guide
+				top: mg, left: mg, bottom: mg * 4 + szPg.height / 10, right: mg,
+				columnCount: 1, columnGutter: 0 };
+			// Vertical middle guide for the ring
 			doc.pages[i].guides.add(undefined, {
 				itemLayer: "guides", guideColor: UIColors.GREEN,
 				orientation: HorizontalOrVertical.vertical,
-				location: szPg.width / 2
-			});
+				location: szPg.width / 2 });
 			break;
 		case false: // Landscape
-			var logo = szPg.height * 0.9 * 0.14; // Logo = 14% of format height except HW
-			// var oring = logo * 1.16 // Outer ring = 116% of logo
-			var mg = logo * 0.3 // Margin = 30% of logo
-			var b = 2; // Bottom margin = 2 x mg
+			// Logo = 14% of format height except HW
+			// Outer ring = 116% of logo
+			// Margin = 30% of logo
+			// Bottom margin = 2 x margin
+			if (szPg.width / szPg.height >= 3) { var k = 1.2 } else { var k = 1 }; // Xlandscape
+			var mg = Math.ceil(szPg.height * 0.9 * 0.14 * 0.3 * k);
+			var logo = mg * 3.333333333333333;
 			// Set margins
 			doc.pages[i].marginPreferences.properties = {
-				top: mg, left: mg, bottom: mg * b + szPg.height / 10, right: mg,
-				columnCount: 1, columnGutter: 0
-			}
-			// Horizontal middle guide
+				top: mg, left: mg, bottom: mg * 2 + szPg.height / 10, right: mg,
+				columnCount: 1, columnGutter: 0 };
+			// Horizontal middle guide for the ring
 			doc.pages[i].guides.add(undefined, {
 				itemLayer: "guides", guideColor: UIColors.GREEN,
 				orientation: HorizontalOrVertical.horizontal,
-				location: szPg.height * 0.9 / 2
-			});
+				location: szPg.height * 0.9 / 2 });
 			// Horizontal bottom guide for extreme formats
 			doc.pages[i].guides.add(undefined, {
 				itemLayer: "guides", guideColor: UIColors.GREEN,
 				orientation: HorizontalOrVertical.horizontal,
-				location: szPg.height * 0.9 - mg
-			});
+				location: szPg.height * 0.9 - mg });
 			break;
 	}
+	// Placeholder for logo
 	var logoFrame = doc.pages[i].ovals.add({
-		geometricBounds: [0, 0, logo, logo], 
-		contentType: ContentType.UNASSIGNED, 
-		fillColor: "Magenta", strokeColor: "None"
-	});
+		itemLayer: "guides", geometricBounds: [0, 0, logo, logo],
+		contentType: ContentType.UNASSIGNED,
+		fillColor: "Magenta", strokeColor: "None" });
 	logoFrame.transparencySettings.blendingSettings.opacity = 90;
 	doc.align(logoFrame, AlignOptions.RIGHT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
 	doc.align(logoFrame, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
