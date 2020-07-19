2e5c192442e20a773ad1b9b7ebdea74f33fa895c
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-07-19 23:30:15 +0300

    Add 'on-file' section (#71)

--- misc/QR.jsx
+++ misc/QR.jsx
@@ -1,33 +1,21 @@
 /*
-	QR code v0.1.0-alpha
+	QR code v1.0.0
 	© July 2020, Paul Chiorean
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
-var idLayerName = "id", idLayer = doc.layers.item(idLayerName);
-var infoLayerName = "info", infoLayer = doc.layers.item(infoLayerName);
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 
-if (!infoLayer.isValid) doc.layers.add({ name: infoLayerName });
-infoLayer.properties = {
-	layerColor: UIColors.CYAN,
-	visible: true,
-	locked: false,
-	printable: false
-};
-if (idLayer.isValid) infoLayer.move(LocationOptions.after, idLayer);
-else infoLayer.move(LocationOptions.AT_BEGINNING);
-
 var w = new Window("dialog");
-	w.text = "QR Code";
+	w.text = "Generate QR Code";
 	w.orientation = "column";
 	w.alignChildren = ["left","top"];
 	w.spacing = 10;
 	w.margins = 16;
 	w.st = w.add("statictext", undefined, undefined, { name: "st" });
 	w.st.text = "Enter QR code text:";
-var label = w.add('edittext { properties: { name: "label", enterKeySignalsOnChange: true }}');
+var label = w.add('edittext { properties: { name: "label", enterKeySignalsOnChange: true } }');
 	label.preferredSize.width = 400;
 	label.active = true;
 var flg_onfile = w.add("checkbox", undefined, undefined, { name: "flg_onfile" });
@@ -52,20 +40,96 @@ switch (flg_onfile.value) {
 }
 
 
+function MakeInfoLayer(doc) {
+	var idLayerName = "id", idLayer = doc.layers.item(idLayerName);
+	var infoLayerName = "info", infoLayer = doc.layers.item(infoLayerName);
+	if (!infoLayer.isValid) doc.layers.add({ name: infoLayerName });
+	infoLayer.properties = {
+		layerColor: UIColors.CYAN,
+		visible: true,
+		locked: false,
+		printable: true
+	};
+	if (idLayer.isValid) infoLayer.move(LocationOptions.after, idLayer);
+	else infoLayer.move(LocationOptions.AT_BEGINNING);
+	return infoLayer;
+}
+
+function QRFile() {
+	var file = File(doc.filePath + "/" + 
+		doc.name.substr(0, doc.name.lastIndexOf(".")) + "_QR.indd");
+	var fileq = app.documents.add();
+	var page = fileq.pages[0];
+	var infoLayer = MakeInfoLayer(fileq);
+	// QR label frame
+	var label = page.textFrames.add({
+		itemLayer: infoLayer.name,
+		contents: QRLabel,
+		label: "QR",
+		fillColor: "None"
+	});
+	label.paragraphs.everyItem().properties = {
+		appliedFont: app.fonts.item("Helvetica Neue\tRegular"),
+		pointSize: 5,
+		autoLeading: 100,
+		horizontalScale: 92,
+		tracking: -15,
+		capitalization: Capitalization.ALL_CAPS,
+		hyphenation: false,
+		fillColor: "Black"
+	}
+	label.geometricBounds = [0, 0, 16.4046459005573,56.6929133858268];
+	label.textFramePreferences.properties = {
+		verticalJustification: VerticalJustification.BOTTOM_ALIGN,
+		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
+		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
+		autoSizingType: AutoSizingTypeEnum.HEIGHT_ONLY,
+		useNoLineBreaksForAutoSizing: true,
+		insetSpacing: [2.83464566929134, 2.83464566929134, 0, 0]
+	}
+	// QR code frame
+	var code = page.rectangles.add({ itemLayer: infoLayer.name, label: "QR" });
+	code.absoluteRotationAngle = -90;
+	code.geometricBounds = [16.4046459005572,0,73.7007874015747,56.6929133858268];
+	code.createPlainTextQRCode(QRLabel);
+	code.frameFittingOptions.properties = {
+		fittingAlignment: AnchorPoint.CENTER_ANCHOR,
+		fittingOnEmptyFrame: EmptyFrameFittingOptions.PROPORTIONALLY,
+		bottomCrop: 4.97468772366082,
+		leftCrop: 4.57520063728848,
+		rightCrop: 4.57520063728848,
+		topCrop: 4.97468772366082
+	}
+	code.epss[0].localDisplaySetting = ViewDisplaySettings.HIGH_QUALITY;
+	var QR = page.groups.add([label, code]);
+	QR.absoluteRotationAngle = 90;
+	// Move in place
+	page.layoutRule = LayoutRuleOptions.OFF; // Don't scale page items
+	page.reframe(CoordinateSpaces.SPREAD_COORDINATES, [
+		QR.resolve(AnchorPoint.TOP_LEFT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0],
+		QR.resolve(AnchorPoint.BOTTOM_RIGHT_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES)[0]
+	]);
+	fileq.documentPreferences.pageWidth = page.bounds[3] - page.bounds[1];
+	fileq.documentPreferences.pageHeight = page.bounds[2] - page.bounds[0];
+	QR.ungroup();
+	fileq.save(file); // fileq.close();
+}
+
 function QRPage() {
+	var infoLayer = MakeInfoLayer(doc);
 	for (var i = 0; i < doc.pages.length; i++) {
 		var page = doc.pages.item(i);
 		// Delete old items
 		for (var j = 0; j < page.pageItems.length; j++)
 			if (page.pageItems.item(j).label == "QR") { page.pageItems.item(j).remove(); j-- };
-		// Add QR label frame
-		var QRLabelFrame = page.textFrames.add({
+		// QR label frame
+		var label = page.textFrames.add({
 			itemLayer: infoLayer.name,
 			contents: QRLabel,
 			label: "QR",
 			fillColor: "None"
 		});
-		QRLabelFrame.paragraphs.everyItem().properties = {
+		label.paragraphs.everyItem().properties = {
 			appliedFont: app.fonts.item("Helvetica Neue\tRegular"),
 			pointSize: 5,
 			autoLeading: 100,
@@ -75,11 +139,11 @@ function QRPage() {
 			hyphenation: false,
 			fillColor: "Black"
 		}
-		QRLabelFrame.geometricBounds = [
+		label.geometricBounds = [
 			0, page.bounds[1],
 			23.4912600737857, page.bounds[1] + 62.3622047244095
 		];
-		QRLabelFrame.textFramePreferences.properties = {
+		label.textFramePreferences.properties = {
 			verticalJustification: VerticalJustification.BOTTOM_ALIGN,
 			firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
 			autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_LEFT_POINT,
@@ -87,18 +151,19 @@ function QRPage() {
 			useNoLineBreaksForAutoSizing: true,
 			insetSpacing: [7.08661417322835, 7.08661417322835, 2.83464566929134, 0]
 		}
-		// Add QR code frame
-		var QRCodeFrame = page.rectangles.add({
+		// QR code frame
+		var code = page.rectangles.add({
 			itemLayer: infoLayer.name,
 			label: "QR",
 			fillColor: "Paper"
 		});
-		QRCodeFrame.geometricBounds = [
+		code.absoluteRotationAngle = -90;
+		code.geometricBounds = [
 			23.4912600737857, page.bounds[1] + 6.23622047244442,
 			56.94007897142, page.bounds[1] + 39.6850393700788
 		];
-		QRCodeFrame.createPlainTextQRCode(QRLabel);
-		QRCodeFrame.frameFittingOptions.properties = {
+		code.createPlainTextQRCode(QRLabel);
+		code.frameFittingOptions.properties = {
 			fittingAlignment: AnchorPoint.CENTER_ANCHOR,
 			fittingOnEmptyFrame: EmptyFrameFittingOptions.PROPORTIONALLY,
 			bottomCrop: 7.26313937552231,
@@ -106,18 +171,12 @@ function QRPage() {
 			rightCrop: 7.26313937552231,
 			topCrop: 7.26313937552231
 		}
-		QRCodeFrame.absoluteRotationAngle = -90;
-		QRCodeFrame.epss[0].localDisplaySetting = ViewDisplaySettings.HIGH_QUALITY;
-		var QR = page.groups.add([QRLabelFrame, QRCodeFrame]);
+		code.epss[0].localDisplaySetting = ViewDisplaySettings.HIGH_QUALITY;
+		// Move in place
+		var QR = page.groups.add([label, code]);
 		QR.absoluteRotationAngle = 90;
 		doc.align(QR, AlignOptions.LEFT_EDGES, AlignDistributeBounds.PAGE_BOUNDS);
 		doc.align(QR, AlignOptions.BOTTOM_EDGES, AlignDistributeBounds.PAGE_BOUNDS);
 		QR.ungroup();
 	}
-}
-
-function QRFile() {
-	var QRFile = File(doc.filePath + "/" + 
-		doc.name.substr(0, doc.name.lastIndexOf(".")) + "_QR.indd");
-	alert(QRFile);
 }
