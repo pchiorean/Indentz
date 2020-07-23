7fc6fc72ead88438e793c2fb503348f92ceb016d
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-07-23 10:35:59 +0300

    Compensate for FirstBaselineOffset

--- geometry/TextAutosizeR.jsx
+++ geometry/TextAutosizeR.jsx
@@ -1,47 +1,78 @@
 /*
-	Fit frame to text, right v1.4.2
+	Fit frame to text, right v1.5.0
 	© July 2020, Paul Chiorean
 	Auto-sizes the text frame to the content, right aligned.
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
-if (doc.selection.length == 0) exit();
-var sel = doc.selection;
+if (doc.selection.length == 0) { exit() } else { var sel = doc.selection };
 
 if (sel[0].hasOwnProperty("parentTextFrames")) var sel = sel[0].parentTextFrames;
 for (var i = 0; i < sel.length; i++) {
-	var obj = sel[i]; if (obj.constructor.name == "TextFrame") FitFrame2Text(obj);
+	var obj = sel[i]; if (obj.constructor.name == "TextFrame") FitFrame2Text(obj, "right");
 }
 
-
-function FitFrame2Text(sel) {
-	var centerBefore = sel.resolve(AnchorPoint.CENTER_ANCHOR,
-		CoordinateSpaces.SPREAD_COORDINATES);
-	var set_VJ = sel.textFramePreferences.verticalJustification;
-	sel.fit(FitOptions.FRAME_TO_CONTENT);
-	sel.textFramePreferences.properties = {
-		autoSizingReferencePoint: AutoSizingReferenceEnum.BOTTOM_RIGHT_POINT,
-		firstBaselineOffset: FirstBaseline.CAP_HEIGHT,
-		useNoLineBreaksForAutoSizing: true
+function FitFrame2Text(sel, align) {
+	switch (align) {
+		case "center":
+			var set_AS = AutoSizingReferenceEnum.CENTER_POINT;
+			var set_VJ = VerticalJustification.CENTER_ALIGN;
+			var set_PJ = Justification.CENTER_ALIGN;
+			break;
+		case "left":
+			var set_AS = AutoSizingReferenceEnum.TOP_LEFT_POINT;
+			var set_VJ = VerticalJustification.TOP_ALIGN;
+			var set_PJ = Justification.LEFT_ALIGN;
+			break;
+		case "right":
+			var set_AS = AutoSizingReferenceEnum.TOP_RIGHT_POINT;
+			var set_VJ = VerticalJustification.TOP_ALIGN;
+			var set_PJ = Justification.RIGHT_ALIGN;
+			break;
 	}
+	var set_oldAS = sel.textFramePreferences.autoSizingType;
+	var set_oldVJ = sel.textFramePreferences.verticalJustification;
+	switch (set_oldVJ) {
+		case 1785951334: // VerticalJustification.JUSTIFY_ALIGN
+			exit();
+		case 1953460256 || 1785951334: // VerticalJustification.TOP_ALIGN || VerticalJustification.JUSTIFY_ALIGN
+			var cBefore = sel.resolve(AnchorPoint.TOP_CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			sel.textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.TOP_CENTER_POINT;
+			sel.textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_ONLY;
+			sel.textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.BOTTOM_CENTER_POINT;
+			sel.textFramePreferences.firstBaselineOffset = FirstBaseline.CAP_HEIGHT;
+			sel.textFramePreferences.useNoLineBreaksForAutoSizing = true;
+			var cAfter = sel.resolve(AnchorPoint.TOP_CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			break;
+		case 1667591796: // VerticalJustification.CENTER_ALIGN
+			var cBefore = sel.resolve(AnchorPoint.CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			sel.textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.CENTER_POINT;
+			sel.textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_ONLY;
+			sel.textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.BOTTOM_CENTER_POINT;
+			sel.textFramePreferences.firstBaselineOffset = FirstBaseline.CAP_HEIGHT;
+			sel.textFramePreferences.useNoLineBreaksForAutoSizing = true;
+			var cAfter = sel.resolve(AnchorPoint.CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			break;
+		case 1651471469: // VerticalJustification.BOTTOM_ALIGN
+			var cBefore = sel.resolve(AnchorPoint.BOTTOM_CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			sel.textFramePreferences.autoSizingReferencePoint = AutoSizingReferenceEnum.BOTTOM_CENTER_POINT;
+			sel.textFramePreferences.autoSizingType = AutoSizingTypeEnum.HEIGHT_ONLY;
+			sel.textFramePreferences.firstBaselineOffset = FirstBaseline.CAP_HEIGHT;
+			sel.textFramePreferences.useNoLineBreaksForAutoSizing = true;
+			var cAfter = sel.resolve(AnchorPoint.BOTTOM_CENTER_ANCHOR, CoordinateSpaces.SPREAD_COORDINATES);
+			break;
+	}
+	sel.textFramePreferences.autoSizingType = set_oldAS;
 	var set_AST = AutoSizingTypeEnum.HEIGHT_AND_WIDTH;
 	if (sel.lines.length > 1) {
 		set_AST = sel.textFramePreferences.autoSizingType ==
-		AutoSizingTypeEnum.OFF ?
-		AutoSizingTypeEnum.HEIGHT_ONLY :
-		AutoSizingTypeEnum.HEIGHT_AND_WIDTH
-	};
+		AutoSizingTypeEnum.OFF ? AutoSizingTypeEnum.HEIGHT_ONLY : AutoSizingTypeEnum.HEIGHT_AND_WIDTH
+	}
 	sel.textFramePreferences.properties = {
 		autoSizingType: set_AST,
-		autoSizingReferencePoint: AutoSizingReferenceEnum.TOP_RIGHT_POINT,
-		verticalJustification: VerticalJustification.TOP_ALIGN
+		autoSizingReferencePoint: set_AS,
+		verticalJustification: set_oldVJ
 	}
-	sel.paragraphs.everyItem().justification = Justification.RIGHT_ALIGN;
-	var centerAfter = sel.resolve(AnchorPoint.RIGHT_CENTER_ANCHOR,
-		CoordinateSpaces.SPREAD_COORDINATES);
-	if (set_VJ == VerticalJustification.CENTER_ALIGN)
-		sel.move(undefined, [0, centerBefore[0][1] - centerAfter[0][1]]);
-	if (set_VJ == VerticalJustification.BOTTOM_ALIGN)
-		sel.move(undefined, [0, (centerBefore[0][1] - centerAfter[0][1]) * 2 ]);
+	sel.paragraphs.everyItem().justification = set_PJ;
 }
