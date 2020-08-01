d23f6e8dc9a9bb23fa8a18a505abddf20ac9b762
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-08-01 10:19:10 +0300

    Add extreme formats

--- misc/GridE.jsx
+++ misc/GridE.jsx
@@ -1,5 +1,5 @@
 /*
-	E grid 1.0.1
+	E grid 1.1.0
 	© July 2020, Paul Chiorean
 */
 
@@ -11,10 +11,25 @@ for (var i = 0; i < doc.pages.length; i++) {
 		width: doc.pages[i].bounds[3] - doc.pages[i].bounds[1],
 		height: doc.pages[i].bounds[2] - doc.pages[i].bounds[0]
 	}
-	var mg = szPg.width * 0.1, cols = 6; // Default: portrait
-	if (szPg.width / szPg.height > 1) { mg = mg / 2; cols = cols * 2 }; // Landscape
+	switch (szPg.width / szPg.height <= 1) {
+		case true: // Portrait
+			// Margin = 10% of page width
+			// Columns = 6, zero gutter
+			var mg = szPg.width * 0.1, cols = 6;
+			var kTB = kLR = 1;
+			break;
+		case false: // Landscape
+			// Margin = 5% of page width
+			// Columns = 12, zero gutter
+			var mg = szPg.width * 0.1 / 2, cols = 12;
+			// Half margins for XL formats
+			var kTB = (szPg.width / szPg.height >= 3) ? 0.5 : 1;
+			var kLR = 1;
+			break;
+	}
+	// Set margins
 	doc.pages[i].marginPreferences.properties = {
-		top: mg, left: mg, bottom: mg + szPg.height / 10, right: mg,
+		top: mg * kTB, left: mg * kLR, bottom: mg * kTB + szPg.height / 10, right: mg * kLR,
 		columnCount: cols, columnGutter: 0
 	}
 }
