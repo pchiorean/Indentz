ac364a7c84458c9756d1610e6358e5a3843f2eba
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-08-11 19:26:21 +0300

    Reset 'Align To'

---
+++ align/ResetAlignTo.jsx
@@ -0,0 +1,8 @@
+/*
+	Reset 'Align To' v1.0.0
+	© August 2020, Paul Chiorean
+*/
+
+if (app.documents.length == 0) exit();
+
+app.alignDistributePreferences.alignDistributeBounds = AlignDistributeBounds.ITEM_BOUNDS;
