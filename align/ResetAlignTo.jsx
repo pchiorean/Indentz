aa9ff7e17683f97a96606bf24a06d9b276e593a8
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-08-27 14:56:15 +0300

    Reset selection key object (#91)

--- align/ResetAlignTo.jsx
+++ align/ResetAlignTo.jsx
@@ -1,8 +1,14 @@
 /*
-	Reset 'Align To' v1.0.0
+	Reset 'Align To' v1.1.0
 	© August 2020, Paul Chiorean
 */
 
 if (app.documents.length == 0) exit();
+
+if (app.selection != NothingEnum.NOTHING && app.selectionKeyObject != NothingEnum.NOTHING) {
+	var sel = app.selection;
+	app.selectionKeyObject = NothingEnum.NOTHING;
+	app.selection = sel;
+}
 
 app.alignDistributePreferences.alignDistributeBounds = AlignDistributeBounds.ITEM_BOUNDS;
