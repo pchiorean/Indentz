d361b61b418bfbc9dd09f7a0a4c8cda2f6468267
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-09-25 22:47:53 +0300

    Add comment support

--- cleanup/CleanupFonts.jsx
+++ cleanup/CleanupFonts.jsx
@@ -1,6 +1,6 @@
 ﻿/*
-	Cleanup fonts 1.1.1
-	© July 2020, Paul Chiorean
+	Cleanup fonts 1.2.0
+	© September 2020, Paul Chiorean
 	Replaces missing or unwanted fonts with equivalents from a list.
 	The list is a TSV file with the same name as the script, with the 
 	following format: <Old Name>\t<Style>\t<New Name>\t<Style>
@@ -16,6 +16,7 @@ if (!infoFile.open("r")) { alert("File " + infoFile.name + " not found."); exit(
 var fontList = [], line = 0;
 while (!infoFile.eof) {
 	var infoLine = infoFile.readln().split("\t"); line++;
+	if (infoLine[0].toString().slice(0,1) == "\u003B") continue; // Skip ';' commented lines
 	if (!infoLine[0] || !infoLine[1] || !infoLine[2] || !infoLine[3]) {
 		alert ("Missing data in record " + line + "."); exit();
 	}
@@ -26,8 +27,8 @@ while (!infoFile.eof) {
 }
 infoFile.close();
 
-app.findTextPreferences = app.changeTextPreferences = NothingEnum.NOTHING;
 for (var i = 1; i < fontList.length; i++) {
+	app.findTextPreferences = app.changeTextPreferences = NothingEnum.NOTHING;
 	app.findChangeTextOptions.includeHiddenLayers =
 	app.findChangeTextOptions.includeLockedLayersForFind =
 	app.findChangeTextOptions.includeLockedStoriesForFind =
@@ -35,5 +36,5 @@ for (var i = 1; i < fontList.length; i++) {
 	app.findTextPreferences.appliedFont = fontList[i][0];
 	app.changeTextPreferences.appliedFont = fontList[i][1];
 	app.activeDocument.changeText();
-	app.findTextPreferences = app.changeTextPreferences = NothingEnum.NOTHING;
 }
+app.findTextPreferences = app.changeTextPreferences = NothingEnum.NOTHING;
