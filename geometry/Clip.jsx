7455f82326da1d3e62f40030292574ba9e556fc2
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-09-29 09:59:26 +0300

    Selection is clipped as a group (#97)

--- geometry/Clip.jsx
+++ geometry/Clip.jsx
@@ -1,25 +1,52 @@
 /*
-	Clip v1.0.0
+	Clip v1.1.0
 	© September 2020, Paul Chiorean
 	Clip selected objects in a "\<clip frame\>", or restores them
 */
 
 if (app.documents.length == 0) exit();
-var items, obj;
+var doc = app.activeDocument;
 
-	var items, obj, page;
-	if ((items = app.activeDocument.selection).length > 0) {
-		while (obj = items.shift()) {
-			if (page = obj.parentPage) obj = Fit2Bounds(obj, page);
-		}
-	}
+var sel = doc.selection;
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
+}
+// Get selection's parent page
+var page;
+for (var i = 0; i < sel.length; i++) {
+	if (sel[i].parentPage != null) { page = doc.pages[sel[i].parentPage.documentOffset]; break };
+}
+if (page == null) { alert("Select an object on page and try again."); exit() };
+// Remember layers for grouping/ungrouping
+var set_URL = app.generalPreferences.ungroupRemembersLayers;
+var set_PRL = app.clipboardPreferences.pasteRemembersLayers;
+app.generalPreferences.ungroupRemembersLayers = true;
+app.clipboardPreferences.pasteRemembersLayers = true;
+
+if (sel.length > 1) { // If multiple selection, group it
+	var selArray = [];
+	for (var i = 0; i < sel.length; i++) if (!sel[i].locked) selArray.push(sel[i]);
+	sel = page.groups.add(selArray);
+	sel.name = "<clip group>";
+} else sel = sel[0];
+Fit2Bounds(sel, page);
+
+// Restore layer grouping settings
+app.generalPreferences.ungroupRemembersLayers = set_URL;
+app.clipboardPreferences.pasteRemembersLayers = set_PRL;
+
 
 function Fit2Bounds(obj, page) {
 	// Undo if already clipped
 	if (obj.name == "<clip frame>" && obj.pageItems[0].isValid) {
 		var objD = obj.pageItems[0].duplicate();
 		objD.label = obj.label;
-		objD.sendToBack(obj); obj.remove(); app.select(objD);
+		objD.sendToBack(obj); obj.remove(); obj = objD;
+		if (obj.name == "<clip group>" ||
+			(obj.constructor.name == "Rectangle" && obj.groups.length == 1)) {
+			var sel_BAK = obj.pageItems.everyItem().getElements();
+			obj.ungroup(); app.select(sel_BAK);
+		} else { app.select(obj) };
 		return obj;
 	}
 	// Compute bounds
