23d263bac1478ebc1347389130fb467c7911a5dd
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-09-29 13:10:51 +0300

    Cosmetic changes

--- misc/Clip.jsx
+++ misc/Clip.jsx
@@ -1,5 +1,5 @@
 /*
-	Clip v1.2.0
+	Clip v1.2.1
 	© September 2020, Paul Chiorean
 	Clip selected objects in a "\<clip frame\>", or restores them
 */
@@ -22,21 +22,20 @@ var set_URL = app.generalPreferences.ungroupRemembersLayers;
 var set_PRL = app.clipboardPreferences.pasteRemembersLayers;
 app.generalPreferences.ungroupRemembersLayers = true;
 app.clipboardPreferences.pasteRemembersLayers = true;
-
+// Do the clippin'
 if (sel.length > 1) { // If multiple selection, group it
 	var selArray = [];
-	for (var i = 0; i < sel.length; i++) if (!sel[i].locked) selArray.push(sel[i]);
+	for (var i = 0; i < sel.length; i++) if (!sel[i].locked) selArray.push(sel[i]); // Skip locked
 	sel = page.groups.add(selArray);
 	sel.name = "<clip group>";
 } else sel = sel[0];
-Fit2Bounds(sel, page);
-
+Clip2Bounds(sel, page);
 // Restore layer grouping settings
 app.generalPreferences.ungroupRemembersLayers = set_URL;
 app.clipboardPreferences.pasteRemembersLayers = set_PRL;
 
 
-function Fit2Bounds(obj, page) {
+function Clip2Bounds(obj, page) {
 	// Undo if already clipped
 	if (obj.name == "<clip frame>" && obj.pageItems[0].isValid) {
 		var objD = obj.pageItems[0].duplicate();
@@ -54,13 +53,13 @@ function Fit2Bounds(obj, page) {
 		} else { app.select(obj) };
 		return obj;
 	}
-	// Compute bounds
+	// Size is intersection between obj bounds and page bounds
 	var objBounds = obj.visibleBounds;
 	var size = [
-		objBounds[2] > page.bounds[0] ? Math.max(objBounds[0], page.bounds[0]) : objBounds[0], // top
-		objBounds[3] > page.bounds[1] ? Math.max(objBounds[1], page.bounds[1]) : objBounds[1], // left
-		objBounds[0] < page.bounds[2] ? Math.min(objBounds[2], page.bounds[2]) : objBounds[2], // bottom
-		objBounds[1] < page.bounds[3] ? Math.min(objBounds[3], page.bounds[3]) : objBounds[3]  // right
+		objBounds[2] > page.bounds[0] ? Math.max(objBounds[0], page.bounds[0]) : objBounds[0],
+		objBounds[3] > page.bounds[1] ? Math.max(objBounds[1], page.bounds[1]) : objBounds[1],
+		objBounds[0] < page.bounds[2] ? Math.min(objBounds[2], page.bounds[2]) : objBounds[2],
+		objBounds[1] < page.bounds[3] ? Math.min(objBounds[3], page.bounds[3]) : objBounds[3] 
 	];
 	// Clip!
 	var frame = page.rectangles.add(
