0fdbea6e7e69780cae6b00620cc886dd9ca4c10d
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-09-30 11:33:39 +0300

    Align objects together instead of individually (#101)

--- align/AlignToC.jsx
+++ align/AlignToC.jsx
@@ -1,29 +1,76 @@
 /*
-	Align to center v1.2.1
+	Align to center v2.0.0
 	© September 2020, Paul Chiorean
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
 
-var items = doc.selection;
-if (items.length == 0 || (items[0].constructor.name == "Guide")) exit();
-
-var set_ADB = app.alignDistributePreferences.alignDistributeBounds;
-if (doc.selectionKeyObject != undefined) set_ADB = AlignDistributeBounds.KEY_OBJECT;
-
-switch (SelectOption()) {
-	case 0:
-		doc.align(items, AlignOptions.HORIZONTAL_CENTERS, set_ADB, doc.selectionKeyObject);
-		break;
-	case 1:
-		doc.align(items, AlignOptions.VERTICAL_CENTERS, set_ADB, doc.selectionKeyObject);
-		break;
-	case 2:
-		doc.align(items, AlignOptions.HORIZONTAL_CENTERS, set_ADB, doc.selectionKeyObject);
-		doc.align(items, AlignOptions.VERTICAL_CENTERS, set_ADB, doc.selectionKeyObject);
-		break;
+var sel = doc.selection, selBAK, selKO, obj;
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
 }
+if (sel.length == 1 && sel[0].locked) {
+	alert("This object is locked."); exit();
+}
+
+// Get selection's parent page
+var page;
+for (var i = 0; i < sel.length; i++) {
+	if (sel[i].parentPage != null) { page = doc.pages[sel[i].parentPage.documentOffset]; break };
+}
+if (page == null) { alert("Select an object on page and try again."); exit() };
+
+// Remember layers for grouping/ungrouping
+var set_URL = app.generalPreferences.ungroupRemembersLayers;
+var set_PRL = app.clipboardPreferences.pasteRemembersLayers;
+app.generalPreferences.ungroupRemembersLayers = true;
+app.clipboardPreferences.pasteRemembersLayers = true;
+
+// Filter selection and get a unitary object
+if (sel.length > 1) { // Multiple objects? Group them
+	var objArray = [];
+	// selBAK = sel;
+	for (var i = 0; i < sel.length; i++) {
+		if (sel[i].locked) { alert("Locked objects will remain in place."); continue }
+		if (sel[i] === doc.selectionKeyObject) { var selKO = sel[i]; continue }
+		objArray.push(sel[i]);
+	}
+	if (objArray.length > 1) { // We *may* encounter an empty objArray
+		obj = page.groups.add(objArray);
+		obj.name = "<align group>";
+		selBAK = obj.pageItems.everyItem().getElements();
+	} else { obj = objArray[0]; selBAK = obj }
+} else { obj = sel[0]; selBAK = obj } // Single object
+
+// Align, ungroup and restore initial selection (sans key object)
+if (obj != undefined) {
+	var set_ADB = app.alignDistributePreferences.alignDistributeBounds;
+	if (selKO == undefined && obj.getElements().length >= 1 && set_ADB == "1416587604") {
+			alert("Align to what?");
+		} else {
+			if (selKO != undefined && obj.getElements().length > 1)
+				set_ADB = AlignDistributeBounds.KEY_OBJECT;
+			switch (SelectOption()) {
+				case 0:
+					doc.align(obj, AlignOptions.HORIZONTAL_CENTERS, set_ADB, selKO);
+					break;
+				case 1:
+					doc.align(obj, AlignOptions.VERTICAL_CENTERS, set_ADB, selKO);
+					break;
+				case 2:
+					doc.align(obj, AlignOptions.HORIZONTAL_CENTERS, set_ADB, selKO);
+					doc.align(obj, AlignOptions.VERTICAL_CENTERS, set_ADB, selKO);
+					break;
+			}
+		}
+	if (obj.name == "<align group>") obj.ungroup();
+	if(selBAK) app.select(selBAK);
+}
+
+// Restore layer grouping settings
+app.generalPreferences.ungroupRemembersLayers = set_URL;
+app.clipboardPreferences.pasteRemembersLayers = set_PRL;
 
 
 function SelectOption() {
--- align/AlignToR.jsx
+++ align/AlignToR.jsx
@@ -1,15 +1,60 @@
 /*
-	Align to right v1.1.0
-	© August 2020, Paul Chiorean
+	Align to right v2.0.0
+	© September 2020, Paul Chiorean
 */
 
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
 
-var items = doc.selection;
-if (items.length == 0 || (items[0].constructor.name == "Guide")) exit();
+var sel = doc.selection, selBAK, selKO, obj;
+if (sel.length == 0 || (sel[0].constructor.name == "Guide")) {
+	alert("Select an object and try again."); exit();
+}
+if (sel.length == 1 && sel[0].locked) {
+	alert("This object is locked."); exit();
+}
 
-var set_ADB = app.alignDistributePreferences.alignDistributeBounds;
-if (doc.selectionKeyObject != undefined) set_ADB = AlignDistributeBounds.KEY_OBJECT;
+// Get selection's parent page
+var page;
+for (var i = 0; i < sel.length; i++) {
+	if (sel[i].parentPage != null) { page = doc.pages[sel[i].parentPage.documentOffset]; break };
+}
+if (page == null) { alert("Select an object on page and try again."); exit() };
 
-doc.align(items, AlignOptions.RIGHT_EDGES, set_ADB, doc.selectionKeyObject);
+// Remember layers for grouping/ungrouping
+var set_URL = app.generalPreferences.ungroupRemembersLayers;
+var set_PRL = app.clipboardPreferences.pasteRemembersLayers;
+app.generalPreferences.ungroupRemembersLayers = true;
+app.clipboardPreferences.pasteRemembersLayers = true;
+
+// Filter selection and get a unitary object
+if (sel.length > 1) { // Multiple objects? Group them
+	var objArray = [];
+	// selBAK = sel;
+	for (var i = 0; i < sel.length; i++) {
+		if (sel[i].locked) { alert("Locked objects will remain in place."); continue }
+		if (sel[i] === doc.selectionKeyObject) { var selKO = sel[i]; continue }
+		objArray.push(sel[i]);
+	}
+	if (objArray.length > 1) { // We *may* encounter an empty objArray
+		obj = page.groups.add(objArray);
+		obj.name = "<align group>";
+		selBAK = obj.pageItems.everyItem().getElements();
+	} else { obj = objArray[0]; selBAK = obj }
+} else { obj = sel[0]; selBAK = obj } // Single object
+
+// Align, ungroup and restore initial selection (sans key object)
+if (obj != undefined) {
+	var set_ADB = app.alignDistributePreferences.alignDistributeBounds;
+	if (selKO == undefined && obj.getElements().length >= 1 &&
+		set_ADB == "1416587604") alert("Align to what?");
+	if (selKO != undefined && obj.getElements().length > 1)
+		set_ADB = AlignDistributeBounds.KEY_OBJECT;
+	doc.align(obj, AlignOptions.RIGHT_EDGES, set_ADB, selKO);
+	if (obj.name == "<align group>") obj.ungroup();
+	if(selBAK) app.select(selBAK);
+}
+
+// Restore layer grouping settings
+app.generalPreferences.ungroupRemembersLayers = set_URL;
+app.clipboardPreferences.pasteRemembersLayers = set_PRL;
