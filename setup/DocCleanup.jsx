bce1d3d057280e606cf3c7ef4905bcadfcf84fba
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-11-10 15:32:56 +0200

    Move prefs to new script

--- setup/DocCleanup.jsx
+++ setup/DocCleanup.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-	Doc cleanup v2.0.1
+	Doc cleanup v2.1.0
 	© November 2020, Paul Chiorean
 	Changes some settings, cleans up swatches/layers/pages/guides and resets scaling.
 */
@@ -7,48 +7,12 @@
 if (app.documents.length == 0) exit();
 var doc = app.activeDocument;
 
-// Step 0. Set preferences
-app.doScript(
-function() {
-	app.scriptPreferences.enableRedraw = false;
-	doc.zeroPoint = [0, 0];
-	try { doc.cmykProfile = "ISO Coated v2 (ECI)" } catch (_) { doc.cmykProfile = "Coated FOGRA39 (ISO 12647-2:2004)" };
-	doc.rgbProfile = "sRGB IEC61966-2.1";
-	doc.guidePreferences.guidesShown = true;
-	doc.guidePreferences.guidesLocked = false;
-	doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.showFrameEdges = true;
-	doc.viewPreferences.showRulers = true;
-	// doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
-	doc.pasteboardPreferences.previewBackgroundColor = UIColors.LIGHT_GRAY;
-	doc.viewPreferences.cursorKeyIncrement = "0.2mm";
-	doc.textPreferences.leadingKeyIncrement = "0.5pt";
-	doc.textPreferences.kerningKeyIncrement = 5;
-	doc.textPreferences.baselineShiftKeyIncrement = "0.1pt";
-	doc.textPreferences.typographersQuotes = true;
-	doc.textPreferences.useParagraphLeading = true;
-	doc.documentPreferences.intent = DocumentIntentOptions.PRINT_INTENT;
-	doc.transparencyPreferences.blendingSpace = BlendingSpace.CMYK;
-	app.transformPreferences.adjustStrokeWeightWhenScaling = true;
-	app.transformPreferences.adjustEffectsWhenScaling = true;
-	app.generalPreferences.includePreview = true;
-	app.generalPreferences.preventSelectingLockedItems = true;
-	app.generalPreferences.ungroupRemembersLayers = true;
-	app.clipboardPreferences.pasteRemembersLayers = true;
-	app.displayPerformancePreferences.persistLocalSettings = true;
-	app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_TO_PAGE;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_OFF;
-	app.preflightOptions.preflightOff = true;
-	doc.pageItemDefaults.transparencySettings.blendingSettings.blendMode = BlendMode.NORMAL;
-	doc.pageItemDefaults.properties = { fillColor: "None", strokeColor: "None" };
-	doc.selection = [];
-},
+// Set preferences
+app.doScript(File(app.activeScript.path + "/DefPrefs.jsx"), 
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set preferences");
 
-// Step 1. Delete unused swatches
+// Delete unused swatches
 app.doScript(
 function() {
 	var swa = doc.unusedSwatches;
@@ -57,7 +21,7 @@ function() {
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete unused swatches");
 
-// Step 2. Turn off 'AutoUpdateURLStatus' from 'Hyperlinks' panel
+// Turn off 'AutoUpdateURLStatus' from 'Hyperlinks' panel
 app.doScript(
 function() {
 	var set_AUU = app.menuActions.itemByName("$ID/AutoUpdateURLStatus");
@@ -70,7 +34,7 @@ function() {
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Turn off 'AutoUpdateURLStatus'");
 
-// Step 3. Show 'guides' layer
+// Show 'guides' layer
 app.doScript(
 function() {
 	try { doc.layers.item("guides").visible = true } catch (_) {
@@ -80,7 +44,7 @@ function() {
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Show 'guides' layer");
 
-// Step 4. Delete guides
+// Delete guides
 // app.doScript(
 // function() {
 // 	var g = doc.guides.everyItem().getElements();
@@ -89,7 +53,7 @@ UndoModes.ENTIRE_SCRIPT, "Show 'guides' layer");
 // ScriptLanguage.javascript, undefined,
 // UndoModes.ENTIRE_SCRIPT, "Delete guides");
 
-// Step 5. Delete unused layers
+// Delete unused layers
 app.doScript(
 function() {
 	try { app.menuActions.item("$ID/Delete Unused Layers").invoke() } catch (_) {};
@@ -97,7 +61,7 @@ function() {
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete unused layers");
 
-// Step 6. Delete empty spreads
+// Delete empty spreads
 app.doScript(
 function() {
 	var s = doc.spreads;
@@ -108,7 +72,7 @@ function() {
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete empty spreads");
 
-// Step 7. Unlock all items & redefine scaling to 100%
+// Unlock all items & redefine scaling to 100%
 app.doScript(
 function() {
 	for (var i = 0; i < doc.spreads.length; i++) {
--- setup/DocDefaults.jsx
+++ setup/DocDefaults.jsx
@@ -1,7 +1,7 @@
 ﻿/*
-	Default layers and more v3.0.0
+	Default layers and more v3.1.0
 	© November 2020, Paul Chiorean
-	Changes some settings, adds default swatches/layers, merges similar layers, 
+	Changes some settings, adds swatches, adds/merges layers,
 	cleans up fonts and sets page dimensions from the filename.
 */
 
@@ -10,46 +10,7 @@ var doc = app.activeDocument;
 
 
 // Set preferences
-app.doScript(
-function() {
-	app.scriptPreferences.enableRedraw = false;
-	doc.zeroPoint = [0, 0];
-	try { doc.cmykProfile = "ISO Coated v2 (ECI)" } catch (_) {
-		doc.cmykProfile = "Coated FOGRA39 (ISO 12647-2:2004)" };
-	doc.rgbProfile = "sRGB IEC61966-2.1";
-	doc.guidePreferences.guidesShown = true;
-	doc.guidePreferences.guidesLocked = false;
-	doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.showFrameEdges = true;
-	doc.viewPreferences.showRulers = true;
-	doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
-	doc.pasteboardPreferences.previewBackgroundColor = UIColors.LIGHT_GRAY;
-	doc.viewPreferences.cursorKeyIncrement = "0.2mm";
-	doc.textPreferences.leadingKeyIncrement = "0.5pt";
-	doc.textPreferences.kerningKeyIncrement = 5;
-	doc.textPreferences.baselineShiftKeyIncrement = "0.1pt";
-	doc.textPreferences.typographersQuotes = true;
-	doc.textPreferences.useParagraphLeading = true;
-	doc.documentPreferences.intent = DocumentIntentOptions.PRINT_INTENT;
-	doc.documentPreferences.allowPageShuffle = false;
-	doc.documentPreferences.preserveLayoutWhenShuffling = true;
-	doc.transparencyPreferences.blendingSpace = BlendingSpace.CMYK;
-	app.transformPreferences.adjustStrokeWeightWhenScaling = true;
-	app.transformPreferences.adjustEffectsWhenScaling = true;
-	app.generalPreferences.includePreview = true;
-	app.generalPreferences.preventSelectingLockedItems = true;
-	app.generalPreferences.ungroupRemembersLayers = true;
-	app.clipboardPreferences.pasteRemembersLayers = true;
-	app.displayPerformancePreferences.persistLocalSettings = true;
-	app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_TO_PAGE;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_OFF;
-	app.preflightOptions.preflightOff = true;
-	doc.pageItemDefaults.transparencySettings.blendingSettings.blendMode = BlendMode.NORMAL;
-	doc.pageItemDefaults.properties = { fillColor: "None", strokeColor: "None" };
-	doc.selection = [];
-},
+app.doScript(File(app.activeScript.path + "/DefPrefs.jsx"), 
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set preferences");
 
