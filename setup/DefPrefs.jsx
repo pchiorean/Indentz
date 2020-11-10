0414ac1a2988908e64c1703e2df38f15673248dd
pchiorean <61617801+pchiorean@users.noreply.github.com>
2020-11-10 15:33:07 +0200

    First commit

---
+++ setup/DefPrefs.jsx
@@ -0,0 +1,50 @@
+/*
+	Default preferences v1.0.0
+	© November 2020, Paul Chiorean
+*/
+
+// if (app.documents.length == 0) exit();
+var doc = app.activeDocument;
+
+app.doScript(
+function() {
+	app.scriptPreferences.enableRedraw = false;
+	doc.zeroPoint = [0, 0];
+	try { doc.cmykProfile = "ISO Coated v2 (ECI)" } catch (_) {
+		doc.cmykProfile = "Coated FOGRA39 (ISO 12647-2:2004)" };
+	doc.rgbProfile = "sRGB IEC61966-2.1";
+	doc.guidePreferences.guidesShown = true;
+	doc.guidePreferences.guidesLocked = false;
+	doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
+	doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
+	doc.viewPreferences.showFrameEdges = true;
+	doc.viewPreferences.showRulers = true;
+	doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
+	doc.pasteboardPreferences.previewBackgroundColor = UIColors.LIGHT_GRAY;
+	doc.viewPreferences.cursorKeyIncrement = "0.2mm";
+	doc.textPreferences.leadingKeyIncrement = "0.5pt";
+	doc.textPreferences.kerningKeyIncrement = 5;
+	doc.textPreferences.baselineShiftKeyIncrement = "0.1pt";
+	doc.textPreferences.typographersQuotes = true;
+	doc.textPreferences.useParagraphLeading = true;
+	doc.documentPreferences.intent = DocumentIntentOptions.PRINT_INTENT;
+	doc.documentPreferences.allowPageShuffle = false;
+	doc.documentPreferences.preserveLayoutWhenShuffling = true;
+	doc.transparencyPreferences.blendingSpace = BlendingSpace.CMYK;
+	app.transformPreferences.adjustStrokeWeightWhenScaling = true;
+	app.transformPreferences.adjustEffectsWhenScaling = true;
+	app.generalPreferences.includePreview = true;
+	app.generalPreferences.preventSelectingLockedItems = true;
+	app.generalPreferences.ungroupRemembersLayers = true;
+	app.clipboardPreferences.pasteRemembersLayers = true;
+	app.displayPerformancePreferences.persistLocalSettings = true;
+	app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
+	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_TO_PAGE;
+	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_OFF;
+	app.preflightOptions.preflightOff = true;
+	doc.pageItemDefaults.transparencySettings.blendingSettings.blendMode = BlendMode.NORMAL;
+	doc.pageItemDefaults.properties = { fillColor: "None", strokeColor: "None" };
+	doc.selection = [];
+},
+ScriptLanguage.javascript, undefined,
+UndoModes.ENTIRE_SCRIPT, "Set preferences");
