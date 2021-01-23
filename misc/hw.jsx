f1b42c59de4b200fffa9d7033ef1a6e87c70509b
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-01-23 17:08:12 +0200

    Update HW logic

--- misc/hw.jsx
+++ misc/hw.jsx
@@ -1,66 +1,59 @@
 /*
-	HW 1.0.0
+	HW 2.0.0
 	© January 2021, Paul Chiorean
-	Labels 'HW' selected objects; w/o selection, adds a 10% bottom guide.
+	Labels 'HW' selected objects and adds a HW bottom guide.
 */
 
 if (!(doc = app.activeDocument)) exit();
 var page = app.activeWindow.activePage;
+app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
+const HW_PCT = 10; // HW: percent of visible area
 
 app.doScript(main, ScriptLanguage.javascript, undefined,
 	UndoModes.ENTIRE_SCRIPT, "HW");
 
-
 function main() {
+	// Make HW layer
 	var hwLayerName = "HW";
 	var hwLayer = doc.layers.item(hwLayerName);
 	if (!hwLayer.isValid) {
 		doc.layers.add({ name: hwLayerName, layerColor: UIColors.LIGHT_GRAY });
 		hwLayer.move(LocationOptions.AT_BEGINNING) }
 	hwLayer.locked = false;
-
-	var sel = doc.selection;
-	if (sel.length >= 1) {
-		for (var i = 0; i < sel.length; i++) {
-			sel[i].label = "HW";
-			if (sel[i].constructor.name == "Rectangle" || sel[i].constructor.name == "TextFrame")
-				sel[i].fillColor = "Paper";
+	doc.activeLayer = hwLayer;
+	// Set selected objects' label and color
+	var item, items = doc.selection;
+	while (item = items.shift()) {
+		item.label = "HW";
+		if (item.constructor.name == "Rectangle" || item.constructor.name == "TextFrame")
+			item.fillColor = "Paper";
+	}
+	// Remove old HW guides
+	var guide, guides = page.guides.everyItem().getElements();
+	while (guide = guides.shift())
+		if (guide.label == "HW") guide.remove();
+	// Check if safe area is set
+	var saFrame, frames = page.rectangles.everyItem().getElements(), flg_SA = false;
+	while (saFrame = frames.shift())
+		if (saFrame.label == "safe area") { flg_SA = true; break };
+	// Check if E grid is set
+	var flg_E = ((page.marginPreferences.columnCount == 6 ||
+		page.marginPreferences.columnCount == 12) &&
+		page.marginPreferences.columnGutter == 0);
+	// Add HW guide
+	if (flg_SA) { // Safe area takes priority
+		var target = { top: saFrame.geometricBounds[0], bottom: saFrame.geometricBounds[2] }
+	} else { // Page height, considering margins (not if E grid)
+		var target = {
+			top: page.bounds[0] + (flg_E ? 0 : page.marginPreferences.top),
+			bottom: page.bounds[2] - (flg_E ? 0 : page.marginPreferences.bottom)
 		}
-	} else {
-		var szPg = page.bounds[2] - page.bounds[0];
-		var szMg = szPg - (page.marginPreferences.top + page.marginPreferences.bottom);
-		var j, guide;
-		for (j = (guide = page.guides.everyItem().getElements()).length; j--;
-			(guide[j].label == "HW") && guide[j].remove());
-		doc.activeLayer = hwLayer;
-		if (szMg != szPg) {
-			if (!((page.marginPreferences.columnCount == 6 ||
-				page.marginPreferences.columnCount == 12) &&
-				page.marginPreferences.columnGutter == 0))
-				AddMarginHWGuide();
-		}
-		AddPageHWGuide();
 	}
-
-	function AddPageHWGuide() {
-		page.guides.add(
-			undefined,
-			{ itemLayer: hwLayer,
-			label: "HW",
-			guideColor: UIColors.GREEN,
-			orientation: HorizontalOrVertical.horizontal,
-			location: page.bounds[0] + szPg * 0.9 }
-		);
-	}
-
-	function AddMarginHWGuide() {
-		page.guides.add(
-			undefined,
-			{ itemLayer: hwLayer,
-			label: "HW",
-			guideColor: UIColors.GREEN,
-			orientation: HorizontalOrVertical.horizontal,
-			location: page.bounds[0] + page.marginPreferences.top + szMg * 0.9 }
-		);
-	}
+	page.guides.add(undefined, {
+		itemLayer: hwLayer,
+		label: "HW",
+		guideColor: UIColors.GREEN,
+		orientation: HorizontalOrVertical.horizontal,
+		location: target.bottom - (target.bottom - target.top) * (Number(HW_PCT) / 100) }
+	);
 }
