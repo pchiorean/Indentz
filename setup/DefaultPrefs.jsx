3693f58e99a3c9ccbef857398f4dba65f4dc54dc
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-04-04 00:33:18 +0300

    Outline preferences for clarity

--- setup/DefaultPrefs.jsx
+++ setup/DefaultPrefs.jsx
@@ -1,50 +1,82 @@
 /*
-	Default preferences v1.1
-	© March 2021, Paul Chiorean
+	Default preferences v1.2
+	© April 2021, Paul Chiorean
 	Sets default preferences.
 */
 
 if (!(doc = app.activeDocument)) exit();
 
-app.doScript(
-function() {
-	doc.zeroPoint = [0, 0];
-	try { doc.cmykProfile = "ISO Coated v2 (ECI)" } catch (_) {
-		doc.cmykProfile = "Coated FOGRA39 (ISO 12647-2:2004)" };
-	doc.rgbProfile = "sRGB IEC61966-2.1";
-	doc.guidePreferences.guidesShown = true;
-	doc.guidePreferences.guidesLocked = false;
-	doc.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.verticalMeasurementUnits = MeasurementUnits.MILLIMETERS;
-	doc.viewPreferences.showFrameEdges = true;
-	doc.viewPreferences.showRulers = true;
-	// doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
-	doc.pasteboardPreferences.previewBackgroundColor = UIColors.LIGHT_GRAY;
-	doc.viewPreferences.cursorKeyIncrement = "0.2mm";
-	doc.textPreferences.leadingKeyIncrement = "0.5pt";
-	doc.textPreferences.kerningKeyIncrement = 5;
-	doc.textPreferences.baselineShiftKeyIncrement = "0.1pt";
-	doc.textPreferences.typographersQuotes = true;
-	doc.textPreferences.useParagraphLeading = true;
-	doc.documentPreferences.intent = DocumentIntentOptions.PRINT_INTENT;
-	doc.documentPreferences.allowPageShuffle = false;
-	doc.documentPreferences.preserveLayoutWhenShuffling = true;
-	doc.transparencyPreferences.blendingSpace = BlendingSpace.CMYK;
-	app.transformPreferences.adjustStrokeWeightWhenScaling = true;
-	app.transformPreferences.adjustEffectsWhenScaling = true;
-	app.generalPreferences.includePreview = true;
-	app.generalPreferences.preventSelectingLockedItems = true;
-	app.generalPreferences.ungroupRemembersLayers = true;
-	app.clipboardPreferences.pasteRemembersLayers = true;
-	app.displayPerformancePreferences.persistLocalSettings = true;
-	app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_TO_PAGE;
-	app.activeWindow.screenMode = ScreenModeOptions.PREVIEW_OFF;
-	app.preflightOptions.preflightOff = true;
-	doc.pageItemDefaults.transparencySettings.blendingSettings.blendMode = BlendMode.NORMAL;
-	doc.pageItemDefaults.transparencySettings.blendingSettings.opacity = 100;
-	doc.pageItemDefaults.properties = { fillColor: "None", strokeColor: "None" };
-	doc.selection = [];
+app.doScript(function() {
+	app.properties = {
+		activeWindow: {
+			screenMode: ScreenModeOptions.PREVIEW_OFF,
+			transformReferencePoint: AnchorPoint.CENTER_ANCHOR,
+		},
+		clipboardPreferences: {
+			pasteRemembersLayers: true,
+		},
+		displayPerformancePreferences: {
+			persistLocalSettings: true,
+		},
+		generalPreferences: {
+			includePreview: true,
+			preventSelectingLockedItems: true,
+			ungroupRemembersLayers: true,
+		},
+		preflightOptions: {
+			preflightOff: true,
+		},
+		transformPreferences: {
+			adjustEffectsWhenScaling: true,
+			adjustStrokeWeightWhenScaling: true,
+		},
+	}
+	doc.properties = {
+		cmykProfile: "ISO Coated v2 (ECI)",
+		documentPreferences: {
+			allowPageShuffle: false,
+			preserveLayoutWhenShuffling: true,
+			intent: DocumentIntentOptions.PRINT_INTENT,
+		},
+		guidePreferences: {
+			guidesLocked: false,
+			guidesShown: true,
+		},
+		pageItemDefaults: {
+			fillColor: "None",
+			strokeColor: "None",
+			transparencySettings: {
+				blendingSettings: {
+					blendMode: BlendMode.NORMAL,
+					opacity: 100,
+				}
+			}
+		},
+		pasteboardPreferences: {
+			// pasteboardMargins: ["150mm", "25mm"],
+			previewBackgroundColor: UIColors.LIGHT_GRAY,
+		},
+		rgbProfile: "sRGB IEC61966-2.1",
+		selection: [],
+		textPreferences: {
+			baselineShiftKeyIncrement: "0.1pt",
+			kerningKeyIncrement: 5,
+			leadingKeyIncrement: "0.5pt",
+			typographersQuotes: true,
+			useParagraphLeading: true,
+		},
+		transparencyPreferences: {
+			blendingSpace: BlendingSpace.CMYK,
+		},
+		viewPreferences: {
+			cursorKeyIncrement: "0.2mm",
+			horizontalMeasurementUnits: MeasurementUnits.MILLIMETERS,
+			verticalMeasurementUnits: MeasurementUnits.MILLIMETERS,
+			showFrameEdges: true,
+			showRulers: true,
+		},
+		zeroPoint: [0, 0],
+	}
 },
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set preferences");
