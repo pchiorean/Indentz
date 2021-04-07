cd1c8c66310d4b8e671dcb5e6ee04a60d07e05c8
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-04-07 23:15:17 +0300

    Add license

--- misc/QRBatch.jsx
+++ misc/QRBatch.jsx
@@ -1,6 +1,7 @@
 /*
-	Batch QR codes v2.0
-	© April 2021, Paul Chiorean
+	Batch QR codes v2.0 (2021-04-05)
+	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
+
 	Batch processes "QR.txt" to add codes to existing documents or to separate files.
 	The list is a 3-column TSV file with the following format:
 
@@ -11,6 +12,24 @@
 	1. <Filename>: document name,
 	2. <Code>: any string,
 	3. <On doc>: any string: on document; empty: separate file.
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 */
 
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
