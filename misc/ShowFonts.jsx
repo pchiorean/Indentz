b62dd70ce515980ee80e6fa11ed2800ee69bf6eb
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-04-08 16:40:16 +0300

    Update alert

--- misc/ShowFonts.jsx
+++ misc/ShowFonts.jsx
@@ -1,7 +1,11 @@
 /*
-	Show used fonts 1.1.3
-	© July 2020, Paul Chiorean
+	Show used fonts 1.2 (2021-04-08)
+	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
+
 	Shows all fonts used in the current document.
+
+	Released under MIT License:
+	https://choosealicense.com/licenses/mit/
 */
 
 if (app.documents.length == 0) exit();
@@ -10,21 +14,11 @@ var fontName1 = [], fontName2 = [], fontName3 = [], fontName4 = [], fontName5 = 
 
 for (var i = 0; i < usedFonts.length; i++) {
 	switch (String(usedFonts[i].status)) {
-		case "INSTALLED":
-			fontName1.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName);
-			break;
-		case "SUBSTITUTED":
-			fontName2.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName);
-			break;
-		case "FAUXED":
-			fontName3.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName);
-			break;
-		case "NOT_AVAILABLE":
-			fontName4.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName);
-			break;
-		case "UNKNOWN":
-			fontName5.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName);
-			break;
+		case "INSTALLED": fontName1.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName); break;
+		case "SUBSTITUTED": fontName2.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName); break;
+		case "FAUXED": fontName3.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName); break;
+		case "NOT_AVAILABLE": fontName4.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName); break;
+		case "UNKNOWN": fontName5.push(usedFonts[i].fontFamily + "\t" + usedFonts[i].fontStyleName); break;
 	}
 }
 
@@ -34,8 +28,22 @@ var fontList = "INSTALLED:\r" + fontName1.join("\n") +
 	"\r\rNOT_AVAILABLE:\r" + fontName4.join("\n") +
 	"\r\rUNKNOWN:\r" + fontName5.join("\n");
 
-var w = new Window('dialog { text: "Document fonts", alignChildren: "right" }');
-w.ed = w.add('edittext', [0, 0, 400, 600], fontList, { multiline: true });
-w.buttons = w.add('group');
-w.close = w.buttons.add('button { text: "Close", properties: { name: "cancel" } }');
-w.show();
+AlertScroll("Document fonts", fontList);
+
+
+// Modified from 'Scrollable alert' by Peter Kahrel
+// http://forums.adobe.com/message/2869250#2869250
+function AlertScroll(title, input) {
+	if (input instanceof Array) input = input.join("\r");
+	var lines = input.split(/\r|\n/g);
+	var w = new Window("dialog", title);
+	var list = w.add("edittext", undefined, input, { multiline: true, scrolling: true, readonly: true });
+	list.characters = (function() {
+		for (var i = 0, width = 50; i < lines.length; i++) width = Math.max(width, lines[i].length);
+		return width;
+	})();
+	list.minimumSize.height = 100;
+	list.maximumSize.height = 880;
+	w.add("button", undefined, "Close", { name: "ok" });
+	w.show();
+}
