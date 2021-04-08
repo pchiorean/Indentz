04d7d5831dd150187cb3ca22e9b26c14250f68e2
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-04-08 18:47:34 +0300

    Fix var name; update alert

--- misc/ShowProperties.jsx
+++ misc/ShowProperties.jsx
@@ -1,5 +1,5 @@
 /*
-	Show properties 1.3 (2021-03-29)
+	Show properties 1.3.1 (2021-04-08)
 	Paul Chiorean (jpeg@basement.ro)
 
 	Shows properties and methods of a selected object.
@@ -23,17 +23,20 @@ ShowProps(obj);
 function ShowProps(obj) {
 	var result = [];
 	// Object
-	result.push('\rOBJECT:\r');
+	result.push('OBJECT:\r');
 	result.push(obj.reflect.name);
 	result.push(obj.toSource());
 	// Properties
 	result.push('\rPROPERTIES:\r');
 	var props = obj.reflect.properties.sort();
 	for (var i = 0; i < props.length; i++) {
-		if (props[i].toString() === "__proto__" || props[i].toString() === "reflect" || props[i].toString() === "properties") continue;
-		try { var result = obj[props[i]] } catch (_) { var result = "N/A" };
-		if (result != null && result.constructor.name === "Array") result.push(props[i] + " = [" + result + "]");
-		else result.push(props[i] + " = " + result);
+		if (props[i].toString() === "__proto__" ||
+			props[i].toString() === "reflect" ||
+			props[i].toString() === "properties") continue;
+		try { var r = obj[props[i]] } catch (_) { var r = "N/A" }
+		if (r != null && r.constructor.name === "Array")
+			result.push(props[i] + " = [" + r + "]");
+		else result.push(props[i] + " = " + r);
 	}
 	// Methods
 	result.push('\rMETHODS:\r');
@@ -56,8 +59,8 @@ function AlertScroll(title, input) {
 		for (var i = 0, width = 50; i < lines.length; i++) width = Math.max(width, lines[i].length);
 		return width;
 	})();
-	list.minimumSize.height = 100;
-	list.maximumSize.height = 880;
+	list.minimumSize.width = 100; list.maximumSize.width = 1024;
+	list.minimumSize.height = 100; list.maximumSize.height = 1024;
 	w.add("button", undefined, "Close", { name: "ok" });
 	w.show();
 }
