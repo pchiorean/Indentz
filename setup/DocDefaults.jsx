cad03cf8cfcde1bfb8896a68a980314f420bd505
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-04-13 18:51:42 +0300

    Run finishing script (close #133)

--- setup/DocDefaults.jsx
+++ setup/DocDefaults.jsx
@@ -1,31 +1,37 @@
 ﻿if (!(doc = app.activeDocument)) exit();
 
 // Set preferences
-app.doScript(File(app.activeScript.path + "/DefaultPrefs.jsx"), 
+app.doScript(File(app.activeScript.path + "/DefaultPrefs.jsx"),
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set preferences");
 
 // Add default swatches
-app.doScript(File(app.activeScript.path + "/DefaultSwatches.jsx"), 
+app.doScript(File(app.activeScript.path + "/DefaultSwatches.jsx"),
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Default swatches");
 
 // Add default layers
-app.doScript(File(app.activeScript.path + "/DefaultLayers.jsx"), 
+app.doScript(File(app.activeScript.path + "/DefaultLayers.jsx"),
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Default layers");
 
 // Replace fonts
-app.doScript(File(app.activeScript.path + "/ReplaceFonts.jsx"), 
+app.doScript(File(app.activeScript.path + "/ReplaceFonts.jsx"),
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Replace fonts");
 
 // Set page dimensions from filename
-app.doScript(File(app.activeScript.path + "/PageSizeFromFilename.jsx"), 
+app.doScript(File(app.activeScript.path + "/PageSizeFromFilename.jsx"),
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set page dimensions");
 
 // Set pasteboard
-(function() {
-	doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"];
-})();
+app.doScript(function() { doc.pasteboardPreferences.pasteboardMargins = ["150mm", "25mm"] },
+ScriptLanguage.javascript, undefined,
+UndoModes.ENTIRE_SCRIPT, "Set page dimensions");
+
+try {
+	app.doScript(File(doc.filePath + "/_finish.jsx"),
+	ScriptLanguage.javascript, undefined,
+	UndoModes.ENTIRE_SCRIPT, "Run finishing script");
+} catch (_) {}
