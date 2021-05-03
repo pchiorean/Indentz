c7c6f5d1d5b787847219c4ca4492f7b69828a82a
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-05-03 23:03:58 +0300

    Fix offset for spreads

--- setup/GridE.jsx
+++ setup/GridE.jsx
@@ -1,5 +1,5 @@
 /*
-	E grid 2.0 (2021-04-14)
+	E grid 2.1 (2021-05-03)
 	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
 */
 
@@ -13,44 +13,43 @@ app.doScript(main, ScriptLanguage.javascript, undefined,
 function main() {
 	try { doc.layers.add({ name: "guides", layerColor: UIColors.MAGENTA }) } catch (_) {};
 	try { doc.layers.itemByName("guides").visible = true } catch (_) {};
+	try { doc.layers.itemByName("guides").printable = false } catch (_) {};
 	try { doc.layers.itemByName("guides").locked = false } catch (_) {};
 
 	for (var i = 0; i < doc.pages.length; i++) {
-		var tgBounds = Bounds(doc.pages[i]).page.visible || Bounds(doc.pages[i]).page.size;
-		var tgSize = {
-			width: tgBounds[3] - tgBounds[1],
-			height: tgBounds[2] - tgBounds[0]
-		}
+		var page = doc.pages[i];
+		var tgBounds = Bounds(page).page.visible || Bounds(page).page.size;
+		var tgSize = { width: tgBounds[3] - tgBounds[1], height: tgBounds[2] - tgBounds[0] };
 		switch (tgSize.width / tgSize.height <= 1) {
 			case true: // Portrait
 				// Margin = 10% of page width
 				// Columns = 6, zero gutter
-				var mg = tgSize.width * 0.1, cols = 6;
+				var MG = tgSize.width * 0.1, cols = 6;
 				var kTB = kLR = 1;
 				break;
 			case false: // Landscape
 				// Margin = 5% of page width
 				// Columns = 12, zero gutter
-				var mg = tgSize.width * 0.1 / 2, cols = 12;
+				var MG = tgSize.width * 0.1 / 2, cols = 12;
 				// Half margins for XL formats
 				var kTB = (tgSize.width / tgSize.height >= 2.5) ? 0.5 : 1;
 				var kLR = 1;
 				// Add a horizontal middle guide
-				doc.pages[i].guides.add(undefined, {
+				page.guides.add(undefined, {
 					itemLayer: "guides", label: "middle",
-					guideColor: UIColors.LIGHT_GRAY,
+					guideColor: UIColors.GRID_GREEN,
 					orientation: HorizontalOrVertical.horizontal,
-					location: (doc.pages[i].bounds[2] - doc.pages[i].bounds[0]) * 0.9 / 2
+					location: (page.bounds[2] - page.bounds[0]) * 0.9 / 2
 				});
 				break;
-		}
+		};
 		// Set margins
-		doc.pages[i].marginPreferences.properties = {
-			top: mg * kTB + tgBounds[0],
-			left: mg * kLR + tgBounds[1],
-			bottom: mg * kTB + tgSize.height / 10 + (doc.pages[i].bounds[2] - tgBounds[2]),
-			right: mg * kLR + (doc.pages[i].bounds[3] - tgBounds[3]),
+		page.marginPreferences.properties = {
+			top: (tgBounds[0] - page.bounds[0]) + MG * kTB,
+			left: (tgBounds[1] - page.bounds[1]) + MG * kLR,
+			bottom: (page.bounds[2] - tgBounds[2]) + MG * kTB + tgSize.height / 10,
+			right: (page.bounds[3] - tgBounds[3]) + MG * kLR,
 			columnCount: cols, columnGutter: 0
-		}
-	}
-}
+		};
+	};
+};
--- setup/GridL.jsx
+++ setup/GridL.jsx
@@ -1,5 +1,5 @@
 /*
-	L grid 2.2 (2021-04-20)
+	L grid 2.3 (2021-05-03)
 	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
 */
 
@@ -14,49 +14,26 @@ app.doScript(main, ScriptLanguage.javascript, undefined,
 function main() {
 	try { doc.layers.add({ name: "guides", layerColor: UIColors.MAGENTA }) } catch (_) {};
 	try { doc.layers.itemByName("guides").visible = true } catch (_) {};
+	try { doc.layers.itemByName("guides").printable = false } catch (_) {};
 	try { doc.layers.itemByName("guides").locked = false } catch (_) {};
 
 	for (var i = 0; i < doc.pages.length; i++) {
-		var tgBounds = Bounds(doc.pages[i]).page.visible || Bounds(doc.pages[i]).page.size;
-		var tgSize = {
-			width: tgBounds[3] - tgBounds[1],
-			height: tgBounds[2] - tgBounds[0]
-		}
-		switch (tgSize.width / tgSize.height < 1) {
+		var page = doc.pages[i];
+		var tgBounds = Bounds(page).page.visible || Bounds(page).page.size;
+		var tgSize = { width: tgBounds[3] - tgBounds[1], height: tgBounds[2] - tgBounds[0] };
+		switch (tgSize.width / tgSize.height < 0.95) {
 			case true: // Portrait
 				// Logo = 12% of format height except HW
 				// Outer ring = 116% of logo
 				// Margin = 35% of logo
 				// Bottom margin = 4 x margin
-				var mg = Math.ceil(tgSize.width * 0.12 * 0.35);
+				var MG = Math.ceil(tgSize.width * 0.12 * 0.35);
 				var bdist = 4;
-				var logo = mg * 2.857142857142857;
-				// Vertical middle guide for the ring
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides", label: "middle",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.vertical,
-					location: tgBounds[1] + tgSize.width / 2
-				});
-				// Additional guides for exceptions
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.horizontal,
-					location: tgBounds[2] - tgSize.height * 0.1 - mg
-				});
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.horizontal,
-					location: tgBounds[2] - tgSize.height * 0.1 - 2 * mg
-				});
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.horizontal,
-					location: tgBounds[2] - tgSize.height * 0.1 - 3 * mg
-				});
+				var logo = MG * 2.857142857142857;
+				// Guides
+				AddGuide("vertical", tgBounds[1] + tgSize.width / 2, "middle", UIColors.GRID_GREEN);
+				AddGuide("horizontal", tgBounds[2] - tgSize.height * 0.1 - 2 * MG, "2 x mg");
+				AddGuide("horizontal", tgBounds[2] - tgSize.height * 0.1 - 3 * MG, "3 x mg");
 				break;
 			case false: // Landscape
 				// Logo = 14% of format height except HW
@@ -64,62 +41,59 @@ function main() {
 				// Margin = 30% of logo
 				// Bottom margin = 2 x margin
 				var k = (tgSize.width / tgSize.height >= 3) ? 1.5 : 1; // XL formats
-				var mg = Math.ceil(tgSize.height * 0.9 * 0.14 * 0.3 * k);
+				var MG = Math.ceil(tgSize.height * 0.9 * 0.14 * 0.3 * k);
 				var bdist = 2;
-				var logo = mg * 3.333333333333333;
-				// Horizontal middle guide for the ring
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides", label: "middle",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.horizontal,
-					location: tgBounds[0] + tgSize.height * 0.9 / 2
-				});
-				// Additional guides for exceptions
-				doc.pages[i].guides.add(undefined, {
-					itemLayer: "guides",
-					guideColor: UIColors.LIGHT_GRAY,
-					orientation: HorizontalOrVertical.horizontal,
-					location: tgBounds[2] - tgSize.height * 0.1 - mg
-				});
+				var logo = MG * 3.333333333333333;
+				// Guides
+				AddGuide("horizontal", tgBounds[0] + tgSize.height * 0.9 / 2,
+					"middle", UIColors.GRID_GREEN);
 				break;
 		}
 		// Set margins
-		doc.pages[i].marginPreferences.properties = {
-			top: tgBounds[0] + mg,
-			left: tgBounds[1] + mg,
-			bottom: mg * bdist + tgSize.height / 10 + (doc.pages[i].bounds[2] - tgBounds[2]),
-			right: (doc.pages[i].bounds[3] - tgBounds[3]) + mg,
+		page.marginPreferences.properties = {
+			top: (tgBounds[0] - page.bounds[0]) + MG,
+			left: (tgBounds[1] - page.bounds[1]) + MG,
+			bottom: (page.bounds[2] - tgBounds[2]) + MG * bdist + tgSize.height / 10,
+			right: (page.bounds[3] - tgBounds[3]) + MG,
 			columnCount: 1, columnGutter: 0
 		};
-		// Guide for HL
-		doc.pages[i].guides.add(undefined, {
-			itemLayer: "guides", label: "HL (3 rows)",
-			guideColor: UIColors.GREEN,
-			orientation: HorizontalOrVertical.horizontal,
-			location: Bounds(doc.pages[i]).page.margins[2] -
-				(((2 * mg) * 1.18 - (2 * mg)) / 2 + // line inner margin
-				2 * ((2 * mg) * 0.28) + // space between lines
-				8 * mg) // 3 lines + #EMM
-		});
+		// Common guides
+		AddGuide("horizontal", tgBounds[2] - tgSize.height * 0.1 - MG, "mg");
+		AddGuide("horizontal",
+			Bounds(page).page.margins[2] -
+				(((2 * MG) * 1.18 - (2 * MG)) / 2 + // line inner margin
+				2 * ((2 * MG) * 0.28) + // space between lines
+				8 * MG), // 3 lines + #EMM
+			"HL (3 rows)", UIColors.GREEN);
 		// Placeholders for logo etc.
-		var logoFrame = doc.pages[i].ovals.add({
+		var logoFrame = page.ovals.add({
 			itemLayer: "guides", label: "logo",
-			geometricBounds: [0, 0, logo, logo],
+			geometricBounds: [ tgBounds[0], tgBounds[1], tgBounds[0] + logo, tgBounds[1] + logo ],
 			contentType: ContentType.UNASSIGNED,
 			fillColor: "Magenta", strokeColor: "None"
 		});
-		logoFrame.transparencySettings.blendingSettings.opacity = 90;
+		logoFrame.transparencySettings.blendingSettings.opacity = 66;
 		doc.align(logoFrame, AlignOptions.RIGHT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
 		doc.align(logoFrame, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
-		var mgFrame = doc.pages[i].rectangles.add({
+		var mgFrame = page.rectangles.add({
 			itemLayer: "guides", label: "mg",
-			geometricBounds: [0, 0, mg, mg],
+			geometricBounds: [ tgBounds[0], tgBounds[1], tgBounds[0] + MG, tgBounds[1] + MG ],
 			contentType: ContentType.UNASSIGNED,
 			fillColor: "Magenta", strokeColor: "None"
 		});
 		mgFrame.transparencySettings.blendingSettings.opacity = 90;
 		doc.align(mgFrame, AlignOptions.RIGHT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
 		doc.align(mgFrame, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
-		mgFrame.move(undefined, [ mg, -mg ]);
+		mgFrame.move(undefined, [ MG, -MG ]);
 	}
-}
+
+	function AddGuide(horv, location, label, color) {
+		page.guides.add(undefined, {
+			itemLayer: "guides", label: label,
+			guideColor: color || UIColors.LIGHT_GRAY,
+			orientation: horv == "horizontal" ?
+				HorizontalOrVertical.HORIZONTAL : HorizontalOrVertical.VERTICAL,
+			location: location
+		});
+	};
+};
