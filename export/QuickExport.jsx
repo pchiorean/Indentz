2e7f4ff849bc382901e586bbd2e0e346877fa2c4
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-05-17 23:33:28 +0300

    Add batch folder export if no files opened
    Also:
    - Fix links updating (typo)
    - Fix base name matching (unescaped chars in regex)
    - Save settings only if modified
    - Small tweaks to UI

--- export/QuickExport.jsx
+++ export/QuickExport.jsx
@@ -1,5 +1,5 @@
 /*
-	Quick export v1.2 (2021-05-16)
+	Quick export v1.3 (2021-05-17)
 	Paul Chiorean (jpeg@basement.ro)
 
 	Exports open documents with several configurable PDF presets.
@@ -26,7 +26,7 @@
 	SOFTWARE.
 */
 
-if (app.documents.length == 0) exit();
+if (app.documents.length == 0) var batchMode = true;
 
 var defaults = {
 	preset1: { name: "", suffix: "", active: true },
@@ -35,7 +35,7 @@ var defaults = {
 	script2: { file: "", active: false },
 	output: {
 		dest: { folder: "", active: false },
-		options: { subfolders: false, updatelinks: true, overwrite: false, save: true, close: true },
+		options: { subfolders: false, updatelinks: true, overwrite: false, save: true, close: true }
 	}
 };
 var settings;
@@ -56,6 +56,16 @@ var ui = new Window('dialog { text: "Quick Export", orientation: "column", \
 	alignChildren: [ "left", "top" ], spacing: 10, margins: 16 }');
 ui.main = ui.add('group { preferredSize: [ 500, -1 ], orientation: "column", \
 	alignChildren: [ "fill", "center" ], spacing: 10, margins: 0 }');
+// -- Batch mode
+if (batchMode) {
+	ui.input = ui.main.add('panel { text: "Input folder", orientation: "column", \
+		alignChildren: [ "left", "center" ] spacing: 10, margins: [ 10, 15, 10, 10 ] }');
+		ui.input.source = ui.input.add('group { orientation: "row", alignChildren: [ "left", "center" ], \
+			spacing: 10, margins: 0 }');
+			ui.input.source.folder = ui.input.source.add('edittext { preferredSize: [ 368, -1 ], \
+				properties: { readonly: true } }');
+			ui.input.source.browse = ui.input.source.add('button { text: "Browse", preferredSize: [ 100, 23 ] }');
+};
 // -- Export presets / scripts
 ui.presets = ui.main.add('panel { text: "Export presets", orientation: "column", \
 	alignChildren: [ "left", "center" ], spacing: 10, margins: [ 10, 15, 10, 10 ] }');
@@ -65,8 +75,8 @@ ui.presets = ui.main.add('panel { text: "Export presets", orientation: "column",
 		ui.presets.exp1.preset = ui.presets.exp1.add('dropdownlist', undefined, presets);
 		ui.presets.exp1.preset.preferredSize.width = 290;
 		ui.presets.exp1.add('statictext { text: "Suffix:", preferredSize: [ 40, -1 ], justify: "right" }');
-		ui.presets.exp1.suffix = ui.presets.exp1.add('edittext { helpTip: "Append to the file name", \
-			preferredSize: [ 100, -1 ] }');
+		ui.presets.exp1.suffix = ui.presets.exp1.add('edittext { preferredSize: [ 100, -1 ], \
+			helpTip: "Appended to the file name. Autodetected if the preset ends with \'_suffix\'" }');
 	ui.presets.exp1.script = ui.presets.add('group { orientation: "row", alignChildren: [ "left", "center" ], \
 		spacing: 10, margins: 0 }');
 		ui.presets.exp1.script.add('statictext { text: "Run a script:", preferredSize: [ 80, -1 ] }');
@@ -74,7 +84,7 @@ ui.presets = ui.main.add('panel { text: "Export presets", orientation: "column",
 		ui.presets.exp1.script.file = ui.presets.exp1.script.add('edittext { preferredSize: [ 250, -1 ], \
 			properties: { readonly: true } }');
 		ui.presets.exp1.script.browse = ui.presets.exp1.script.add('button { text: "Browse", \
-			preferredSize: [ 100, 22 ] }');
+			preferredSize: [ 100, 23 ] }');
 	ui.presets.add('panel { alignment: "fill" }');
 	ui.presets.exp2 = ui.presets.add('group { orientation: "row", \
 		alignChildren: [ "left", "center" ], spacing: 10, margins: 0 }');
@@ -83,7 +93,7 @@ ui.presets = ui.main.add('panel { text: "Export presets", orientation: "column",
 		ui.presets.exp2.preset.preferredSize.width = 290;
 		ui.presets.exp2.add('statictext { text: "Suffix:", preferredSize: [ 40, -1 ], justify: "right" }');
 		ui.presets.exp2.suffix = ui.presets.exp2.add('edittext { preferredSize: [ 100, -1 ], \
-			helpTip: "Append to the file name" }');
+			helpTip: "Appended to the file name. Autodetected if the preset ends with \'_suffix\'" }');
 	ui.presets.exp2.script = ui.presets.add('group { orientation: "row", alignChildren: [ "left", "center" ], \
 		spacing: 10, margins: 0 }');
 		ui.presets.exp2.script.add('statictext { text: "Run a script:", preferredSize: [ 80, -1 ] }');
@@ -91,7 +101,7 @@ ui.presets = ui.main.add('panel { text: "Export presets", orientation: "column",
 		ui.presets.exp2.script.file = ui.presets.exp2.script.add('edittext { preferredSize: [ 250, -1 ], \
 			properties: { readonly: true } }');
 		ui.presets.exp2.script.browse = ui.presets.exp2.script.add('button { text: "Browse", \
-			preferredSize: [ 100, 22 ] }');
+			preferredSize: [ 100, 23 ] }');
 // -- Output options
 ui.output = ui.main.add('panel { text: "Output folder and options", orientation: "column", \
 	alignChildren: [ "left", "center" ] spacing: 10, margins: [ 10, 15, 10, 10 ] }');
@@ -100,7 +110,7 @@ ui.output = ui.main.add('panel { text: "Output folder and options", orientation:
 		ui.output.dest.isActive = ui.output.dest.add('checkbox { alignment: "bottom" }');
 		ui.output.dest.folder = ui.output.dest.add('edittext { preferredSize: [ 340, -1 ], \
 			properties: { readonly: true } }');
-		ui.output.dest.browse = ui.output.dest.add('button { text: "Browse", preferredSize: [ 100, 22 ] }');
+		ui.output.dest.browse = ui.output.dest.add('button { text: "Browse", preferredSize: [ 100, 23 ] }');
 	ui.output.options = ui.output.add('group { orientation: "row", alignChildren: [ "left", "top" ], \
 		spacing: 0, margins: [ 0, 5, 0, 0 ] }');
 		ui.output.opt1 = ui.output.options.add('group { preferredSize: [ 200, -1 ], orientation: "column", \
@@ -113,6 +123,7 @@ ui.output = ui.main.add('panel { text: "Output folder and options", orientation:
 			spacing: 5, margins: 0 }');
 			ui.output.options.docSave = ui.output.opt2.add('checkbox { text: "Save and ..." }');
 			ui.output.options.docClose = ui.output.opt2.add('checkbox { text: "Close exported documents" }');
+			ui.output.options.docClose.enabled = !batchMode;
 // -- Actions
 ui.actions = ui.add('group { orientation: "row", alignChildren: [ "left", "center" ], spacing: 10, margins: 0, \
 	alignment: [ "right", "center" ] }');
@@ -181,20 +192,31 @@ ui.presets.exp2.preset.onChange = function() {
 };
 ui.presets.exp1.suffix.onChange = function() {
 	var str = this.text.replace(/^\s+/, '').replace(/\s+$/, ''); // Trim
-	str = str.replace(/[\/\\?|%*:"<>\[\]\.]/, ''); // Remove periods/illegal characters
+	str = str.replace(/[\/\\?|%*:"<>\[\]\.]/, ''); // Remove illegal characters
 	if (this.text != str) this.text = str;
 };
 ui.presets.exp2.suffix.onChange = function() {
 	var str = this.text.replace(/^\s+/, '').replace(/\s+$/, ''); // Trim
-	str = str.replace(/[\/\\?|%*:"<>\[\]\.]/, ''); // Remove periods/illegal characters
+	str = str.replace(/[\/\\?|%*:"<>\[\]\.]/, ''); // Remove illegal characters
 	if (this.text != str) this.text = str;
 };
 ui.output.dest.isActive.onClick = function() {
 	ui.output.dest.folder.enabled = ui.output.dest.browse.enabled = this.value;
 };
+if (batchMode) {
+	ui.input.source.browse.onClick = function() {
+		var newFolder = Folder.selectDialog("Select a folder:");
+		if (!!newFolder) {
+			ui.input.source.path = newFolder;
+			ui.input.source.folder.text = TruncatePath(decodeURI(newFolder.fullName), 50);
+			ui.input.source.folder.helpTip = decodeURI(newFolder.fullName);
+			var docs = ui.input.source.path.getFiles("*.indd");
+			ui.actions.ok.enabled = (docs.length > 0);
+		};
+	};
+};
 ui.output.dest.browse.onClick = function() {
 	var newFolder = Folder.selectDialog("Select a folder:");
-	// alert(ui.output.dest.folder.characters); // TODO
 	if (!!newFolder) {
 		ui.output.dest.path = newFolder;
 		ui.output.dest.folder.text = TruncatePath(decodeURI(newFolder.fullName), 50);
@@ -208,109 +230,132 @@ if (ADV) {
 	ui.actions.savePrefs.onClick = function() { if (this.value) SaveSettings() };
 };
 ui.onShow = function() { ReadSettings() };
-
-// Main loop
 if (ui.show() == 2) exit();
-if (ui.actions.savePrefs.value) SaveSettings();
-// Sort opened documents
-var doc, docs = app.documents.everyItem().getElements(), names = [];
-while (doc = docs.shift()) names.push(doc.name); names.sort();
-var name, docs = [], pbWidth = 50;
-while (name = names.shift()) {
-	docs.push(app.documents.itemByName(name));
-	pbWidth = Math.max(pbWidth, name.length); // While we are here, find the longest name
-};
-var progressBar = new ProgressBar("Exporting", pbWidth + 10);
-progressBar.reset(docs.length);
-var counter = 1, errors = [];
-var doc; while (doc = docs.shift()) {
-	if (!doc.saved) { errors.push(doc.name + " has no path. Skipped."); continue };
-	// Base folder
-	var baseFolder = decodeURI(doc.filePath);
-	if (ui.output.dest.isActive.value && !!ui.output.dest.path) {
-		if (!ui.output.dest.path.exists) ui.output.dest.path.create();
-		baseFolder = decodeURI(ui.output.dest.path.fullName);
+main();
+
+
+function main() {
+	if (ui.actions.savePrefs.value) SaveSettings();
+	var oldUIL = app.scriptPreferences.userInteractionLevel;
+	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
+	// Get source documents
+	if (batchMode) {
+		var docs = ui.input.source.path.getFiles("*.indd").sort();
+	} else {
+		var doc, docs = app.documents.everyItem().getElements(), names = [];
+		while (doc = docs.shift()) names.push(doc.name); names.sort();
+		var name, docs = [];
+		while (name = names.shift()) docs.push(app.documents.itemByName(name));
 	};
-	for (var i = 1; i < 3; i++) {
-		var exp = ui.presets["exp" + i];
-		if (!exp.isActive.value) continue;
-		var preset = app.pdfExportPresets.item(String(exp.preset.selection));
-		// Suffix
-		var suffix = !!exp.suffix.text ? ("_" + exp.suffix.text.replace(/^_/, "")) : "";
-		// Subfolder
-		var subfolder = "";
-		if (ui.output.options.subfolders.value && !!suffix) {
-			subfolder = suffix.replace(/^_/, "");
-			if (!Folder(baseFolder + "/" + subfolder).exists) Folder(baseFolder + "/" + subfolder).create();
-		};
-		// PDF name
-		var pdfBaseName = decodeURI(doc.name).replace(/\.indd$/i, "") + suffix;
-		var pdfName = pdfBaseName + ".pdf";
-		var folder = baseFolder + (!!subfolder ? "/" + subfolder : "");
-		var file = folder + "/" + pdfName;
-		// Unique name
-		if (!ui.output.options.fileOverwrite.value) {
-			var baseRE = RegExp("^" + pdfBaseName + (!!suffix ? "[ _-]*" : "[ _-]+") + "\\d+.*.pdf$", "i");
-			var indxRE = RegExp("^" + pdfBaseName + (!!suffix ? "[ _-]*" : "[ _-]+") +
-				"(\\d+)([ _-]*v *\\d*)?([ _-]*copy *\\d*)?([ _-]*v *\\d*)?$", "i");
-			var pdfFiles = Folder(folder).getFiles(function(f) {
-				if (!/\.pdf$/i.test(f)) return false;
-				return baseRE.test(decodeURI(f.name));
-			});
-			for (var j = 0, maxIndex = 0; j < pdfFiles.length; j++) { // Get max index
-				var n = indxRE.exec(decodeURI(pdfFiles[j].name).replace(/\.pdf$/i, ""));
-				if (!!n) maxIndex = Math.max(maxIndex, Number(n[1]));
-			};
-			if (maxIndex == 0) {
-				if (File(file).exists) file = file.replace(/\.pdf$/i, "") + (!!suffix ? "": " ") + "2.pdf";
-			} else {
-				maxIndex++;
-				file = file.replace(/\.pdf$/i, "") + (!!suffix ? "": " ") + String(maxIndex) + ".pdf";
+	// Init progress bar
+	for (var i = 0, pbWidth = 50; i < docs.length; i++)
+		pbWidth = Math.max(pbWidth, decodeURI(docs[i].name).length);
+	var progressBar = new ProgressBar("Exporting", pbWidth + 10);
+	progressBar.reset(docs.length);
+	// Processing
+	var doc, counter = 1, errors = [];
+	while (doc = docs.shift()) {
+		if (batchMode) {
+			doc = app.open(doc);
+		} else {
+			app.activeDocument = doc;
+			if (!doc.saved) {
+				errors.push(doc.name + " has no path. Skipped.");
+				continue;
 			};
 		};
-		// Processing
-		app.activeDocument = doc;
-		progressBar.update(counter, (baseFolder == decodeURI(doc.filePath) ? decodeURI(File(file).name) : file));
-		// Update links
-		for (var l = 0; l < doc.links.length; l++) {
-			switch (doc.links[l].status) {
-				case LinkStatus.LINK_OUT_OF_DATE:
-					if (ui.output.options.updateLinks.value) link.update()
-					else errors.push(doc.name + ": Link '" + doc.links[l].name + "' is out of date.");
-					break;
-				case LinkStatus.LINK_MISSING:
-					errors.push(doc.name + ": Link '" + doc.links[l].name + "' not found.");
-					break;
+		// Base folder
+		var baseFolder = decodeURI(doc.filePath);
+		if (ui.output.dest.isActive.value && !!ui.output.dest.path) {
+			if (!ui.output.dest.path.exists) ui.output.dest.path.create();
+			baseFolder = decodeURI(ui.output.dest.path.fullName);
+		};
+		for (var i = 1; i < 3; i++) {
+			var exp = ui.presets["exp" + i];
+			if (!exp.isActive.value) continue;
+			var preset = app.pdfExportPresets.item(String(exp.preset.selection));
+			// Suffix
+			var suffix = !!exp.suffix.text ? ("_" + exp.suffix.text.replace(/^_/, "")) : "";
+			// Subfolder
+			var subfolder = "";
+			if (ui.output.options.subfolders.value && !!suffix) {
+				subfolder = suffix.replace(/^_/, "");
+				if (!Folder(baseFolder + "/" + subfolder).exists) Folder(baseFolder + "/" + subfolder).create();
 			};
+			// PDF name
+			var pdfBaseName = decodeURI(doc.name).replace(/\.indd$/i, "") + suffix;
+			var pdfName = pdfBaseName + ".pdf";
+			var folder = baseFolder + (!!subfolder ? "/" + subfolder : "");
+			var file = folder + "/" + pdfName;
+			if (!ui.output.options.fileOverwrite.value) { // Unique name
+				pdfBaseName = pdfBaseName.replace(/(\.|\^|\$|\+|\(|\)|\[|\]|\{|\})/g, "\\$1"); // Escape special chars
+				var baseRE = RegExp("^" + pdfBaseName + (!!suffix ? "[ _-]*" : "[ _-]+") + "\\d+.*.pdf$", "i");
+				var indxRE = RegExp("^" + pdfBaseName + (!!suffix ? "[ _-]*" : "[ _-]+") +
+					"(\\d+)([ _-]*v *\\d*)?([ _-]*copy *\\d*)?([ _-]*v *\\d*)?$", "i");
+				var pdfFiles = Folder(folder).getFiles(function(f) {
+					if (!(f instanceof File)) return false;
+					if (!/\.pdf$/i.test(f)) return false;
+					return baseRE.test(decodeURI(f.name));
+				});
+				for (var j = 0, maxIndex = 0; j < pdfFiles.length; j++) { // Get max index
+					var n = indxRE.exec(decodeURI(pdfFiles[j].name).replace(/\.pdf$/i, ""));
+					if (!!n) maxIndex = Math.max(maxIndex, Number(n[1]));
+				};
+				if (maxIndex == 0) {
+					if (File(file).exists) file = file.replace(/\.pdf$/i, "") + (!!suffix ? "": " ") + "2.pdf";
+				} else {
+					maxIndex++;
+					file = file.replace(/\.pdf$/i, "") + (!!suffix ? "": " ") + String(maxIndex) + ".pdf";
+				};
+			};
+			progressBar.update(counter, (baseFolder == decodeURI(doc.filePath) ? decodeURI(File(file).name) : file));
+			// Update links
+			for (var l = 0; l < doc.links.length; l++) {
+				switch (doc.links[l].status) {
+					case LinkStatus.LINK_OUT_OF_DATE:
+						if (ui.output.options.updateLinks.value) doc.links[l].update()
+						else errors.push(doc.name + ": Link '" + doc.links[l].name + "' is out of date.");
+						break;
+					case LinkStatus.LINK_MISSING:
+						errors.push(doc.name + ": Link '" + doc.links[l].name + "' not found.");
+						break;
+				};
+			};
+			// Run script
+			if (exp.script.enabled && exp.script.isActive.value && exp.script.path.exists) {
+				var ext = decodeURI(exp.script.path.fullName).
+					substr(decodeURI(exp.script.path.fullName).lastIndexOf(".") + 1);
+				var scriptLanguage = (function(ext) {
+					return {
+						"scpt": ScriptLanguage.APPLESCRIPT_LANGUAGE,
+						"js": ScriptLanguage.JAVASCRIPT,
+						"jsx": ScriptLanguage.JAVASCRIPT,
+						"jsxbin": ScriptLanguage.JAVASCRIPT,
+					}[ext];
+				})(ext);
+				app.doScript(exp.script.path,
+				scriptLanguage, undefined,
+				UndoModes.ENTIRE_SCRIPT, "Run script");
+			};
+			// Export PDF
+			if (!ui.output.options.fileOverwrite.value && File(file).exists) {
+				alert("Attempted illegal overwrite.\nBetter quit than sorry..."); exit() };
+			doc.exportFile(ExportFormat.PDF_TYPE, File(file), false, preset);
 		};
-		// Run script
-		if (exp.script.enabled && exp.script.isActive.value && exp.script.path.exists) {
-			var ext = decodeURI(exp.script.path.fullName).substr(decodeURI(exp.script.path.fullName).lastIndexOf(".") + 1);
-			var scriptLanguage = (function(ext) {
-				return {
-					"scpt": ScriptLanguage.APPLESCRIPT_LANGUAGE,
-					"js": ScriptLanguage.JAVASCRIPT,
-					"jsx": ScriptLanguage.JAVASCRIPT,
-					"jsxbin": ScriptLanguage.JAVASCRIPT,
-				}[ext];
-			})(ext);
-			app.doScript(exp.script.path,
-			scriptLanguage, undefined,
-			UndoModes.ENTIRE_SCRIPT, "Run script");
+		counter++;
+		if (ui.output.options.docSave.value && doc.saved && doc.modified) doc.save();
+		if (batchMode) {
+			doc.close(SaveOptions.NO);
+			app.scriptPreferences.userInteractionLevel = oldUIL;
+		} else {
+			if (ui.output.options.docClose.value) doc.close(SaveOptions.NO);
 		};
-		// Export PDF
-		if (!ui.output.options.fileOverwrite.value && File(file).exists) {
-			alert("Attempted illegal overwrite.\nBetter quit than sorry..."); exit() };
-		doc.exportFile(ExportFormat.PDF_TYPE, File(file), false, preset);
 	};
-	counter++;
-	if (ui.output.options.docSave.value && doc.saved && doc.modified) doc.save();
-	if (ui.output.options.docClose.value) doc.close(SaveOptions.NO);
+	progressBar.close();
+	app.scriptPreferences.userInteractionLevel = old.userInteractionLevel;
+	app.pdfExportPreferences.viewPDF = old.viewPDF;
+	if (errors.length > 0) AlertScroll("Errors", errors);
 };
-progressBar.close();
-app.scriptPreferences.userInteractionLevel = old.userInteractionLevel;
-app.pdfExportPreferences.viewPDF = old.viewPDF;
-if (errors.length > 0) AlertScroll("Errors", errors);
 
 function TruncatePath(/*string*/path, maxLength) {
 	if (path.length > maxLength + 3) path = "..." + path.slice(- maxLength + 3);
@@ -347,7 +392,7 @@ function ReadSettings() {
 	ui.output.options.updateLinks.value = settings.output.options.updatelinks;
 	ui.output.options.fileOverwrite.value = settings.output.options.overwrite;
 	ui.output.options.docSave.value = settings.output.options.save;
-	ui.output.options.docClose.value = settings.output.options.close;
+	ui.output.options.docClose.value = batchMode ? true : settings.output.options.close;
 	ui.presets.exp1.isActive.onClick();
 	ui.presets.exp1.script.isActive.onClick();
 	ui.presets.exp2.isActive.onClick();
@@ -372,7 +417,7 @@ function SaveSettings() {
 			active: ui.presets.exp1.isActive.value
 		},
 		script1: {
-			file: decodeURI(ui.presets.exp1.script.path.fullName),
+			file: ui.presets.exp1.script.path.exists ? decodeURI(ui.presets.exp1.script.path.fullName) : "",
 			active: ui.presets.exp1.script.isActive.value
 		},
 		preset2: {
@@ -381,12 +426,12 @@ function SaveSettings() {
 			active: ui.presets.exp2.isActive.value
 		},
 		script2: {
-			file: decodeURI(ui.presets.exp2.script.path.fullName),
+			file: ui.presets.exp2.script.path.exists ? decodeURI(ui.presets.exp2.script.path.fullName) : "",
 			active: ui.presets.exp2.script.isActive.value
 		},
 		output: {
 			dest: {
-				folder: decodeURI(ui.output.dest.path.fullName),
+				folder: ui.output.dest.path.exists ? decodeURI(ui.output.dest.path.fullName) : "",
 				active: ui.output.dest.isActive.value
 			},
 			options: {
@@ -398,6 +443,11 @@ function SaveSettings() {
 			},
 		}
 	};
+	try {
+		oldSettings = $.evalFile(settingsFile);
+		if (settings.toSource() === oldSettings.toSource()) return;
+	} catch (_) {};
+
 	settingsFile.open('w');
 	settingsFile.write(settings.toSource());
 	settingsFile.close();
