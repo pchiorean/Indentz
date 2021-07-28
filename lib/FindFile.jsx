6717734e609244df71cb293a21ce60e50a525108
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-07-28 17:56:29 +0300

    `upd` Add option to skip current folder

--- lib/FindFile.jsx
+++ lib/FindFile.jsx
@@ -1,14 +1,17 @@
 /**
  * Finds the first occurence of a file, looking in the current folder, on the desktop, or next to the script.
- * @param {string} fn - Filename
+ * @param {string} filename - Filename
+ * @param {boolean} skipLocal - Don't search in current folder
  * @returns {File} - File object
  */
-function FindFile(fn) {
+function FindFile(filename, skipLocal) {
 	var file = "";
 	var script = (function() { try { return app.activeScript } catch(e) { return new File(e.fileName) } })();
-	if (doc.saved && (file = File(app.activeDocument.filePath + "/_" + fn)) && file.exists) return file;
-	if (doc.saved && (file = File(app.activeDocument.filePath + "/" + fn)) && file.exists) return file;
-	if ((file = File(Folder.desktop + "/" + fn)) && file.exists) return file;
-	if ((file = File(script.path + "/" + fn)) && file.exists) return file;
-	if ((file = File(script.path + "/../" + fn)) && file.exists) return file;
+	if (!skipLocal) {
+		if (doc.saved && (file = File(app.activeDocument.filePath + "/_" + filename)) && file.exists) return file;
+		if (doc.saved && (file = File(app.activeDocument.filePath + "/" + filename)) && file.exists) return file;
+	};
+	if ((file = File(Folder.desktop + "/" + filename)) && file.exists) return file;
+	if ((file = File(script.path + "/" + filename)) && file.exists) return file;
+	if ((file = File(script.path + "/../" + filename)) && file.exists) return file;
 };
