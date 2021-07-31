1b84882918e190a641fd40f3fe320f61537139f9
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-07-31 10:22:33 +0300

    `doc` Update license

--- view/TileAll.jsx
+++ view/TileAll.jsx
@@ -15,13 +15,13 @@ var page = app.activeWindow.activePage;
 var size = {
 	width: page.parent.pages.lastItem().bounds[3] - page.parent.pages.firstItem().bounds[1],
 	height: page.parent.pages.lastItem().bounds[2] - page.parent.pages.firstItem().bounds[0]
-}
+};
 switch (size.width / size.height <= 1) {
 	case true: app.menuActions.item("$ID/Tile All Vertically").invoke(); break;
 	case false: app.menuActions.item("$ID/Tile All Horizontally").invoke(); break;
-}
+};
 app.selection = NothingEnum.NOTHING;
 for (var i = 0; i < app.windows.length; i++) {
 	app.windows[i].zoom(ZoomOptions.FIT_SPREAD);
 	// app.windows[i].zoomPercentage *= 0.9;
-}
+};
