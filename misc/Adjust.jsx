05935ced6f1ae43f28b9f660ac112aba087fe8b1
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-08-26 10:07:23 +0300

    `upd` Add `align` labels

--- misc/Adjust.jsx
+++ misc/Adjust.jsx
@@ -1,21 +1,47 @@
+/*
+	Adjust items v0.1 (2021-08-19)
+	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
+
+	Released under MIT License:
+	https://choosealicense.com/licenses/mit/
+*/
+
 // @include "../lib/FitTo.jsxinc";
 
-function ExpandItems(items) {
+if (!(doc = app.activeDocument)) exit();
+
+app.doScript(main, ScriptLanguage.javascript, undefined,
+	UndoModes.ENTIRE_SCRIPT, "Adjust items");
+
+function main() {
+	var item, items = doc.selection.length == 0 ? doc.allPageItems : doc.selection;
+
 	while (item = items.shift()) {
-		item.redefineScaling();
-		switch (item.label) {
-			case "fit":
-				app.select(item);
-				item.fit(FitOptions.FRAME_TO_CONTENT);
-				app.doScript(FitTo, ScriptLanguage.javascript, [ "page", "size" ],
-				UndoModes.ENTIRE_SCRIPT, "Resize to page");
-				break;
-			case "bleed":
-				app.select(item);
-				app.doScript(FitTo, ScriptLanguage.javascript, [ "spread", "bleed", true ],
-				UndoModes.ENTIRE_SCRIPT, "Resize to spread bleed");
-				break;
-		}
-	}
+		// item.redefineScaling();
+		app.select(item);
+
+		if (/\balign[TB]?L\b/ig.test(item.label)) {
+			doc.align(item, AlignOptions.LEFT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+		};
+		if (/\balign[TB]?R\b/ig.test(item.label)) {
+			doc.align(item, AlignOptions.RIGHT_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+		};
+		if (/\balignT[LR]?\b/ig.test(item.label)) {
+			doc.align(item, AlignOptions.TOP_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+		};
+		if (/\balignB[LR]?\b/ig.test(item.label)) {
+			doc.align(item, AlignOptions.BOTTOM_EDGES, AlignDistributeBounds.MARGIN_BOUNDS);
+		};
+
+		if (/\bfitV\b/ig.test(item.label)) {
+			item.fit(FitOptions.FRAME_TO_CONTENT);
+			app.doScript(FitTo, ScriptLanguage.JAVASCRIPT, [ "page", "size" ],
+			UndoModes.ENTIRE_SCRIPT, "Fit to page");
+		};
+		if (/\bfitB\b/ig.test(item.label)) {
+			app.doScript(FitTo, ScriptLanguage.JAVASCRIPT, [ "spread", "bleed", true ],
+			UndoModes.ENTIRE_SCRIPT, "Fit to spread bleed");
+		};
+	};
 	app.select(null);
-}
+};
