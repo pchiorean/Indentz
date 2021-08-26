1c74e34d4077c019b392f41e1fe651e0428fd401
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-08-26 10:00:23 +0300

    `new` Fix paragraphs end leading

--- setup/DocCleanup.jsx
+++ setup/DocCleanup.jsx
@@ -1,5 +1,5 @@
 ﻿/*
-	Doc cleanup v2.6 (2021-07-26)
+	Doc cleanup v2.7 (2021-08-26)
 	(c) 2020-2021 Paul Chiorean (jpeg@basement.ro)
 
 	Changes some settings, cleans up swatches/layers/pages and resets scaling.
@@ -17,8 +17,7 @@ ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Set preferences");
 
 // Turn off 'AutoUpdateURLStatus' from 'Hyperlinks' panel
-app.doScript(
-function() {
+app.doScript(function () {
 	var setAUU = app.menuActions.itemByName("$ID/AutoUpdateURLStatus");
 	var hyperLinksPanel = app.panels.itemByName("$ID/Hyperlinks");
 	var oldHLP = hyperLinksPanel.visible;
@@ -30,8 +29,7 @@ ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Turn off 'AutoUpdateURLStatus'");
 
 // Show 'guides' layer
-app.doScript(
-function() {
+app.doScript(function () {
 	var layer;
 	if ((layer = doc.layers.item("guides")).isValid) layer.visible = true;
 	if ((layer = doc.layers.item("Guides")).isValid) layer.visible = true;
@@ -40,8 +38,7 @@ ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Show 'guides' layer");
 
 // Unlock layers, items, delete hidden, reset scaling
-app.doScript(
-function() {
+app.doScript(function () {
 	doc.layers.everyItem().locked = false;
 	var item, items = doc.pageItems.everyItem().getElements();
 	var askd, delHidden;
@@ -58,48 +55,64 @@ ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Unlock items, delete hidden, reset scaling");
 
 // Delete unused layers
-app.doScript(
-function() {
+app.doScript(function () {
 	if ((menu = app.menuActions.item("$ID/Delete Unused Layers")).enabled) menu.invoke();
 },
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete unused layers");
 
 // Delete empty spreads
-app.doScript(
-function() {
-	for (var i = 0, s = doc.spreads; i < s.length; i++)
-		if (s[i].pageItems.length == 0 && s.length > 1) { s[i].remove(); i-- };
+app.doScript(function () {
+	var spread, spreads = doc.spreads.everyItem().getElements();
+	while (spread = spreads.shift()) {
+		if (spread.allPageItems.length == 0 && doc.spreads.length > 1) spread.remove();
+	};
 },
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete empty spreads");
 
 // Delete unused swatches
-app.doScript(
-function() {
-	var item, items = doc.unusedSwatches;
-	while (item = items.shift()) if (item.name != "") item.remove();
+app.doScript(function () {
+	var swatch, swatches = doc.unusedSwatches;
+	while (swatch = swatches.shift()) {
+		if (swatch.name != "") swatch.remove();
+	};
 },
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Delete unused swatches");
 
 // Convert empty text frames to generic frames
-app.doScript(
-function() {
-	var item, items = doc.textFrames.everyItem().getElements();
-	while (item = items.shift()) {
-		if (/\s+$/g.test(item.contents) && item.nextTextFrame == null)
-			item.contents = item.contents.replace(/\s+$/g, "");
-		if (item.contents.length == 0) item.contentType = ContentType.UNASSIGNED;
+app.doScript(function () {
+	var frame, frames = doc.textFrames.everyItem().getElements();
+	while (frame = frames.shift()) {
+		if (/\s+$/g.test(frame.contents) && frame.nextTextFrame == null)
+			frame.contents = frame.contents.replace(/\s+$/g, "");
+		if (frame.contents.length == 0) frame.contentType = ContentType.UNASSIGNED;
 	};
 },
 ScriptLanguage.javascript, undefined,
 UndoModes.ENTIRE_SCRIPT, "Convert empty text frames to generic frames");
 
+// Fix paragraphs end leading
+app.doScript(function () {
+	var story, stories = doc.stories.everyItem().getElements();
+	while (story = stories.shift()) {
+		var paragraph, paragraphs = story.paragraphs.everyItem().getElements();
+		while (paragraph = paragraphs.shift()) {
+			if (paragraph.characters.length > 1 && paragraph.characters[-1].contents == "\r") {
+				paragraph.characters[-1].pointSize = paragraph.characters[-2].pointSize;
+				paragraph.characters[-1].leading = paragraph.characters[-2].leading;
+			};
+		};
+	};
+},
+ScriptLanguage.javascript, undefined,
+UndoModes.ENTIRE_SCRIPT, "Fix paragraphs end leading");
+
 doc.textPreferences.showInvisibles = false;
 
 // Set pasteboard
-app.doScript(function() {
+app.doScript(function () {
 	const P = { width: 150, height: 50 }; // Defaults (mm)
 	var size = {
 		width: doc.spreads[0].pages.lastItem().bounds[3] - doc.spreads[0].pages.firstItem().bounds[1],
