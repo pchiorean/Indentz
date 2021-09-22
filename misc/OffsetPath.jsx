4e1e7021326a1afc48c2e40c9ce54c1279b3cdd2
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-09-22 19:15:51 +0300

    `fix` Fix containers; add option to join contours

--- misc/OffsetPath.jsx
+++ misc/OffsetPath.jsx
@@ -1,5 +1,5 @@
-﻿/*
-	Offset Paths v1.2 (2021-09-11)
+/*
+	Offset Paths v2.0 (2021-09-22)
 
 	Use InDesign's text wrap feature to create offset/inset paths.
 
@@ -8,8 +8,11 @@
 	https://www.siliconpublishing.com/blog/free-indesign-scripts/
 
 	Changes by Paul Chiorean (jpeg@basement.ro):
-	- Default value is in millimeters;
+	- Take any objects as source;
+	- Duplicate source object to avoid some corner cases (e.g., images);
 	- Select outlined paths on exit;
+	- Add a default value in millimeters;
+	- Add option to join contours;
 	- Add undo.
 */
 
@@ -21,83 +24,62 @@ app.doScript(main, ScriptLanguage.JAVASCRIPT, doc.selection,
 	UndoModes.ENTIRE_SCRIPT, 'Offset paths');
 
 function main(selection) {
-	var objectList = [];
+	var ui, joinContours, offsetField, offset;
+	var paths = [];
 	var oldMU  = app.scriptPreferences.measurementUnit;
 	var oldUIL = app.scriptPreferences.userInteractionLevel;
 	app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.INTERACT_WITH_ALL;
+	app.scriptPreferences.enableRedraw = false;
 
-	for (var i = 0, n = selection.length; i < n; i++) {
-		switch (selection[i].constructor.name) {
-			case 'GraphicLine':
-			case 'Rectangle':
-			case 'Oval':
-			case 'Polygon':
-				objectList.push(selection[i]);
-				break;
+	ui = app.dialogs.add({ name: 'Offset Paths' });
+	with (ui.dialogColumns.add()) {
+		with (dialogRows.add()) {
+			staticTexts.add({ staticLabel: 'Offset:' });
+			offsetField = measurementEditboxes.add({
+				minWidth:   85,
+				editUnits:  MeasurementUnits.MILLIMETERS,
+				editValue:  UnitValue('3 mm').as('pt'),
+				smallNudge: 1,
+				largeNudge: 1
+			});
 		}
+		with (dialogRows.add()) { '' }
+		with (dialogRows.add()) {
+			joinCB = checkboxControls.add({
+				staticLabel:  'Join contours',
+				checkedState: true
+			});
+		}
+		with (ui.dialogColumns.add()) { '' }
 	}
-	if (objectList.length > 0) displayDialog(objectList);
+	if (ui.show()) {
+		offset = offsetField.editValue;
+		joinContours = joinCB.checkedState;
+		for (var i = 0; i < selection.length; i++) paths.push(offsetPath(selection[i]));
+		if (joinContours && paths.length > 1) paths = (paths.shift()).addPath(paths);
+		app.select(paths);
+	}
+	ui.destroy();
 	app.scriptPreferences.measurementUnit = oldMU;
 	app.scriptPreferences.userInteractionLevel = oldUIL;
-}
 
-function displayDialog(objectList) {
-	var offsetField, result, offset;
-	var dialog = app.dialogs.add({ name: 'Offset Paths' });
-	with (dialog.dialogColumns.add()) {
-		with (dialogRows.add()) {
-			with (dialogColumns.add()) {
-				staticTexts.add({ staticLabel: 'Offset:' });
-			}
-			with (dialogColumns.add()) {
-				offsetField = measurementEditboxes.add({
-					minWidth:   85,
-					editUnits:  MeasurementUnits.MILLIMETERS,
-					editValue:  UnitValue('3 mm').as('pt'),
-					smallNudge: 1,
-					largeNudge: 1
-				});
-			}
+	function offsetPath(item) {
+		var tmpItem, wrapPaths, newPath;
+		tmpItem = item.duplicate();
+		// tmpItem.bringToFront(item);
+		if (tmpItem.contentType === ContentType.GRAPHIC_TYPE && tmpItem.graphics.length === 1)
+			tmpItem.pageItems[0].remove();
+		tmpItem.textWrapPreferences.textWrapMode = TextWrapModes.CONTOUR;
+		tmpItem.textWrapPreferences.textWrapOffset = [ offset, offset, offset, offset ];
+		wrapPaths = tmpItem.textWrapPreferences.paths;
+		newPath = tmpItem.parent.polygons.add();
+		for (var i = 0; i < wrapPaths.length; i++) {
+			if (i > 0) newPath.paths.add();
+			newPath.paths.item(i).pathType   = wrapPaths.item(i).pathType;
+			newPath.paths.item(i).entirePath = wrapPaths.item(i).entirePath;
 		}
-	}
-	result = dialog.show();
-	if (result) {
-		offset = offsetField.editValue;
-		dialog.destroy();
-		offsetPaths(objectList, offset);
-	} else { dialog.destroy(); }
-}
-
-function offsetPaths(objectList, offset) {
-	app.select(NothingEnum.NOTHING);
-	for (var i = 0, n = objectList.length; i < n; i++)
-		app.select(offsetPath(objectList[i], offset), SelectionOptions.ADD_TO);
-}
-
-function offsetPath(pageItem, offset) {
-	var offsetArray, textWrapPaths, newPageItem;
-	pageItem.textWrapPreferences.textWrapMode = TextWrapModes.CONTOUR;
-	offsetArray = makeOffsetArray(pageItem, offset);
-	pageItem.textWrapPreferences.textWrapOffset = offsetArray;
-	textWrapPaths = pageItem.textWrapPreferences.paths;
-	newPageItem = pageItem.parent.polygons.add();
-	for (var i = 0, n = textWrapPaths.length; i < n; i++) {
-		if (i > 0) newPageItem.paths.add();
-		newPageItem.paths.item(i).pathType   = textWrapPaths.item(i).pathType;
-		newPageItem.paths.item(i).entirePath = textWrapPaths.item(i).entirePath;
-	}
-	pageItem.textWrapPreferences.textWrapMode = TextWrapModes.NONE;
-	return newPageItem;
-
-	function makeOffsetArray() {
-		var array, arrayLength;
-		if (pageItem.textWrapPreferences.textWrapOffset.length > 0) {
-			arrayLength = pageItem.textWrapPreferences.textWrapOffset.length;
-			array = new Array(arrayLength);
-			for (var i = 0, n = arrayLength; i < n; i++) array[i] = offset;
-			return array;
-		}
-		return offset;
+		tmpItem.remove();
+		return newPath;
 	}
 }
