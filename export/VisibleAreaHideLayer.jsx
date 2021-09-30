e6fec1f9a59c1d3822580612d024b29d970a5ff5
pchiorean <61617801+pchiorean@users.noreply.github.com>
2021-10-01 00:18:13 +0300

    `doc` Update description

--- export/VisibleAreaHideLayer.jsx
+++ export/VisibleAreaHideLayer.jsx
@@ -22,10 +22,10 @@ if (app.documents.length === 0) exit();
 
 	/**
 	* Matches a string against elements of an array, using wildcards and case sensitivity.
-	* @param {String} searchValue - String to be matched
-	* @param {Array} array - An array of strings; wildcards: '*' (zero or more characters), '?' (exactly one character)
-	* @param {Boolean} [caseSensitive=false] - Case sensitivity; default false
-	* @returns {Boolean} - True for match, false for no match
+	* @param {string} searchValue - String to be matched.
+	* @param {array} array - An array of strings; wildcards: '*' (zero or more characters), '?' (any character).
+	* @param {boolean} [caseSensitive=false] - If `true` the search is case sensitive. (Optional.)
+	* @returns {boolean} - Returns `true` for match, `false` for no match.
 	*/
 	function isIn(searchValue, array, caseSensitive) {
 		caseSensitive = (caseSensitive === undefined) ? false : caseSensitive;
