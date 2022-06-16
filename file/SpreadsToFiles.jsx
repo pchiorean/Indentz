d5f2a4c5f3cc42a26a8b63bd90539645f60ba464
pchiorean <61617801+pchiorean@users.noreply.github.com>
2022-06-16 21:33:28 +0300

    `upd` Add a custom positioning placeholder

--- file/SpreadsToFiles.jsx
+++ file/SpreadsToFiles.jsx
@@ -1,5 +1,5 @@
 /*
-	Spreads to files 22.3.14
+	Spreads to files 22.6.16
 	(c) 2020-2022 Paul Chiorean (jpeg@basement.ro)
 
 	Saves the spreads of the active document in separate files.
@@ -32,36 +32,35 @@ if (!(doc = app.activeDocument)) exit();
 if (!doc.saved) { alert('Document is not saved.'); exit(); }
 if (doc.spreads.length === 1) { alert('Document has only one spread.'); exit(); }
 
-var suffix, spread, inc, targetFile, target;
+var defaultTemplate, detectedTemplate, template, customPosition, spread, targetFile, target, progressBar, i, j;
 var r = [];
 var ADV = ScriptUI.environment.keyboardState.altKey;
-var forbiddenFilenameCharsRE = /[#%^{}\\<>*?\/$!'":@`|=]/g; // eslint-disable-line no-useless-escape
+var invalidFilenameChars = /[<>:"\/\\|?*]/g; // https://gist.github.com/doctaphred/d01d05291546186941e1b7ddc02034d3
 var oldUIL = app.scriptPreferences.userInteractionLevel;
 var currentPath = doc.filePath;
 var baseName = (/\./g.test(doc.name) && doc.name.slice(0, doc.name.lastIndexOf('.'))) || doc.name;
-var progressBar = new ProgressBar('Saving', doc.spreads.length);
 
-// Detect or ask for a suffix
-var defaultSufx = '-123456789abcdefghijklmnopqrstuvwxyz'.slice(0, doc.spreads.length + 1);
-var detectedSufx = RegExp('[ ._-][a-zA-Z0-9]{' + doc.spreads.length + '}$', 'i').exec(baseName);
-if (/\d\s*x\s*\d/i.test(detectedSufx)) detectedSufx = null; // Exclude '0x0' suffixes
-if (ADV) suffix = getSuffix(null);
-else suffix = detectedSufx ? String(detectedSufx) : getSuffix(null);
-// Main loop
+// Detect or ask user for a template
+defaultTemplate = '-123456789abcdefghijklmnopqrstuvwxyz'.slice(0, doc.spreads.length + 1);
+detectedTemplate = RegExp('[ ._-][a-zA-Z0-9]{' + doc.spreads.length + '}$', 'i').exec(baseName);
+if (/\d\s*x\s*\d/i.test(detectedTemplate)) detectedTemplate = null; // Exclude '0x0' suffixes
+if (ADV) template = templateFromUser(null); // In advanced mode always ask user
+else template = detectedTemplate ? String(detectedTemplate) : templateFromUser(null);
+if (template === String(detectedTemplate)) baseName = baseName.replace(detectedTemplate, '');
+// Detect custom position placeholder
+customPosition = baseName.match(/(.+)#(.+)/);
+
+progressBar = new ProgressBar('Saving', doc.spreads.length);
 for (spread = 0; spread < doc.spreads.length; spread++) {
 	// Filter out current spread
 	r = [];
-	for (var i = 0; i < doc.spreads.length; i++) if (i !== spread) r.push(i);
+	for (i = 0; i < doc.spreads.length; i++) if (i !== spread) r.push(i);
 	// Get unique name
-	if (suffix === String(detectedSufx)) baseName = baseName.replace(detectedSufx, '');
-	inc = 0;
-	do {
-		targetFile = File(currentPath + '/' + baseName +              // Base name
-			suffix[0] + suffix[spread + 1] +                          // Separator + suffix char
-			(inc > 1 ? ' copy ' + inc : (inc === 0 ? '' : ' copy')) + // Unique name
-			'.indd');
-		inc++;
-	} while (targetFile.exists);
+	targetFile = File(currentPath + '/' +
+		uniqueName(customPosition ?
+			(customPosition[1] + template[spread + 1] + customPosition[2]) : // Custom position
+			(baseName + template[0] + template[spread + 1]) // Default position
+		) + '.indd');
 	// Disable user interaction and open a copy
 	progressBar.update();
 	doc.saveACopy(targetFile);
@@ -69,36 +68,47 @@ for (spread = 0; spread < doc.spreads.length; spread++) {
 	target = app.open(targetFile, false);
 	app.scriptPreferences.userInteractionLevel = oldUIL;
 	// Remove other spreads from copy and save file
-	for (var j = r.length - 1; j >= 0; j--) target.spreads[r[j]].remove();
+	for (j = r.length - 1; j >= 0; j--) target.spreads[r[j]].remove();
 	target.save(targetFile);
 	target.close();
 }
 progressBar.close();
 doc.close(SaveOptions.ASK);
 
-
-function getSuffix(str) {
-	if (!str) str = detectedSufx || defaultSufx;
-	// Ask user
-	str = prompt('Enter a suffix template.\nThe first character is the separator, the rest ' +
-	'are added sequentially to the file names.\n\nE.g.: \'-ABC\' will split the file into\n\n' +
-	'Filename-A.indd\nFilename-B.indd\nFilename-C.indd', str, 'Enter a suffix template');
+function templateFromUser(str) {
+	if (!str) str = detectedTemplate || defaultTemplate;
+	str = prompt('Please enter an index list.\nThe first character is the separator, the rest will be ' +
+		'added sequentially to each file name; for example \'-123\' will split a 3-spread document into\n\n' +
+		'Document-1.indd\nDocument-2.indd\nDocument-3.indd\n\nIf the filename contains a "#", the index ' +
+		'will be placed in that position.', str);
 	if (!str) exit();
+
 	// Sanitize input
 	// -- Length
 	if (str.length < doc.spreads.length + 1) {
-		alert('You must enter a separator and ' + doc.spreads.length + ' additional characters.', 'Enter a suffix');
-		getSuffix(str); // Ask again
+		alert('You must enter a separator and ' + doc.spreads.length + ' additional characters.');
+		templateFromUser(str);
 	}
 	// -- Separator
 	if (!/[ ._\-~]/.test(str[0])) {
-		alert('Invalid separator, please try again.', 'Enter a suffix');
-		getSuffix(str); // Ask again
+		alert('You entered an invalid separator, please try again.\nUse only space, period, ' +
+		'underscore, hyphen or tilde.');
+		templateFromUser(str);
 	}
 	// -- Filename
-	if (forbiddenFilenameCharsRE.test(str)) {
-		alert('You entered a forbidden character, please try again.', 'Enter a suffix');
-		getSuffix(str); // Ask again
+	if (invalidFilenameChars.test(str)) {
+		alert('You entered an invalid character, please try again.\nAvoid < > : " / \\ | ? and *.');
+		templateFromUser(str);
 	}
 	return str.slice(0, doc.spreads.length + 1);
+}
+
+function uniqueName(str) {
+	var name;
+	var i = 0;
+	do {
+		name = str + (i === 0 ? '' : (' copy' + (i === 1 ? '' : ' ' + i)));
+		i++;
+	} while (File(currentPath + '/' + name + '.indd').exists);
+	return name;
 }
