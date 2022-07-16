c44522729df1d402ab5129ac020e735202e435e4
pchiorean <61617801+pchiorean@users.noreply.github.com>
2022-07-16 18:29:53 +0300

    `upd` Update to use `lib/addGuide()`

--- layout/GuidesAdd.jsx
+++ layout/GuidesAdd.jsx
@@ -1,6 +1,6 @@
 /*
-	Add page guides 21.10.10
-	(c) 2021 Paul Chiorean (jpeg@basement.ro)
+	Add page guides 22.7.16
+	(c) 2021-2022 Paul Chiorean (jpeg@basement.ro)
 
 	Adds guides on pages' edges and inner centers or selected objects' edges.
 
@@ -10,7 +10,10 @@
 
 if (!(doc = app.activeDocument)) exit();
 
-app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.ENTIRE_SCRIPT, 'Add page guides');
+// @include '../lib/AddGuide.jsxinc';
+
+app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined,
+	UndoModes.ENTIRE_SCRIPT, 'Add page guides');
 
 function main() {
 	var hwLayer, guidesLayer, page, guide, s, i, n;
@@ -44,7 +47,6 @@ function main() {
 		guidesLayer.move(LocationOptions.BEFORE, hwLayer);
 	}
 
-	// Add guides
 	if (items.length > 0) { // Selection guides
 		// Get selection's parent page
 		s = items[0].parent;
@@ -52,6 +54,7 @@ function main() {
 		page = s.pages[0];
 		for (i = 0, n = items.length; i < n; i++)
 			if (items[i].parentPage) { page = items[i].parentPage; break; }
+
 		// Get selection's bounds
 		targetBounds = items[0].visibleBounds;
 		for (i = 1, n = items.length; i < n; i++) {
@@ -60,31 +63,32 @@ function main() {
 			targetBounds[2] = Math.max(items[i].visibleBounds[2], targetBounds[2]);
 			targetBounds[3] = Math.max(items[i].visibleBounds[3], targetBounds[3]);
 		}
-		// Add selection guides
-		addGuide(page.parent, guidesLayer, 'v', targetBounds[1]);
-		addGuide(page.parent, guidesLayer, 'v', targetBounds[3]);
+
+		// Add guides
 		addGuide(page.parent, guidesLayer, 'h', targetBounds[0]);
+		addGuide(page.parent, guidesLayer, 'v', targetBounds[1]);
 		addGuide(page.parent, guidesLayer, 'h', targetBounds[2]);
+		addGuide(page.parent, guidesLayer, 'v', targetBounds[3]);
 	} else { // Page guides
 		// If old guides exist, remove them and exit (undo mode)
 		while ((guide = guides.shift()))
-			if (/\b(left|right|top|bottom|middle|hw)\b/i.test(guide.label)) { guide.remove(); flgExit = true;}
+			if (/\b(left|right|top|bottom|middle|hw)\b/i.test(guide.label)) { guide.remove(); flgExit = true; }
 		if (flgExit) exit();
-		// Add page guides
-		for (i = 0, n = doc.pages.length; i < n; i++) {
+
+		// Add guides
+		for (i = 0; i < doc.pages.length; i++) {
 			page = doc.pages[i];
 			mgbounds = mgBounds(page);
 			pageSize = { width: page.bounds[3] - page.bounds[1], height: page.bounds[2] - page.bounds[0] };
 			innerSize = { width: mgbounds[3] - mgbounds[1], height: mgbounds[2] - mgbounds[0] };
-			addGuide(page.parent, guidesLayer, 'v', page.bounds[1], 'left');
-			addGuide(page.parent, guidesLayer, 'v', page.bounds[3], 'right');
-			addGuide(page.parent, guidesLayer, 'h', page.bounds[0], 'top');
-			addGuide(page.parent, guidesLayer, 'h', page.bounds[2], 'bottom');
-			addGuide(page, guidesLayer, 'v', (mgbounds[1] + innerSize.width / 2), 'middle');
-			addGuide(page, guidesLayer, 'h', (mgbounds[0] + innerSize.height * (1 - HW) / 2), 'middle');
-			if (hwLayer.isValid)
-				addGuide(page, hwLayer, 'h', (mgbounds[2] - innerSize.height * HW), 'HW', UIColors.GRID_GREEN);
-			else addGuide(page, guidesLayer, 'h', (mgbounds[2] - innerSize.height * HW), 'HW', UIColors.GRID_GREEN);
+			addGuide(page/*.parent*/, guidesLayer, 'h', page.bounds[0], 'top', 'g');
+			addGuide(page/*.parent*/, guidesLayer, 'v', page.bounds[1], 'left', 'g');
+			addGuide(page/*.parent*/, guidesLayer, 'h', page.bounds[2], 'bottom', 'g');
+			addGuide(page/*.parent*/, guidesLayer, 'v', page.bounds[3], 'right', 'g');
+			addGuide(page, guidesLayer, 'v', mgbounds[1] + innerSize.width / 2, 'middle', 'x');
+			addGuide(page, guidesLayer, 'h', mgbounds[0] + innerSize.height * (1 - HW) / 2, 'middle', 'x');
+			addGuide(page, (hwLayer.isValid ? hwLayer : guidesLayer), 'h',
+				(mgbounds[2] - innerSize.height * HW), 'HW', 's');
 		}
 	}
 
@@ -102,18 +106,5 @@ function main() {
 			page.bounds[2] - margins.bottom,
 			(target.side === PSO.LEFT_HAND) ? target.bounds[3] - margins.left : target.bounds[3] - margins.right
 		];
-	}
-
-	function addGuide(target, layer, HorV, location, label, color) {
-		target.guides.add(
-			undefined,
-			{
-				itemLayer:   layer,
-				label:       label || '',
-				guideColor:  color || UIColors.LIGHT_GRAY,
-				orientation: (HorV === 'h') ? HorizontalOrVertical.HORIZONTAL : HorizontalOrVertical.VERTICAL,
-				location:    location
-			}
-		);
 	}
 }
