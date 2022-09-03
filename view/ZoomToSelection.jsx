17d5cb5ee2483fb73ec522f907b469f74bb65ca8
pchiorean <61617801+pchiorean@users.noreply.github.com>
2022-09-03 23:17:11 +0300

    `ref` Refactor to use similar code to its twin

--- view/ZoomToSelection.jsx
+++ view/ZoomToSelection.jsx
@@ -1,5 +1,5 @@
 /*
-	Zoom to selection 22.8.9
+	Zoom to selection 22.8.31
 	(c) 2020-2022 Paul Chiorean (jpeg@basement.ro)
 
 	Zooms to the selected objects. If no selection, it zooms to the current spread.
@@ -31,38 +31,25 @@ if (!(doc = app.activeDocument)) exit();
 var items, item, zoom, i, n;
 var window = app.activeWindow;
 var page = window.activePage;
+var selection = doc.selection;
 var oldSelection = doc.selection;
 var targetBounds = [];
 var TL = AnchorPoint.TOP_LEFT_ANCHOR;
 var BR = AnchorPoint.BOTTOM_RIGHT_ANCHOR;
 var CS_PBRD = CoordinateSpaces.PASTEBOARD_COORDINATES;
-var SP = 600;  // Side panels width
-var CP = 60;   // Control panel height
-var Z = 5.469; // Generic 4K monitor: 5.469; iMac Retina 5K: 5.856
-var N = 1;     // Number of spreads to zoom to
+
+// Customizable items
+var Z = 5.469; // App maximized on generic 4K monitor
+// var Z = 5.856; // App maximized on iMac Retina 5K
+var HC = 700; // Horizontal compensation (side panels)
+var VC = 150; // Vertical compensation (Application Bar, document tabs, Control Panel)
+var N = 1; // Number of spreads to zoom to
+
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 
-if (doc.selection.length === 0 || doc.selection[0].constructor.name === 'Guide') {
-	// Nothing useful is selected, we'll zoom to the spread(s)
-	targetBounds[0] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][1];
-	targetBounds[1] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][0];
-	targetBounds[2] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][1];
-	targetBounds[3] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][0];
-	for (i = 0; N > 1 && i < doc.spreads.length && i < N; i++) {
-		targetBounds[0] = Math.min(targetBounds[0], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][1]);
-		targetBounds[1] = Math.min(targetBounds[1], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][0]);
-		targetBounds[2] = Math.max(targetBounds[2], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][1]);
-		targetBounds[3] = Math.max(targetBounds[3], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][0]);
-	}
-	if (app.properties.activeWindow.screenMode === ScreenModeOptions.PREVIEW_OFF) {
-		targetBounds[0] -= doc.documentPreferences.properties.documentBleedTopOffset;
-		targetBounds[1] -= doc.documentPreferences.properties.documentBleedInsideOrLeftOffset;
-		targetBounds[2] += doc.documentPreferences.properties.documentBleedBottomOffset;
-		targetBounds[3] += doc.documentPreferences.properties.documentBleedOutsideOrRightOffset;
-	}
-} else {
-	// Zoom to selection
-	items = doc.selection;
+if (selection.length > 0 && selection[0].constructor.name !== 'Guide') {
+	// We have a selection, get bounds
+	items = selection;
 	item = items[0];
 	if (Object.prototype.hasOwnProperty.call(item, 'parentTextFrames')) { // Inside a text frame
 		item = item.parentTextFrames[0];
@@ -79,19 +66,43 @@ if (doc.selection.length === 0 || doc.selection[0].constructor.name === 'Guide')
 		targetBounds[2] = Math.max(items[i].visibleBounds[2], targetBounds[2]);
 		targetBounds[3] = Math.max(items[i].visibleBounds[3], targetBounds[3]);
 	}
+} else {
+	// Nothing is selected, get spreads' bounds
+	targetBounds[0] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][1];
+	targetBounds[1] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][0];
+	targetBounds[2] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][1];
+	targetBounds[3] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][0];
+	for (i = 0; N > 1 && i < doc.spreads.length && i < N; i++) {
+		targetBounds[0] = Math.min(targetBounds[0], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][1]);
+		targetBounds[1] = Math.min(targetBounds[1], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][0]);
+		targetBounds[2] = Math.max(targetBounds[2], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][1]);
+		targetBounds[3] = Math.max(targetBounds[3], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][0]);
+	}
+	if (app.properties.activeWindow.screenMode === ScreenModeOptions.PREVIEW_OFF) {
+		targetBounds[0] -= doc.documentPreferences.properties.documentBleedTopOffset;
+		targetBounds[1] -= doc.documentPreferences.properties.documentBleedInsideOrLeftOffset;
+		targetBounds[2] += doc.documentPreferences.properties.documentBleedBottomOffset;
+		targetBounds[3] += doc.documentPreferences.properties.documentBleedOutsideOrRightOffset;
+	}
+	app.select(null);
 }
+
 // Compute zoom percentage
 zoom = Math.min(
-	(UnitValue(window.bounds[3] - window.bounds[1] - SP, 'px').as('pt')) / (targetBounds[3] - targetBounds[1]),
-	(UnitValue(window.bounds[2] - window.bounds[0] + CP, 'px').as('pt')) / (targetBounds[2] - targetBounds[0])
+	(window.bounds[3] - window.bounds[1] - HC) / UnitValue((targetBounds[3] - targetBounds[1]), 'pt').as('px'),
+	(window.bounds[2] - window.bounds[0] + VC) / UnitValue((targetBounds[2] - targetBounds[0]), 'pt').as('px')
 );
 zoom = Number(zoom * 10 * Z).toFixed(2);
 zoom = (Math.max(5, zoom), Math.min(zoom, 4000)); // Keep it in 5-4000% range
+
 // Zoom to target
+if (N > 1 && selection.length === 0)
+	window.activePage = (doc.spreads.length > 2) ? doc.spreads[1].pages[0] : page;
 window.zoom(ZoomOptions.FIT_SPREAD);
 try { window.zoomPercentage = zoom; } catch (e) {
 	// Avoid error 30481 'Data is out of range'
 	try { app.menuActions.item('$ID/Fit Selection in Window').invoke(); } catch (_) {}
 }
+
 // Restore initial selection
 app.select(oldSelection);
--- view/ZoomToSpreads.jsx
+++ view/ZoomToSpreads.jsx
@@ -1,5 +1,5 @@
 /*
-	Zoom to spreads 22.8.9
+	Zoom to spreads 22.8.31
 	(c) 2021-2022 Paul Chiorean (jpeg@basement.ro)
 
 	Zooms to the current spread (if N = 1) or the first N spreads (if N > 1).
@@ -28,43 +28,81 @@
 
 if (!(doc = app.activeDocument)) exit();
 
-var zoom;
+var items, item, zoom, i, n;
 var window = app.activeWindow;
 var page = window.activePage;
+var selection = [];
+var oldSelection = doc.selection;
 var targetBounds = [];
 var TL = AnchorPoint.TOP_LEFT_ANCHOR;
 var BR = AnchorPoint.BOTTOM_RIGHT_ANCHOR;
 var CS_PBRD = CoordinateSpaces.PASTEBOARD_COORDINATES;
-var SP = 600;  // Side panels width
-var CP = 60;   // Control panel height
-var Z = 5.469; // Generic 4K monitor: 5.469; iMac Retina 5K: 5.856
-var N = 3;     // Number of spreads to zoom to
+
+// Customizable items
+var Z = 5.469; // App maximized on generic 4K monitor
+// var Z = 5.856; // App maximized on iMac Retina 5K
+var HC = 700; // Horizontal compensation (side panels)
+var VC = 150; // Vertical compensation (Application Bar, document tabs, Control Panel)
+var N = 3; // Number of spreads to zoom to
+
 app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 
-targetBounds[0] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][1];
-targetBounds[1] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][0];
-targetBounds[2] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][1];
-targetBounds[3] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][0];
-for (var i = 0; N > 1 && i < doc.spreads.length && i < N; i++) {
-	targetBounds[0] = Math.min(targetBounds[0], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][1]);
-	targetBounds[1] = Math.min(targetBounds[1], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][0]);
-	targetBounds[2] = Math.max(targetBounds[2], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][1]);
-	targetBounds[3] = Math.max(targetBounds[3], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][0]);
+if (selection.length > 0 && selection[0].constructor.name !== 'Guide') {
+	// We have a selection, get bounds
+	items = selection;
+	item = items[0];
+	if (Object.prototype.hasOwnProperty.call(item, 'parentTextFrames')) { // Inside a text frame
+		item = item.parentTextFrames[0];
+		app.select(item);
+	} else if (item.constructor.name === 'Table' || item.constructor.name === 'Cell') {
+		item = item.parent;
+		while (item.constructor.name !== 'TextFrame') item = item.parent;
+		app.select(item);
+	}
+	targetBounds = item.visibleBounds;
+	for (i = 1, n = items.length; i < n; i++) { // Get selection's extremities
+		targetBounds[0] = Math.min(items[i].visibleBounds[0], targetBounds[0]);
+		targetBounds[1] = Math.min(items[i].visibleBounds[1], targetBounds[1]);
+		targetBounds[2] = Math.max(items[i].visibleBounds[2], targetBounds[2]);
+		targetBounds[3] = Math.max(items[i].visibleBounds[3], targetBounds[3]);
+	}
+} else {
+	// Nothing is selected, get spreads' bounds
+	targetBounds[0] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][1];
+	targetBounds[1] = ((N === 1) ? page.parent : doc.spreads[0]).pages[0].resolve(TL, CS_PBRD)[0][0];
+	targetBounds[2] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][1];
+	targetBounds[3] = ((N === 1) ? page.parent : doc.spreads[0]).pages[-1].resolve(BR, CS_PBRD)[0][0];
+	for (i = 0; N > 1 && i < doc.spreads.length && i < N; i++) {
+		targetBounds[0] = Math.min(targetBounds[0], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][1]);
+		targetBounds[1] = Math.min(targetBounds[1], doc.spreads[i].pages[0].resolve(TL, CS_PBRD)[0][0]);
+		targetBounds[2] = Math.max(targetBounds[2], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][1]);
+		targetBounds[3] = Math.max(targetBounds[3], doc.spreads[i].pages[-1].resolve(BR, CS_PBRD)[0][0]);
+	}
+	if (app.properties.activeWindow.screenMode === ScreenModeOptions.PREVIEW_OFF) {
+		targetBounds[0] -= doc.documentPreferences.properties.documentBleedTopOffset;
+		targetBounds[1] -= doc.documentPreferences.properties.documentBleedInsideOrLeftOffset;
+		targetBounds[2] += doc.documentPreferences.properties.documentBleedBottomOffset;
+		targetBounds[3] += doc.documentPreferences.properties.documentBleedOutsideOrRightOffset;
+	}
+	app.select(null);
 }
-if (app.properties.activeWindow.screenMode === ScreenModeOptions.PREVIEW_OFF) {
-	targetBounds[0] -= doc.documentPreferences.properties.documentBleedTopOffset;
-	targetBounds[1] -= doc.documentPreferences.properties.documentBleedInsideOrLeftOffset;
-	targetBounds[2] += doc.documentPreferences.properties.documentBleedBottomOffset;
-	targetBounds[3] += doc.documentPreferences.properties.documentBleedOutsideOrRightOffset;
-}
+
 // Compute zoom percentage
 zoom = Math.min(
-	(UnitValue(window.bounds[3] - window.bounds[1] - SP, 'px').as('pt')) / (targetBounds[3] - targetBounds[1]),
-	(UnitValue(window.bounds[2] - window.bounds[0] + CP, 'px').as('pt')) / (targetBounds[2] - targetBounds[0])
+	(window.bounds[3] - window.bounds[1] - HC) / UnitValue((targetBounds[3] - targetBounds[1]), 'pt').as('px'),
+	(window.bounds[2] - window.bounds[0] + VC) / UnitValue((targetBounds[2] - targetBounds[0]), 'pt').as('px')
 );
 zoom = Number(zoom * 10 * Z).toFixed(2);
 zoom = (Math.max(5, zoom), Math.min(zoom, 4000)); // Keep it in 5-4000% range
+
 // Zoom to target
-window.activePage = (doc.spreads.length > 2) ? doc.spreads[1].pages[0] : doc.spreads[0].pages[0];
+if (N > 1 && selection.length === 0)
+	window.activePage = (doc.spreads.length > 2) ? doc.spreads[1].pages[0] : page;
 window.zoom(ZoomOptions.FIT_SPREAD);
-window.zoomPercentage = zoom;
+try { window.zoomPercentage = zoom; } catch (e) {
+	// Avoid error 30481 'Data is out of range'
+	try { app.menuActions.item('$ID/Fit Selection in Window').invoke(); } catch (_) {}
+}
+
+// Restore initial selection
+app.select(oldSelection);
