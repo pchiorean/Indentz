ce11e9620ea2ca1ed62b9fa4c9ce73a4430e9801
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2023-02-22 18:53:29 +0200

    `upd` Add generic tiling for 1:1 formats

--- view/TileAll.jsx
+++ view/TileAll.jsx
@@ -1,9 +1,8 @@
 /*
-	Tile all 22.11.12
-	(c) 2020-2022 Paul Chiorean (jpeg@basement.ro)
+	Tile all 23.2.22
+	(c) 2020-2023 Paul Chiorean <jpeg@basement.ro>
 
-	Invokes 'Tile All Vertically' or 'Tile All Horizontally',
-	depending on current spread orientation.
+	Invokes different window tiling options, depending on current spread orientation.
 
 	Released under MIT License:
 	https://choosealicense.com/licenses/mit/
@@ -18,15 +17,11 @@ var size = {
 	width:  page.parent.pages.lastItem().bounds[3] - page.parent.pages.firstItem().bounds[1],
 	height: page.parent.pages.lastItem().bounds[2] - page.parent.pages.firstItem().bounds[0]
 };
+var R = size.width / size.height;
 
-switch (size.width / size.height <= 1) {
-	case true:
-		app.menuActions.item('$ID/Tile All Vertically').invoke();
-		break;
-	case false:
-		app.menuActions.item('$ID/Tile All Horizontally').invoke();
-		break;
-}
+if (R < 1) app.menuActions.item('$ID/Tile All Vertically').invoke();
+else if (R > 1) app.menuActions.item('$ID/Tile All Horizontally').invoke();
+else app.menuActions.item('$ID/Tile').invoke();
 
 app.selection = NothingEnum.NOTHING;
 for (var i = 0, n = app.windows.length; i < n; i++)
