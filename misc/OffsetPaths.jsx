8b25054e7e3c7f18a5cca89de361806197d4e75d
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2023-05-29 13:47:22 +0300

    `ref` Minor tweaks

--- misc/OffsetPaths.jsx
+++ misc/OffsetPaths.jsx
@@ -1,19 +1,24 @@
 /*
-	Offset Paths 21.9.22
+	Offset paths 21.9.22
 
 	Use InDesign's text wrap feature to create offset/inset paths.
 
-	(c) Olav Martin Kvern. Distributed by Silicon Publishing, Inc.
+	Original version (c) 2020 Olav Martin Kvern.
+	Distributed by Silicon Publishing, Inc.
 	https://creativepro.com/indesign-cad-tool/
 	https://www.siliconpublishing.com/blog/free-indesign-scripts/
 
-	Changes by Paul Chiorean (jpeg@basement.ro):
-	- Take any objects as source;
-	- Duplicate source object to avoid some corner cases (e.g., images);
-	- Select outlined paths on exit;
-	- Add a default value in millimeters;
-	- Add option to join contours;
-	- Add undo.
+	This is a custom version of the above script with some tweaks:
+
+	- Now takes any objects as source;
+	- Duplicates the source object to avoid some corner cases (e.g., images);
+	- Selects the outlined paths on exit;
+	- Has a default value in millimeters;
+	- Has an option for joining contours;
+	- Has undo.
+
+	2021/09/22
+	Paul Chiorean <jpeg@basement.ro>
 */
 
 /* eslint-disable no-with */
@@ -24,10 +29,12 @@ app.doScript(main, ScriptLanguage.JAVASCRIPT, doc.selection,
 	UndoModes.ENTIRE_SCRIPT, 'Offset paths');
 
 function main(selection) {
-	var ui, joinContours, offsetField, offset;
+	var i, ui, joinContours, offsetField, offset;
 	var paths = [];
-	var oldMU  = app.scriptPreferences.measurementUnit;
-	var oldUIL = app.scriptPreferences.userInteractionLevel;
+	var old = {
+		MU: app.scriptPreferences.measurementUnit,
+		UIL: app.scriptPreferences.userInteractionLevel
+	};
 	app.scriptPreferences.measurementUnit = MeasurementUnits.POINTS;
 	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.INTERACT_WITH_ALL;
 	app.scriptPreferences.enableRedraw = false;
@@ -37,9 +44,9 @@ function main(selection) {
 		with (dialogRows.add()) {
 			staticTexts.add({ staticLabel: 'Offset:' });
 			offsetField = measurementEditboxes.add({
-				minWidth:   85,
-				editUnits:  MeasurementUnits.MILLIMETERS,
-				editValue:  UnitValue('3 mm').as('pt'),
+				minWidth: 85,
+				editUnits: MeasurementUnits.MILLIMETERS,
+				editValue: UnitValue('3 mm').as('pt'),
 				smallNudge: 1,
 				largeNudge: 1
 			});
@@ -47,7 +54,7 @@ function main(selection) {
 		with (dialogRows.add()) { '' }
 		with (dialogRows.add()) {
 			joinCB = checkboxControls.add({
-				staticLabel:  'Join contours',
+				staticLabel: 'Join contours',
 				checkedState: true
 			});
 		}
@@ -56,27 +63,26 @@ function main(selection) {
 	if (ui.show()) {
 		offset = offsetField.editValue;
 		joinContours = joinCB.checkedState;
-		for (var i = 0; i < selection.length; i++) paths.push(offsetPath(selection[i]));
+		for (i = 0; i < selection.length; i++) paths.push(offsetPath(selection[i]));
 		if (joinContours && paths.length > 1) paths = (paths.shift()).addPath(paths);
 		app.select(paths);
 	}
 	ui.destroy();
-	app.scriptPreferences.measurementUnit = oldMU;
-	app.scriptPreferences.userInteractionLevel = oldUIL;
+	app.scriptPreferences.measurementUnit = old.MU;
+	app.scriptPreferences.userInteractionLevel = old.UIL;
 
 	function offsetPath(item) {
-		var tmpItem, wrapPaths, newPath;
+		var i, tmpItem, wrapPaths, newPath;
 		tmpItem = item.duplicate();
-		// tmpItem.bringToFront(item);
 		if (tmpItem.contentType === ContentType.GRAPHIC_TYPE && tmpItem.graphics.length === 1)
 			tmpItem.pageItems[0].remove();
 		tmpItem.textWrapPreferences.textWrapMode = TextWrapModes.CONTOUR;
 		tmpItem.textWrapPreferences.textWrapOffset = [ offset, offset, offset, offset ];
 		wrapPaths = tmpItem.textWrapPreferences.paths;
 		newPath = tmpItem.parent.polygons.add();
-		for (var i = 0; i < wrapPaths.length; i++) {
+		for (i = 0; i < wrapPaths.length; i++) {
 			if (i > 0) newPath.paths.add();
-			newPath.paths.item(i).pathType   = wrapPaths.item(i).pathType;
+			newPath.paths.item(i).pathType = wrapPaths.item(i).pathType;
 			newPath.paths.item(i).entirePath = wrapPaths.item(i).entirePath;
 		}
 		tmpItem.remove();
