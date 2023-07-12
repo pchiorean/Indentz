00293f306215e4fc31e6d1667c7ce07428612b11
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2023-07-12 22:21:19 +0300

    `upd` Check if doc path is valid; update header

--- cleanup/DumpLayers.jsx
+++ cleanup/DumpLayers.jsx
@@ -1,18 +1,20 @@
 /*
-	Dump layers 23.2.3
+	Dump layers 23.7.12
 	(c) 2022-2023 Paul Chiorean <jpeg@basement.ro>
 
-	Saves document's layers to a TSV file compatible with `Defaultlayers.jsx`:
+	Saves document's layers to a TSV file compatible with `DefaultLayers.jsx`
 
-	Name     | Color   | Visible | Printable
-	dielines | Magenta | yes     | yes
-	template | Gray    | no      | no
+	Name       | Color   | Visible | Printable | Order | Variants
+	dielines   | Magenta | yes     | yes       |       |
+	bg         | Red     | yes     | yes       |       |
+	.reference | Black   | no      | no        |       |
 	...
 
-	<Name>: layer name,
-	<Color>: layer color (see UIColors.txt for color names),
-	<Visible>: `yes` or `no`,
-	<Printable>: `yes` or `no`.
+	<Name>: layer name
+	<Color>: layer color (see UIColors.txt for color names)
+	<Visible>: `yes` or `no`
+	<Printable>: `yes` or `no`
+	<Order> and <Variants>: empty (see `DefaultLayers.jsx` for more info)
 
 	Released under MIT License:
 	https://choosealicense.com/licenses/mit/
@@ -20,13 +22,23 @@
 
 if (!(doc = app.activeDocument)) exit();
 
-var l, layers;
-var dataFile = File(String(doc.fullName).replace(/\.indd$/ig, '_layers.tsv'));
+var dataFile, l, layers;
+var docHasPath = (function () {
+	var ret = false;
+	try { ret = !!doc.filePath; } catch (e) {}
+	return ret;
+}());
 
+if (!docHasPath) {
+	alert('Can\'t get document path.\nSave the document first and try again.');
+	exit();
+}
+
+dataFile = File(String(doc.filePath + '/' + doc.name).replace(/\.indd$/ig, '_layers.tsv'));
 dataFile.open('w');
 dataFile.encoding = 'UTF-8';
 dataFile.lineFeed = 'Unix';
-dataFile.writeln('Name\tColor\tVisible\tPrintable'); // Header
+dataFile.writeln('Name\tColor\tVisible\tPrintable\tOrder\tVariants'); // Header
 
 layers = doc.layers.everyItem().getElements();
 while ((l = layers.shift())) {
--- cleanup/DumpSwatches.jsx
+++ cleanup/DumpSwatches.jsx
@@ -1,19 +1,20 @@
 /*
-	Dump swatches 23.2.3
+	Dump swatches 23.7.12
 	(c) 2020-2023 Paul Chiorean <jpeg@basement.ro>
 
-	Saves document's swatches to a TSV file compatible with `DefaultSwatches.jsx`:
+	Saves document's swatches to a TSV file compatible with `DefaultSwatches.jsx`
 
-	Name       | Color Model | Color Space | Values
-	Rich Black | process     | cmyk        | 60 40 40 100
-	RGB Grey   | process     | rgb         | 128 128 128
-	Cut        | spot        | cmyk        | 0 100 0 0
+	Name       | Color Model | Color Space | Values       | Variants
+	Rich Black | process     | cmyk        | 60 40 40 100 |
+	RGB Grey   | process     | rgb         | 128 128 128  |
+	Cut        | spot        | cmyk        | 0 100 0 0    |
 	...
 
-	<Name>: swatch name,
-	<Color Model>: `process` or `spot`,
-	<Color Space>: `cmyk`, `rgb` or `lab`,
-	<Values>: list of values, depends on the color model & space.
+	<Name>: swatch name
+	<Color Model>: `process` or `spot`
+	<Color Space>: `cmyk`, `rgb` or `lab`
+	<Values>: list of values, depends on the color model & space
+	<Variants>: empty (see `DefaultSwatches.jsx` for more info)
 
 	Released under MIT License:
 	https://choosealicense.com/licenses/mit/
@@ -21,13 +22,23 @@
 
 if (!(doc = app.activeDocument)) exit();
 
-var c, cols, i, k;
-var dataFile = File(String(doc.fullName).replace(/\.indd$/ig, '_swatches.tsv'));
+var dataFile, c, cols, i, k;
+var docHasPath = (function () {
+	var ret = false;
+	try { ret = !!doc.filePath; } catch (e) {}
+	return ret;
+}());
 
+if (!docHasPath) {
+	alert('Can\'t get document path.\nSave the document first and try again.');
+	exit();
+}
+
+dataFile = File(String(doc.filePath + '/' + doc.name).replace(/\.indd$/ig, '_swatches.tsv'));
 dataFile.open('w');
 dataFile.encoding = 'UTF-8';
 dataFile.lineFeed = 'Unix';
-dataFile.writeln('Name\tColor Model\tColor Space\tValues'); // Header
+dataFile.writeln('Name\tColor Model\tColor Space\tValues\tVariants'); // Header
 
 cols = doc.colors.everyItem().getElements();
 while ((c = cols.shift())) {
