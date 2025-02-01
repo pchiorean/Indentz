7abc92a6890ae8318b4537cd1f165f6c1ad13546
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2025-02-01 15:42:48 +0200

    `upd` Tolerate slight mismatches

--- Document/Pagination/SplitSpreadsByLayers.jsx
+++ Document/Pagination/SplitSpreadsByLayers.jsx
@@ -1,8 +1,8 @@
 /*
-	Split/join spreads by layers 24.6.16
-	(c) 2024 Paul Chiorean <jpeg@basement.ro>
+	Split/join spreads by layers 25.2.1
+	(c) 2025 Paul Chiorean <jpeg@basement.ro>
 
-	Splits or joins document spreads using special layers.
+	Splits or joins document spreads using a list of layers.
 
 	Released under MIT License:
 	https://choosealicense.com/licenses/mit/
@@ -17,38 +17,44 @@ app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.ENTIRE_SCRIPT
 
 function main() {
 	var item, items, layer, i, j;
-	var msg = [];
-	var layers = [ 'DE', 'FR', 'IT' ]; // Special layers
+	var layers = {
+		master: [ 'DE', 'FR', 'IT' ],
+		used: []
+	};
+
 	app.scriptPreferences.enableRedraw = false;
 
-	// Check if all special layers are valid
-	for (i = 0; i < layers.length; i++) if (!doc.layers.itemByName(layers[i]).isValid) msg.push(layers[i]);
-	if (msg.length > 0) {
-		alert('Missing layers\nThe document must also contain the following layers:\n\'' + msg.join('\', \'') + '\'.');
-		exit();
-	}
+	// Build a list of valid layers from the master list
+	for (i = 0; i < layers.master.length; i++)
+		if ((layer = doc.layers.itemByName(layers.master[i])).isValid) layers.used.push(layer);
 
-	// One spread? Split by special layers
-	// Duplicate the first spread and filter special layers' items for each spread
-	if (doc.spreads.length === 1) {
-		for (i = 1; i < layers.length; i++) doc.spreads[0].duplicate(LocationOptions.AT_END);
+	// If we have one spread, split it by layers
+	if (doc.spreads.length === 1 && layers.used.length > 1) {
+		// Duplicate the first spread
+		for (i = 1; i < layers.used.length; i++) doc.spreads[0].duplicate(LocationOptions.AT_END);
+
+		// For each spread, keep only the items on the corresponding layer
 		for (i = 0; i < doc.spreads.length; i++) {
-			for (j = 0; j < layers.length; j++) {
-				if ((layer = doc.layers.itemByName(layers[j])).isValid) layer.visible = true;
-				else continue;
-				if (i === j) continue;
+			for (j = 0; j < layers.used.length; j++) {
+				layer = doc.layers.itemByName(layers.used[j].name);
+				if (i === j) continue; // Skip self
+
 				items = getPageItems('<*>', doc.spreads[i], layer.name);
-				if (!items) continue;
+				if (!items) continue; // Skip empty layers
+
 				while ((item = items.shift())) try { item.remove(); } catch (_) {}
 			}
 		}
-	// Multiple spreads? Combine by special layers
-	// Get each special layer's parent spread and move items to the first spread
-	} else if (doc.spreads.length === layers.length) {
-		for (i = 0; i < layers.length; i++) {
-			items = getPageItems('<*>', doc, layers[i]);
-			if (!items) { alert('Warning\nLayer \'' + layers[i] + '\' is empty.'); continue; }
-			items = getPageItems('<*>', items[0].parentPage.parent, layers[i]);
+
+	// If we have multiple spreads, combine them:
+	// Get each layer's parent spread and move items to the first spread
+	} else if (doc.spreads.length <= layers.used.length) {
+		for (i = 0; i < layers.used.length; i++) {
+			layer = layers.used[i];
+			items = getPageItems('<*>', doc, layer.name);
+			if (!items) continue; // Skip empty layers
+
+			items = getPageItems('<*>', items[0].parentPage.parent, layer.name);
 			while ((item = items.shift())) {
 				try {
 					item.duplicate(doc.spreads[0]);
@@ -56,8 +62,17 @@ function main() {
 				} catch (_) {}
 			}
 		}
-		doc.spreads.itemByRange(1, doc.spreads.length - 1).remove(); // Remove obsolete spreads
-	} else {
-		alert('Mismatched spreads\nThe document must have either one or ' + layers.length + ' spreads.');
+
+		// Remove obsolete spreads
+		doc.spreads.itemByRange(1, doc.spreads.length - 1).remove();
+	} else if (layers.used.length > 0) {
+		alert('Source/destination mismatch'
+			+ '\nSource: ' + doc.spreads.length + ' spreads.'
+			+ ' \nTarget: ' + layers.used.length + ' layers: \''
+			+ (function (r) {
+				var i, rr; for (i = 0, rr = []; i < r.length; i++) rr[i] = r[i].name; return rr;
+				}(layers.used)).join('\', \'')
+			+ '\'.'
+		);
 	}
 }
