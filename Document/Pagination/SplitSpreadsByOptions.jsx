37cf63e9b03b3d133a85ff74c299828e920eccb2
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2025-03-09 21:50:58 +0200

    `upd` Delete optionless spreads

--- Document/Pagination/SplitSpreadsByOptions.jsx
+++ Document/Pagination/SplitSpreadsByOptions.jsx
@@ -24,37 +24,38 @@ function main() {
 	var optionRE = /^.*:\s*(.+)\s*$/; // Match layers containing ':<option>'
 	var optionLayers = {};
 	var optionNames = [];
+	var optionVisibility = {};
 
 	app.scriptPreferences.enableRedraw = false;
 
 	// Build a list of options
 	for (i = 0; i < doc.layers.length; i++) {
 		layer = doc.layers[i];
-		if (optionRE.test(layer.name))
+		if (optionRE.test(layer.name)) {
 			optionLayers[layer.name.match(optionRE)[1]] = [];
+			optionVisibility[layer.name.match(optionRE)[1]] = true;
+		}
 	}
-	for (var key in optionLayers)
-		if (Object.prototype.hasOwnProperty.call(optionLayers, key)) optionNames.push(key);
-	if (optionNames.length === 0) exit();
+	for (var key in optionLayers) if (Object.prototype.hasOwnProperty.call(optionLayers, key)) optionNames.push(key);
+	if (optionNames.length === 0) exit(); // Exit if no option-specific layers exist
 
 	// Build a list of corresponding layers
 	for (i = 0; i < doc.layers.length; i++) {
 		layer = doc.layers[i];
-		if (optionRE.test(layer.name))
-			optionLayers[layer.name.match(optionRE)[1]].push(layer);
+		if (optionRE.test(layer.name)) optionLayers[layer.name.match(optionRE)[1]].push(layer);
 	}
 
-	// If we have one spread, split it by options
+	// 1. If we have one spread, split it by options
+	// - Duplicate the first spread
+	// - For each spread, keep the corresponding option and delete others
+	// - Delete optionless spreads
 	if (doc.spreads.length === 1 && optionNames.length > 1) {
-		// Duplicate the first spread
 		for (i = 1; i < optionNames.length; i++) doc.spreads[0].duplicate(LocationOptions.AT_END);
 
-		// For each spread, keep the corresponding option and delete others
 		for (i = 0; i < doc.spreads.length; i++) {
 			for (k = 0; k < optionNames.length; k++) {
 				option = optionNames[k];
 				if (k === i) continue; // Skip self
-
 				for (l = 0; l < optionLayers[option].length; l++) {
 					layer = optionLayers[option][l];
 					items = getPageItems('<*>', doc.spreads[i], layer.name);
@@ -64,27 +65,39 @@ function main() {
 			}
 		}
 
-	// If we have multiple spreads, combine them:
-	// Get each layer's parent spread and move items to the first spread
+		for (i = 0; i < doc.spreads.length; i++) {
+			for (k = 0; k < optionNames.length; k++) {
+				option = optionNames[k];
+				if (k !== i) continue; // Skip self
+				for (l = 0; l < optionLayers[option].length; l++) {
+					layer = optionLayers[option][l];
+					items = getPageItems('<*>', doc.spreads[i], layer.name);
+					optionVisibility[option] = optionVisibility[option] && (items.length > 0);
+				}
+			}
+		}
+		for (i = 0; i < doc.spreads.length; i++)
+			if (!optionVisibility[optionNames[i]]) try { doc.spreads[i].remove(); } catch (_) {}
+
+	// 2. If we have multiple spreads, combine them:
+	// - Get each option-specific layer's parent spread and move items to the first spread
+	// - Delete obsolete spreads
 	} else if (doc.spreads.length <= optionNames.length) {
 		for (k = 0; k < optionNames.length; k++) {
 			option = optionNames[k];
-
 			for (l = 0; l < optionLayers[option].length; l++) {
 				layer = optionLayers[option][l];
 				items = getPageItems('<*>', doc, layer.name);
 				if (items.length === 0) continue; // Skip empty layers
 				items = getPageItems('<*>', items[0].parentPage.parent, layer.name);
-				while ((item = items.shift())) {
-					try {
-						item.duplicate(doc.spreads[0]);
-						item.remove();
-					} catch (_) {}
-				}
+				while ((item = items.shift())) try { item.duplicate(doc.spreads[0]); item.remove(); } catch (_) {}
 			}
 		}
-		doc.spreads.itemByRange(1, doc.spreads.length - 1).remove(); // Remove obsolete spreads
-	} else if (optionNames.length > 0) {
+
+		doc.spreads.itemByRange(1, doc.spreads.length - 1).remove();
+
+	// 3. Source/destination mismatch
+	} else {
 		alert('Source/destination mismatch'
 			+ '\nSource: ' + doc.spreads.length + ' spreads.'
 			+ '\nDestination: \'' + optionNames.join('\', \'')
