d734d88b7a5bf09e1f7f8bfada4d7470f133e528
Paul Chiorean <61617801+pchiorean@users.noreply.github.com>
2026-06-08 12:54:34 +0300

    `upd` Make the script non-interactive

--- Document/Pagination/SplitDocBySpreads.jsx
+++ Document/Pagination/SplitDocBySpreads.jsx
@@ -1,6 +1,6 @@
 /*
-	Split document by spreads 24.6.16
-	(c) 2020-2024 Paul Chiorean <jpeg@basement.ro>
+	Split document by spreads 26.6.8
+	(c) 2020-2026 Paul Chiorean <jpeg@basement.ro>
 
 	Saves the spreads of the active document in separate files.
 
@@ -33,26 +33,16 @@ if (!(doc = app.activeDocument)) exit();
 if (!doc.saved) { alert('Document is not saved.'); exit(); }
 if (doc.spreads.length === 1) { alert('Document has only one spread.'); exit(); }
 
-var defaultTemplate, detectedTemplate, template, customPosition, spread, targetFile, target, progressBar, i, j;
+var customPosition, progressBar, spread, target, targetFile, i, j;
 var r = [];
-var ADV = ScriptUI.environment.keyboardState.altKey;
-var invalidFilenameCharsRE = /[<>:"\/\\|?*]/g; // https://gist.github.com/doctaphred/d01d05291546186941e1b7ddc02034d3
 var oldUIL = app.scriptPreferences.userInteractionLevel;
 var currentPath = doc.filePath;
 var baseName = (/\./g.test(doc.name) && doc.name.slice(0, doc.name.lastIndexOf('.'))) || doc.name;
 
-// Detect or ask user for a template
-defaultTemplate = '-123456789abcdefghijklmnopqrstuvwxyz'.slice(0, doc.spreads.length + 1);
-detectedTemplate = RegExp('[ ._-][a-zA-Z0-9]{' + doc.spreads.length + '}$', 'i').exec(baseName);
-if (/\d\s*x\s*\d/i.test(detectedTemplate)) detectedTemplate = null; // Exclude '0x0' suffixes
-if (ADV) template = templateFromUser(null); // In advanced mode always ask user
-else template = detectedTemplate ? String(detectedTemplate) : templateFromUser(null);
-if (template === String(detectedTemplate)) baseName = baseName.replace(detectedTemplate, '');
-
 // Detect custom position placeholder
 customPosition = baseName.match(/(.+?)#(.+)/);
 
-progressBar = new ProgressBar('Saving', doc.spreads.length);
+if (doc.spreads.length > 3) progressBar = new ProgressBar('Saving', doc.spreads.length);
 for (spread = 0; spread < doc.spreads.length; spread++) {
 	// Filter out current spread
 	r = [];
@@ -61,12 +51,12 @@ for (spread = 0; spread < doc.spreads.length; spread++) {
 	// Get unique name
 	targetFile = File(currentPath + '/'
 		+ uniqueName(customPosition
-			? (customPosition[1] + template[spread + 1] + customPosition[2]) // Custom position
-			: (baseName + template[0] + template[spread + 1]) // Default position
+			? (customPosition[1] + (spread + 1) + customPosition[2]) // Custom position
+			: (baseName + '-' + (spread + 1)) // Default position
 		) + '.indd');
 
 	// Disable user interaction and open a copy
-	progressBar.update();
+	if (progressBar) progressBar.update();
 	doc.saveACopy(targetFile);
 	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
 	target = app.open(targetFile, false);
@@ -77,36 +67,8 @@ for (spread = 0; spread < doc.spreads.length; spread++) {
 	target.save(targetFile);
 	target.close();
 }
-progressBar.close();
+if (progressBar) progressBar.close();
 doc.close(SaveOptions.ASK);
-
-function templateFromUser(str) {
-	if (!str) str = detectedTemplate || defaultTemplate;
-	str = prompt('Please enter an index list.\nThe first character is the separator, the rest will be '
-		+ 'added sequentially to each filename; for example \'-123\' will split a 3-spreads document into\n\n'
-		+ 'Document-1.indd\nDocument-2.indd\nDocument-3.indd\n\nIf the filename contains a "#", the index '
-		+ 'will be placed in that position (the separator is ignored).', str);
-	if (!str) exit();
-
-	// Sanitize input
-	// -- Length
-	if (str.length < doc.spreads.length + 1) {
-		alert('You must enter a separator and ' + doc.spreads.length + ' additional characters.');
-		templateFromUser(str);
-	}
-	// -- Separator
-	if (!/[ ._\-~]/.test(str[0])) {
-		alert('You entered an invalid separator, please try again.\nUse only space, period, '
-			+ 'underscore, hyphen or tilde.');
-		templateFromUser(str);
-	}
-	// -- Filename
-	if (invalidFilenameCharsRE.test(str)) {
-		alert('You entered an invalid character, please try again.\nAvoid < > : " / \\ | ? and *.');
-		templateFromUser(str);
-	}
-	return str.slice(0, doc.spreads.length + 1);
-}
 
 function uniqueName(str) {
 	var name;
